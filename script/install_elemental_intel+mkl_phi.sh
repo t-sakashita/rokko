9775f7503837fcc3828fdf15898d7749297450ac
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-05-02 17:54:14 +0900

    remove unnecessary files.

--- script/install_elemental_intel+mkl_phi.sh
+++ script/install_elemental_intel+mkl_phi.sh
@@ -5,7 +5,7 @@ tar xvf elemental-0.78-p1.tgz
 mkdir -p $HOME/build
 cd $HOME/build
 cmake -D CMAKE_CXX_COMPILER=mpicxx -D CMAKE_C_COMPILER=mpicc -D CMAKE_Fortran_COMPILER=mpif90 \
--D MATH_LIBS="-mkl=parallel" \
+-D MATH_LIBS="-mkl=parallel -lifcore" \
 -D CMAKE_INSTALL_PREFIX=/opt/nano/rokko \
 -D ELEM_EXAMPLES=ON -D ELEM_TESTS=ON \
 ..
