d3dc28d946c002de53c14f251717052cdc91d5eb
Tatsuya Sakashita <n000402@maki1.fx10hpc>
2013-05-15 09:49:35 +0900

    modified install script of anasazi in trilinos-11.2.3 for fx10

--- script/install_anasazi_FX10.sh
+++ script/install_anasazi_FX10.sh
@@ -1,25 +1,65 @@
 #!/bin/bash
 
-tar xvf ../trilinos-11.0.3-Source.tar.bz2 
-patch -u -p1 -d ./trilinos-11.0.3-Source/ < ./trilinos_anasazi_fx10.patch
+PREFIX="$1"
+test -z "$PREFIX" && PREFIX=$HOME/opt/rokko
+echo "PREFIX = $PREFIX"
 
-cd trilinos-11.0.3-Source
-rm -Rf ./build/*
-mkdir build
-cd build
+mkdir -p $WORK/build
+cd $WORK/build
+rm -rf trilinos-11.2.3-Source
+tar jxf $WORK/source/trilinos-11.2.3-Source.tar.bz2
+
+cd $WORK/build/trilinos-11.2.3-Source
+patch -p1 << EOF
+--- trilinos-11.2.3-Source/cmake/tribits/package_arch/TribitsFortranMangling.cmake
++++ trilinos-11.2.3-Source/cmake/tribits/package_arch/TribitsFortranMangling.cmake
+@@ -63,7 +63,7 @@
+   # Verify the selected combination of Fortran and C++ compilers.
+   IF("${CMAKE_VERSION}" VERSION_GREATER 2.7.20090824 AND NOT ${PROJECT_NAME}_SKIP_FORTRANCINTERFACE_VERIFY_TEST)
+     INCLUDE(FortranCInterface)
+-    FortranCInterface_VERIFY(CXX)
++#    FortranCInterface_VERIFY(CXX)
+   ENDIF()
+ ENDIF()
+
+EOF
+
+
+patch -p1 << EOF
+--- trilinos-11.2.3-Source/packages/triutils/src/Trilinos_Util_CrsMatrixGallery.cpp
++++ trilinos-11.2.3-Source/packages/triutils/src/Trilinos_Util_CrsMatrixGallery.cpp
+@@ -64,6 +64,8 @@
+ #include "Trilinos_Util_CommandLineParser.h"
+ #include "Trilinos_Util_CrsMatrixGallery.h"
+
++inline long long abs(long long i) { return llabs(i); }
++
+ const double UNDEF = -99999.87;
+ const bool Scaling = false;
+
+EOF
+
+patch -p1 < ~/development/rokko/script/TPI.patch.c_option
+
+
+cd $WORK/build
+mkdir trilinos-11.2.3-build
+rm -rf trilinos-11.2.3-build/*
+cd trilinos-11.2.3-build
 
 cmake \
 -D TPL_ENABLE_MPI:BOOL=ON  \
 -D CMAKE_Fortran_COMPILER:FILEPATH="mpifrtpx" -D CMAKE_CXX_COMPILER:FILEPATH="mpiFCCpx" -D CMAKE_C_COMPILER:FILEPATH="mpifccpx" \
--D CMAKE_CXX_FLAGS="-Xg -mt" \
--D CMAKE_C_FLAGS="-Xg -mt" \
--D TPL_BLAS_LIBRARIES:STRING="-SSL2 --linkfortran" \
--D TPL_LAPACK_LIBRARIES:STRING="-SSL2 --linkfortran" \
+-D CMAKE_CXX_FLAGS="-Kopenmp -Xg -mt" \
+-D CMAKE_C_FLAGS="-Kopenmp -Xg -mt" \
+-D TPL_BLAS_LIBRARIES:STRING="-SSL2BLAMP --linkfortran" \
+-D TPL_LAPACK_LIBRARIES:STRING="-SSL2BLAMP --linkfortran" \
 -D CMAKE_INSTALL_PREFIX:PATH="../install_build/" \
 -D CMAKE_EXE_LINKER_FLAGS="--linkfortran" \
 -D Trilinos_EXTRA_LINK_FLAGS:STRING="--linkfortran" \
 -D Trilinos_ENABLE_Anasazi:BOOL=ON \
-..
+-D Trilinos_ENABLE_EXAMPLES:BOOL=ON -D Trilinos_ENABLE_TESTS:BOOL=ON \
+$WORK/build/trilinos-11.2.3-Source
 
 make -j2 2>&1 | tee make.log
 make install
