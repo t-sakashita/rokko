3946d512822a345660884b14d24df59a1e8761c5
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-05-15 09:42:42 +0900

    revised install script for new version of Elemental / Anasazi

--- script/install_anasazi_phi.sh
+++ script/install_anasazi_phi.sh
@@ -10,6 +10,7 @@ rm -rf trilinos-11.2.3-Source
 tar jxf $HOME/source/trilinos-11.2.3-Source.tar.bz2
 
 mkdir trilinos-11.2.3-build
+rm -rf trilinos-11.2.3-build/*
 cd trilinos-11.2.3-build
 
 #cmake -DTPL_ENABLE_MPI=ON -DCMAKE_Fortran_COMPILER="openmpif90" -DCMAKE_CXX_COMPILER="openmpicxx" -DCMAKE_C_COMPILER="openmpicc" -DTPL_BLAS_LIBRARIES="-L/opt/local/lib -lptcblas -lptf77b#las -latlas" -DTPL_LAPACK_LIBRARIES="-L/opt/local/lib -llapack" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DTrilinos_ENABLE_Anasazi=ON $HOME/build/trilinos-11.0.3-Source
@@ -22,6 +23,7 @@ cmake \
 -D CMAKE_INSTALL_PREFIX:PATH="../install_build"  \
 -D TPL_ENABLE_Boost:BOOL=ON -D Boost_INCLUDE_DIRS:PATH=/opt/nano/alps/alps-20121208-r6630/include/boost/ -D Boost_LIBRARY_DIRS:PATH=/opt/nano/alps/alps-20121208-r6630/lib/ \
 -D Trilinos_ENABLE_Anasazi:BOOL=ON \
+-D Trilinos_ENABLE_EXAMPLES:BOOL=ON -D Trilinos_ENABLE_TESTS:BOOL=ON \
 $HOME/build/trilinos-11.2.3-Source
 
 make -j4
