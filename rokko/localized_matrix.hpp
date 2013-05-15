0311c5fc16d3aa44cc767162da7dd91183f27219
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-05-15 23:12:52 +0900

    fix for g++ 4.7

--- rokko/localized_matrix.hpp
+++ rokko/localized_matrix.hpp
@@ -1,36 +1,20 @@
 #ifndef ROKKO_LOCALIZED_H
 #define ROKKO_LOCALIZED_H
 
-#include <iostream>
-#include <cstdlib>
-#include <boost/type_traits/is_same.hpp>
-
-#include <Eigen/Core>
 #include <Eigen/Dense>
 
-
 namespace rokko {
-
-class solver;
-
-/*
-struct matrix_row_major {};
-
-struct matrix_col_major {};
-*/
 
 template<typename MATRIX_MAJOR = rokko::matrix_row_major>
 struct eigen3_matrix_major;
 
-
-struct eigen3_matrix_major<rokko::matrix_row_major>
-{
+template<>
+struct eigen3_matrix_major<rokko::matrix_row_major> {
   static const int value= Eigen::RowMajor;
 };
 
 template<>
-struct eigen3_matrix_major<rokko::matrix_col_major>
-{
+struct eigen3_matrix_major<rokko::matrix_col_major> {
   static const int value = Eigen::ColMajor;
 };
 
@@ -40,7 +24,6 @@ public:
   localized_matrix() : Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, eigen3_matrix_major<MATRIX_MAJOR>::value >() {};
   localized_matrix(int rows, int cols) : Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, eigen3_matrix_major<MATRIX_MAJOR>::value >(rows, cols) {};
 };
-
 
 } // namespace rokko
 
