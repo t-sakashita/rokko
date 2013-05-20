c6804c8ac3a0916077559cbd8982221e0512d74e
u00306 <u00306@psi.issp.u-tokyo.ac.jp>
2013-05-21 02:06:58 +0900

    modified draw_time_dim.py

--- python_benchmark/draw_time_dim.py
+++ python_benchmark/draw_time_dim.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-You can use TeX to render all of your matplotlib text if the rc
-parameter text.usetex is set.  This works currently on the agg and ps
-backends, and requires that you have tex and the other dependencies
-described at http://matplotlib.sf.net/matplotlib.texmanager.html
-properly installed on your system.  The first time you run a script
-you will see a lot of output from tex and associated tools.  The next
-time, the run may be silent, as a lot of the information is cached in
-~/.tex.cache
-
-"""
-
+#import pdb
 import sys
 from os.path import expandvars
-from subprocess import check_output
 import matplotlib
 matplotlib.use('pdf')
 
@@ -34,7 +22,7 @@ argc = len(argvs) # number of command line arguments
 print argvs
 print argc
 print
-if (argc != 3):
+if (argc < 3):
     print 'errror: two command line arguments are needed.'
     print 'Usage: # python %s library_name matrix_name' % argvs[0]
     quit()
@@ -60,21 +48,33 @@ filename_output_fig = filename_output + ".pdf"
 
 fp_output = open(filename_output_txt, "w")
 
-num_step = 10
-nums = range(num_step, max_num_dim+1, num_step)
-
+nums = [];
+#range(num_step, max_num_dim+1, num_step)
 times = [];
 iters = [];
 
-for num in nums:
-    print "dim=", num
-    output = check_output(["mpirun", "-np", str(num_procs), run_filename, library_type, str(num) ])
-    for line in output.split('\n'):
+count = 0;
+for filename_input in argvs[3:-1]:
+#for num in nums:
+#print "dim=", num
+    print "filename_input=", filename_input
+    fp_input = open(expandvars(filename_input), "r")
+    for line in fp_input.read().split('\n'):
         items = line.split(' ')
+        #items = items_before.split(' ')
+        print "items=", items
+        print "items[0]=", items[0]
+        #items = line.split(' ')
+        if items[0] == "num_procs":
+            print "num_procs.append: ",items[2]
+            nums.append(int(items[2]))
+            
         if items[0] == "time":
             print "times.append: ",items[2]
             times.append(float(items[2]))
-            fp_output.write(str(num) + "  " + items[2] + '\n')
+            fp_output.write(str(nums[-1]) + "  " + str(times[-1]) + '\n')
+    count = count + 1
+    fp_input.close()
 
 fp_output.close()
 
