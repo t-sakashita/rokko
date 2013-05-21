596dda32027def6c1bc864fa722254bc8e1f9e8a
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-05-21 21:09:04 +0900

    modified rokko_build_script/build_sample_scalapack_intel+mkl_kashiwa.sh to use blacs_sgimpt

--- rokko_build_script/build_sample_scalapack_intel+mkl_kashiwa.sh
+++ rokko_build_script/build_sample_scalapack_intel+mkl_kashiwa.sh
@@ -7,7 +7,7 @@ cmake $WORK/development/rokko/ \
 -DCMAKE_INSTALL_PREFIX="$WORK/lib/rokko" \
 -DMPI_C_INCLUDE_PATH="/usr/include" -DMPI_CXX_INCLUDE_PATH="/usr/include" \
 -DMPI_C_LIBRARIES="-lmpi" -DMPI_CXX_LIBRARIES="-lmpi++;-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
--DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -lifcore -mkl=parallel" \
+-DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_sgimpt_lp64 -lifcore -mkl=parallel" \
 -DCMAKE_BUILD_TYPE=Debug \
 -DBOOST_INCLUDE_DIR="/opt/nano/alps/boost_1_52_0/"
 
