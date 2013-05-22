ff7105b7132a8ad60cd457619b08ba7034dd1dc7
u00306 <u00306@psi.issp.u-tokyo.ac.jp>
2013-05-22 18:46:02 +0900

    added build.conf build-rokko.conf for ISSP psi

---
+++ rokko_build_script/build_benchmark_intel+mkl_psi_release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+rm -R ../../build/rokko/*
+
+#-lmkl_blacs_lp64
+cmake ~/development/rokko/ \
+-DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+-DCMAKE_CXX_COMPILER="mpicxx" -DCMAKE_CXX_COMPILER="mpicc" -DCMAKE_Fortran_COMPILER="mpif90" \
+-DCMAKE_BUILD_TYPE=Release
+
+make frank_matrix VERBOSE=1
+
+make diagonalize_scalapack VERBOSE=1
