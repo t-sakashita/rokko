d161febbfb45d71c21609a27937570ce9c559cde
Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-06-04 20:11:16 +0900

    added wrapper for Eigen_exa

---
+++ rokko/eigen_exa/core.hpp
@@ -0,0 +1,56 @@
+#ifndef ROKKO_EIGEN_EXA_CORE_HPP
+#define ROKKO_EIGEN_EXA_CORE_HPP
+
+#include <rokko/eigen_exa/eigen_exa.hpp>
+#include <rokko/eigen_exa/diagonalize.hpp>
+
+namespace rokko {
+namespace eigen_exa {
+
+class solver {
+public:
+  void initialize(int& argc, char**& argv) {}
+
+  void finalize() {}
+
+  template<typename MATRIX_MAJOR>
+  void optimized_matrix_size(distributed_matrix<MATRIX_MAJOR>& mat) {
+    int n = mat.get_m_global();
+
+    // calculate sizes of my proc's local part of distributed matrix
+    int NPROW = mat.g.get_nprow();
+    int NPCOL = mat.g.get_npcol();
+
+    int n1 = ((n-1)/NPROW+1);
+    int nm;
+    int i1 = 6, i2 = 16*4, i3 = 16*4*2; 
+    CSTAB_get_optdim( n1, i1, i2, i3, nm );
+      
+    int NB  = 64;
+    int nmz = ((n-1)/NPROW+1);
+    nmz = ((nmz-1)/NB+1)*NB+1;
+    int nmw = ((n-1)/NPCOL+1);
+    nmw = ((nmw-1)/NB+1)*NB+1;
+
+    int larray = std::max(nmz, nm)*nmw;
+
+    mat.set_lld(nm);
+    mat.set_length_array(larray);
+    mat.set_block_size(1, 1);
+    int m_local = mat.calculate_row_size();
+    int n_local = mat.calculate_col_size();
+    mat.set_local_size(m_local, n_local);
+  }
+
+  template<typename MATRIX_MAJOR>
+  void diagonalize(distributed_matrix<MATRIX_MAJOR>& mat, localized_vector& eigvals,
+                   distributed_matrix<MATRIX_MAJOR>& eigvecs, timer& timer_in) {
+    rokko::eigen_exa::diagonalize(mat, eigvals, eigvecs, timer_in);
+  }
+};
+
+} // namespace eigen_exa
+} // namespace rokko
+
+#endif // ROKKO_EIGEN_EXA_CORE_HPP
+
---
+++ rokko/eigen_exa/diagonalize.hpp
@@ -0,0 +1,44 @@
+#ifndef ROKKO_EIGEN_EXA_DIAGONALIZE_HPP
+#define ROKKO_EIGEN_EXA_DIAGONALIZE_HPP
+
+#include <mpi.h>
+#include <rokko/distributed_matrix.hpp>
+#include <rokko/localized_vector.hpp>
+#include <rokko/eigen_exa/eigen_exa.hpp>
+
+namespace rokko {
+namespace eigen_exa {
+
+template <typename MATRIX_MAJOR>
+void diagonalize(rokko::distributed_matrix<MATRIX_MAJOR>& mat, double* eigvals, rokko::distributed_matrix<MATRIX_MAJOR>& eigvecs, timer& timer_in) {
+  //if(mat.g.is_row_major()) throw "eigen_sx doesn't support grid_row_major.  Use eigen_sx with grid_col_major.";
+  if(mat.is_row_major()) throw "eigen_sx doesn't support matrix_row_major.  Use eigen_sx with matrix_col_major.";
+
+  int m = 32;  // block_size
+
+  int dim = mat.get_m_global();
+  int lld = mat.get_lld();
+  double* mat_array = mat.get_array_pointer();
+  double* eigvecs_array= eigvecs.get_array_pointer();
+
+  int m_forward = 48;
+  int m_backward = 128;
+
+  timer_in.start(1);  
+  eigen_sx(dim, dim, mat_array, lld, eigvals, eigvecs_array, lld, m_forward, m_backward);
+  timer_in.stop(1);
+
+}
+
+template<typename MATRIX_MAJOR>
+void diagonalize(rokko::distributed_matrix<MATRIX_MAJOR>& mat, localized_vector& eigvals, rokko::distributed_matrix<MATRIX_MAJOR>& eigvecs, timer& timer_in) {
+  //if(mat.g.is_row_major()) throw "eigen_sx doesn't support grid_row_major.  Use eigen_sx with grid_col_major.";
+  if(mat.is_row_major()) throw "eigen_sx doesn't support matrix_row_major.  Use eigen_sx with matrix_col_major.";
+
+  diagonalize(mat, &eigvals[0], eigvecs, timer_in);
+}
+
+} // namespace eigen_exa
+} // namespace rokko
+
+#endif // ROKKO_EIGEN_EXA_DIAGONALIZE_HPP
