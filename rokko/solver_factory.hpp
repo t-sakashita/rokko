19244ef8bc92c08d2ede92fbb5f9b4b8e17bda33
Ryo IGARASHI <rigarash@gmail.com>
2013-06-07 12:59:16 +0900

    Fix copyright statements.

--- rokko/solver_factory.hpp
+++ rokko/solver_factory.hpp
@@ -3,7 +3,8 @@
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
 * Copyright (C) 2012-2013 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
-*                            Synge Todo <wistaria@comp-phys.org>
+*                            Synge Todo <wistaria@comp-phys.org>,
+*               2013-2013    Ryo IGARASHI <rigarash@issp.u-tokyo.ac.jp>
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
