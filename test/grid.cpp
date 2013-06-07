8cf6602d828cbf89469d1c7000508bbe3558b967
Ryo IGARASHI <rigarash@gmail.com>
2013-06-07 14:28:26 +0900

    Add grid test.

---
+++ test/grid.cpp
@@ -0,0 +1,32 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2013-2013 by Ryo IGARASHI <rigarash@issp.u-tokyo.ac.jp>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <boost/shared_ptr.hpp>
+#include <boost/foreach.hpp>
+
+#include <rokko/grid.hpp>
+
+#define BOOST_TEST_MODULE test_grid
+#ifndef BOOST_TEST_DYN_LINK
+#include <boost/test/included/unit_test.hpp>
+#else
+#include <boost/test/unit_test.hpp>
+#endif
+
+BOOST_AUTO_TEST_CASE(test_grid) {
+    MPI_Init(&boost::unit_test::framework::master_test_suite().argc,
+             &boost::unit_test::framework::master_test_suite().argv);
+    MPI_Comm comm = MPI_COMM_WORLD;
+    rokko::grid<rokko::grid_col_major> g(comm);
+    BOOST_CHECK_EQUAL(0, g.get_myrank());
+    BOOST_CHECK_EQUAL(1, g.get_nprocs());
+    MPI_Finalize();
+}
