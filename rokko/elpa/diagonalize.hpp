41dd3bcc9d841722e1c3be41f975562b61fb2497
Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-06-11 13:36:06 +0900

    fixed elpa/diagonalize.hpp

--- rokko/elpa/diagonalize.hpp
+++ rokko/elpa/diagonalize.hpp
@@ -43,18 +43,22 @@ int diagonalize(distributed_matrix<MATRIX_MAJOR>& mat, double* eigvals, distribu
 
   std::cout << "before: get_elpa_row_col_comms_wrap" << std::endl;
 
-  //get_elpa_row_col_comms_wrap_(&comm_f, nprow, npcol,
-  //			  &mpi_comm_rows_f, &mpi_comm_cols_f);
+  get_elpa_row_col_comms_wrap_(&comm_f, nprow, npcol,
+			       &mpi_comm_rows_f, &mpi_comm_cols_f);
  
-  int nblk = mat.get_nb();
+  int nblk = 5; //mat.get_nb();
+
+  double* mat_array = mat.get_array_pointer();
+  double* eigvecs_array = eigvecs.get_array_pointer();
 
   std::cout << "before: solve_evp_real_wrap_" << std::endl;
 
   // 固有値分解
   timer_in.start(1);
-  solve_evp_real_wrap2_(nprow, npcol, dim, dim, mat.get_array_pointer(), dim, eigvals, eigvecs.get_array_pointer(), dim, nblk);    
-  //solve_evp_real_wrap_(dim, dim, mat.get_array_pointer(), dim, eigvals, eigvecs.get_array_pointer(), dim, nblk,
-  //		 mpi_comm_rows_f, mpi_comm_cols_f);
+  //solve_evp_real_wrap2_(nprow, npcol, dim, dim, mat.get_array_pointer(), dim, eigvals, eigvecs.get_array_pointer(), dim, nblk);    
+  solve_evp_real_wrap_(dim, dim, mat_array, mat.get_m_local(), eigvals, eigvecs_array, eigvecs.get_m_local(), nblk,
+		       &mpi_comm_rows_f, &mpi_comm_cols_f);
+  std::cout << "after: solve_evp_real_wrap_" << std::endl;
   timer_in.stop(1);
 
   if (info) {
