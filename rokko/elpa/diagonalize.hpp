7f139ebfa5da4232bb0ee2269cb4022385cee4a8
wistaria <synge.todo@gmail.com>
2013-06-13 20:00:17 +0900

    Fixed interface of elpa library

--- rokko/elpa/diagonalize.hpp
+++ rokko/elpa/diagonalize.hpp
@@ -14,16 +14,22 @@ template<typename MATRIX_MAJOR>
 int diagonalize(distributed_matrix<MATRIX_MAJOR>& mat, double* eigvals, distributed_matrix<MATRIX_MAJOR>& eigvecs, timer& timer_in) {
   int dim = mat.get_m_global();
 
-  MPI_Fint comm_f = MPI_COMM_WORLD;
-  MPI_Fint mpi_comm_rows_f, mpi_comm_cols_f;
-
-  get_elpa_row_col_comms_wrap_(comm_f, mat.get_grid().get_myrow(), mat.get_grid().get_mycol(),
-  			       mpi_comm_rows_f, mpi_comm_cols_f);
+  MPI_Fint comm_f = MPI_Comm_c2f(mat.get_grid().get_comm());
+  MPI_Fint comm_rows_f, comm_cols_f;
+  int myrow = mat.get_grid().get_myrow();
+  int mycol = mat.get_grid().get_mycol();
+  
+  get_elpa_row_col_comms_wrap_(&comm_f, &myrow, &mycol, &comm_rows_f, &comm_cols_f);
 
   // call eigenvalue routine
   timer_in.start(1);
-  solve_evp_real_wrap_(dim, dim, mat.get_array_pointer(), mat.get_lld(), eigvals, eigvecs.get_array_pointer(), eigvecs.get_lld(), mat.get_mb(),
-  		       mpi_comm_rows_f, mpi_comm_cols_f);
+  int mat_lld = mat.get_lld();
+  int mat_mb = mat.get_mb();
+  int eigvecs_lld = eigvecs.get_lld();
+  
+  solve_evp_real_wrap_(&dim, &dim, mat.get_array_pointer(), &mat_lld, eigvals,
+                       eigvecs.get_array_pointer(), &eigvecs_lld, &mat_mb,
+                       &comm_rows_f, &comm_cols_f);
   timer_in.stop(1);
 }
 
--- rokko/elpa/elpa.hpp
+++ rokko/elpa/elpa.hpp
@@ -4,9 +4,13 @@
 #include "mpi.h"
 
 extern "C" {
-  void get_elpa_row_col_comms_wrap_(const MPI_Comm& mpi_comm_world, const int& my_prow, const int& my_pcol, MPI_Fint& mpi_comm_rows, MPI_Fint& mpi_comm_cols);
 
-  void solve_evp_real_wrap_(const int& na, const int& nev, double* a, const int& na_rows, double* ev, double* z, const int& nz_rows, const int& nblk, const MPI_Fint& mpi_comm_rows, const MPI_Fint& mpi_comm_cols);
+void get_elpa_row_col_comms_wrap_(MPI_Fint* comm, int* my_prow, int* my_pcol, MPI_Fint* comm_rows,
+                                  MPI_Fint* comm_cols);
+
+void solve_evp_real_wrap_(int* na, int* nev, double* a, int* na_rows, double* ev, double* z,
+                          int* nz_rows, int* nblk, MPI_Fint* comm_rows, MPI_Fint* comm_cols);
+
 }
 
 #endif // ROKKO_ELPA_HPP
