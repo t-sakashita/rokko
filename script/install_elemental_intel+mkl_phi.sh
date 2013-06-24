d050893b7fd9c5608a7ce83bbb9f7d6f913ef560
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-06-24 18:49:31 +0900

    modified script/install_elemental_intel+mkl_phi.sh for elemental-0.80

--- script/install_elemental_intel+mkl_phi.sh
+++ script/install_elemental_intel+mkl_phi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
 PREFIX="$1"
-test -z "$PREFIX" && PREFIX="/opt/nano/rokko"
+test -z "$PREFIX" && PREFIX="~/opt/nano/rokko"
 echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build && cd $HOME/build
-rm -rf elemental-0.79-p1 elemental-0.79-p1-build
-wget -O - http://elemental.googlecode.com/files/elemental-0.79-p1.tgz | tar zxf -
-mkdir -p elemental-0.79-p1-build && cd elemental-0.79-p1-build
-cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 -DMATH_LIBS="-mkl=parallel;-lifcore" -DIFCORE_LIB="-lifcore" -DSHARED_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX=$PREFIX $HOME/build/elemental-0.79-p1
+rm -rf elemental-0.80 elemental-0.80-build
+wget -O - http://elemental.googlecode.com/files/elemental-0.80.tgz | tar zxf -
+mkdir -p elemental-0.80-build && cd elemental-0.80-build
+cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 -DMATH_LIBS="-mkl=parallel;-lifcore" -DIFCORE_LIB="-lifcore" -DSHARED_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX=$PREFIX $HOME/build/elemental-0.80
 
 make -j4 all
 make install
