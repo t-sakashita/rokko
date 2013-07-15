22290bf6362789a7cc3925c862cdd28cada59a2c
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-07-15 18:23:30 +0900

    modified install script for petsc-3.4.2 and slepc-3.4.0

--- script/install_petsc_linux_intel+mkl_phi.sh
+++ script/install_petsc_linux_intel+mkl_phi.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -x
 
-#export PETSC_ARCH=arch-installed-petsc
-
 PREFIX="$1"
-#test -z "$PREFIX" && PREFIX="${HOME}/lib/petsc-3.3-p6_installed/${PETSC_ARCH}"
-test -z "$PREFIX" && PREFIX="${HOME}/lib/petsc-3.3-p6_installed"
+#test -z "$PREFIX" && PREFIX="${HOME}/lib/petsc-3.4.2_installed/${PETSC_ARCH}"
+test -z "$PREFIX" && PREFIX="${HOME}/rokko_lib"
 echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf petsc-3.3-p6
-wget -O - http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p6.tar.gz | tar zxf -
+rm -rf petsc-3.4.2
+wget -O - http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.2.tar.gz | tar zxf -
 
-cd petsc-3.3-p6
-#export PETSC_DIR=
+cd petsc-3.4.2
 unset PETSC_DIR
 
-./configure --prefix="$PREFIX" --with-cc="mpicc" --COPTFLAGS="-O3" --CXXOPTFLAGS="-O3" --FOPTFLAGS=-O3 --with-cxx="mpicxx" --with-fc="mpif90" --with-mpiexec="mpiexec" --with-blas-lapack-lib="-mkl=sequential" --with-c++-support=1 --with-debugging=0
-make -j4
+./configure --prefix="$PREFIX" --with-cc="mpicc" --COPTFLAGS="-O3" --CXXOPTFLAGS="-O3" --FOPTFLAGS=-O3 --with-cxx="mpicxx" --with-fc="mpif90" --with-mpiexec="mpiexec" --with-blas-lapack-dir=/home/issp/intel/composer_xe_2013.4.183/mkl/bin/intel64 --with-c++-support=1 --with-debugging=0
+make
 make install
+# You can't use -j4 option no longer in make, SLEPc automatically sets the number of parallelization.
