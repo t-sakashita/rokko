e02933d2135d9db47a401b2a380aa31fb652801c
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-08-02 15:40:49 +0900

    install_slepc_linux_intel+mkl_phi.sh for slepc-3.4.1

--- script/install_slepc_linux_intel+mkl_phi.sh
+++ script/install_slepc_linux_intel+mkl_phi.sh
@@ -6,10 +6,10 @@ echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf slepc-3.4.0
-wget -O - http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.4.0.tar.gz | tar zxf -
+rm -rf slepc-3.4.1
+wget -O - http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.4.1.tar.gz | tar zxf -
 
-cd slepc-3.4.0
+cd slepc-3.4.1
 export SLEPC_DIR=$PWD
 export PETSC_DIR=$PREFIX  #$HOME/build/petsc-3.4.2  #  /arch-linux2-c-opt
 unset PETSC_ARCH
