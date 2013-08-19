f439aa50d1d9dd11435ea5463081e5fc92adad74
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2013-08-19 18:20:32 +0900

    fixed order of loops in rokko/utility/spin_hamiltonian.hpp

--- rokko/utility/spin_hamiltonian.hpp
+++ rokko/utility/spin_hamiltonian.hpp
@@ -22,24 +22,21 @@ namespace spin_hamiltonian {
 
 void multiply(int L, std::vector<std::pair<int, int> >& lattice, const double* v, double* w) {
   int N = 1 << L;
-  for (int k=0; k<N; ++k) {
-    for (int l=0; l<lattice.size(); ++l) {
-      int i = lattice[l].first;
-      int j = lattice[l].second;
-      //cout << "k=" << k << " i=" << i << " j=" << j << endl;
-      int m1 = 1 << i;
-      int m2 = 1 << j;
-      int m3 = m1 + m2;
+  for (int l=0; l<lattice.size(); ++l) {
+    int i = lattice[l].first;
+    int j = lattice[l].second;
+    int m1 = 1 << i;
+    int m2 = 1 << j;
+    int m3 = m1 + m2;
+    for (int k=0; k<N; ++k) {
       if (((k & m3) == m1)) {  // when (bit i == 1, bit j == 0)
         w[k] += 0.5 * v[k^m3] - 0.25 * v[k];
-        //cout << "if" << endl;
       }
       else if ((k & m3) == m2) { // when (bit i == 0, bit j == 1)
         w[k] += 0.5 * v[k^m3] - 0.25 * v[k];
       }
       else {
         w[k] += 0.25 * v[k];
-        //      cout << "else" << endl;
       }
     }
   }
@@ -53,13 +50,15 @@ void fill_diagonal(int L, std::vector<std::pair<int, int> >& lattice, double* w)
   int N = 1 << L;
   for (int k=0; k<N; ++k) {
     w[k] = 0;
-    for (int l=0; l<lattice.size(); ++l) {
-      int i = lattice[l].first;
-      int j = lattice[l].second;
-      //cout << "k=" << k << " i=" << i << " j=" << j << endl;
-      int m1 = 1 << i;
-      int m2 = 1 << j;
-      int m3 = m1 + m2;
+  }
+
+  for (int l=0; l<lattice.size(); ++l) {
+    int i = lattice[l].first;
+    int j = lattice[l].second;
+    int m1 = 1 << i;
+    int m2 = 1 << j;
+    int m3 = m1 + m2;
+    for (int k=0; k<N; ++k) {
       if (((k & m3) == m1)) {  // when (bit i == 1, bit j == 0) or (bit i == 0, bit j == 1)
         w[k] -= 0.25;
         //cout << "if" << endl;
@@ -83,15 +82,14 @@ template <typename MATRIX_MAJOR>
 void generate(int L, std::vector<std::pair<int, int> >& lattice, rokko::localized_matrix<MATRIX_MAJOR>& mat) {
   mat.setZero();
   int N = 1 << L;
-  for (int k1=0; k1<N; ++k1) {
-    for (int k2=0; k2<N; ++k2) {
-      for (int l=0; l<lattice.size(); ++l) {
-        int i = lattice[l].first;
-        int j = lattice[l].second;
-        //cout << "k=" << k << " i=" << i << " j=" << j << endl;
-        int m1 = 1 << i;
-        int m2 = 1 << j;
-        int m3 = m1 + m2;
+  for (int l=0; l<lattice.size(); ++l) {
+    int i = lattice[l].first;
+    int j = lattice[l].second;
+    int m1 = 1 << i;
+    int m2 = 1 << j;
+    int m3 = m1 + m2;
+    for (int k1=0; k1<N; ++k1) {
+      for (int k2=0; k2<N; ++k2) {
         if (((k2 & m3) == m1) || ((k2 & m3) == m2)) {  // when (bit i == 1, bit j == 0) or (bit i == 0, bit j == 1)
           if (k1 == (k2^m3)) {
             mat(k1, k2) += 0.5;
