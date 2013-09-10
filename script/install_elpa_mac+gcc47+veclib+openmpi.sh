b79bc47a0ffc200265d76c6a419c7c240bd2fc05
Synge Todo <synge.todo@gmail.com>
2013-09-10 16:47:02 +0900

    Updated install script of elpa for mac

--- script/install_elpa_mac+gcc47+veclib+openmpi.sh
+++ script/install_elpa_mac+gcc47+veclib+openmpi.sh
@@ -8,14 +8,14 @@ SCRIPT_DIR=`cd $dir && pwd`
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf elpa_lib-201305
+rm -rf elpa_lib-201305 elpa_lib-201305-build
 tar zxf $HOME/source/elpa_lib-201305.tar.gz
 
 cd $HOME/build/elpa_lib-201305
-patch -p1 < $SCRIPT_DIR/elpa-cmake.patch
+patch -p1 < $SCRIPT_DIR/elpa_lib-201305-cmake.patch
 
 cd $HOME/build
-rm -rf elpa_lib-201305-build && mkdir -p elpa_lib-201305-build && cd elpa_lib-201305-build
+mkdir -p elpa_lib-201305-build && cd elpa_lib-201305-build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 -DCMAKE_Fortran_FLAGS="-O3 -ffree-line-length-none" -DSCALAPACK_LIB="-L$PREFIX/lib -lscalapack -Wl,-framework -Wl,vecLib" $HOME/build/elpa_lib-201305
 
 make -j4
