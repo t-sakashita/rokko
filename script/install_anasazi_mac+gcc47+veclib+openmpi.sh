ebdfa0039f356f876b988221efbdc6b63f3394eb
Synge Todo <synge.todo@gmail.com>
2013-09-21 09:15:15 +0900

    Updated install script for Mac OS X

--- script/install_anasazi_mac+gcc47+veclib+openmpi.sh
+++ script/install_anasazi_mac+gcc47+veclib+openmpi.sh
@@ -6,13 +6,14 @@ echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf trilinos-11.4.1-Source
+rm -rf trilinos-11.4.1-Source trilinos-11.4.1-Source-build
 tar jxf $HOME/source/trilinos-11.4.1-Source.tar.bz2
 
 cd $HOME/build
 mkdir -p trilinos-11.4.1-Source-build && cd trilinos-11.4.1-Source-build
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF \
     -DTPL_ENABLE_MPI=ON -DMPI_USE_COMPILER_WRAPPERS=ON \
     -DCMAKE_Fortran_COMPILER="openmpif90" -DCMAKE_CXX_COMPILER="openmpicxx" -DCMAKE_C_COMPILER="openmpicc" \
     -DTPL_BLAS_LIBRARIES="-framework veclib" \
--- script/install_eigen_exa_mac+gcc47+veclib+openmpi.sh
+++ script/install_eigen_exa_mac+gcc47+veclib+openmpi.sh
@@ -8,15 +8,18 @@ SCRIPT_DIR=`cd $dir && pwd`
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf EigenExa-1.0-RC5
-tar zxf $HOME/source/EigenExa-1.0-RC5.tgz
-
-cd $HOME/build/EigenExa-1.0-RC5
-patch -p1 < $SCRIPT_DIR/EigenExa-1.0-RC5.patch
+rm -rf EigenExa-1.3
+if test -f $HOME/source/EigenExa-1.3.tgz; then
+  tar zxf $HOME/source/EigenExa-1.3.tgz
+else
+  wget -O - http://www.aics.riken.jp/labs/lpnctrt/EigenExa-1.3.tgz | tar zxf -
+fi
+cd $HOME/build/EigenExa-1.3
+patch -p1 < $SCRIPT_DIR/EigenExa-1.3.patch
 
 cd $HOME/build
-rm -rf EigenExa-1.0-RC5-build && mkdir -p EigenExa-1.0-RC5-build && cd EigenExa-1.0-RC5-build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_CXX_COMPILER=openmpicxx -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 -DSCALAPACK_LIB="-L$PREFIX/lib -lscalapack -Wl,-framework -Wl,vecLib" $HOME/build/EigenExa-1.0-RC5
+rm -rf EigenExa-1.3-build && mkdir -p EigenExa-1.3-build && cd EigenExa-1.3-build
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_CXX_COMPILER=openmpicxx -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 -DSCALAPACK_LIB="-L$PREFIX/lib -lscalapack -Wl,-framework -Wl,vecLib" $HOME/build/EigenExa-1.3
 
 make -j4
 make install
