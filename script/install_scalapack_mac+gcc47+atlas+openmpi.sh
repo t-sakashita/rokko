3f0657307bef92fc50168e7e8b62bcc5d72526fa
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-09-21 13:40:15 +0900

    Updated scalapack install script for mac

--- script/install_scalapack_mac+gcc47+atlas+openmpi.sh
+++ script/install_scalapack_mac+gcc47+atlas+openmpi.sh
@@ -6,15 +6,20 @@ echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf scalapack-2.0.2
+rm -rf scalapack-2.0.2 scalapack-2.0.2-build
 if test -f $HOME/source/scalapack-2.0.2.tgz; then
   tar zxf $HOME/source/scalapack-2.0.2.tgz
 else
   wget -O - http://www.netlib.org/scalapack/scalapack-2.0.2.tgz | tar zxf -
 fi
 
-rm -rf scalapack-2.0.2-build && mkdir -p scalapack-2.0.2-build && cd scalapack-2.0.2-build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 -DLAPACK_LIBRARIES="-L/opt/local/lib -llapack -lptcblas -lptf77blas -latlas -lgfortran" -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_INSTALL_NAME_DIR=$PREFIX/lib $HOME/build/scalapack-2.0.2
+cd $HOME/build
+mkdir -p scalapack-2.0.2-build && cd scalapack-2.0.2-build
+cmake -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 \
+    -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF \
+    -DLAPACK_LIBRARIES="-L/opt/local/lib -llapack -lptcblas -lptf77blas -latlas -lgfortran" \
+    -DCMAKE_INSTALL_NAME_DIR=$PREFIX/lib \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX $HOME/build/scalapack-2.0.2
 
 make -j4
 make install
--- script/install_scalapack_mac+gcc47+veclib+openmpi.sh
+++ script/install_scalapack_mac+gcc47+veclib+openmpi.sh
@@ -15,7 +15,10 @@ fi
 
 cd $HOME/build
 mkdir -p scalapack-2.0.2-build && cd scalapack-2.0.2-build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_INSTALL_NAME_DIR=$PREFIX/lib $HOME/build/scalapack-2.0.2
+cmake -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 \
+    -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF \
+    -DCMAKE_INSTALL_NAME_DIR=$PREFIX/lib \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX $HOME/build/scalapack-2.0.2
 
 make -j4
 make install
