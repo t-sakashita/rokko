0913373107d83b0ffb10d8d492e2da2febc5bfdc
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-09-21 12:33:09 +0900

    Updated elemental-0.80 for fx10

--- script/install_elemental_fx10.sh
+++ script/install_elemental_fx10.sh
@@ -1,34 +1,31 @@
 #!/bin/bash -x
 
 PREFIX="$1"
-test -z "$PREFIX" && PREFIX=$HOME/rokko
+test -z "$PREFIX" && PREFIX=$HOME/opt/rokko
 echo "PREFIX = $PREFIX"
+dir=`dirname $0`
+SCRIPT_DIR=`cd $dir && pwd`
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf elemental-0.79-p1
-tar xvf $HOME/source/elemental-0.79-p1.tgz
-cd elemental-0.79-p1
-patch -p1 << EOF
---- elemental-0.79-p1.orig/CMakeLists.txt
-+++ elemental-0.79-p1/CMakeLists.txt
-@@ -185,7 +185,7 @@
-   set(HAVE_F90_INTERFACE FALSE)
-   if(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
-     include(FortranCInterface)
--    FortranCInterface_VERIFY(CXX)
-+# :   FortranCInterface_VERIFY(CXX)
-     if(FortranCInterface_VERIFIED_CXX)
-       set(HAVE_F90_INTERFACE TRUE)
-       FortranCInterface_HEADER(
-EOF
+rm -rf elemental-0.80 elemental-0.80-build
+if test -f $HOME/source/elemental-0.80.tgz; then
+  tar zxf $HOME/source/elemental-0.80.tgz
+else
+  wget -O - http://elemental.googlecode.com/files/elemental-0.80.tgz | tar zxf -
+fi
+
+cd $HOME/build/elemental-0.80
+patch -p1 < $SCRIPT_DIR/elemental-0.80.patch
+patch -p1 < $SCRIPT_DIR/elemental-0.80-fx10.patch
 
 cd $HOME/build
-rm -rf elemental-0.79-p1-build
-mkdir elemental-0.79-p1-build
-cd elemental-0.79-p1-build
+mkdir elemental-0.80-build && cd elemental-0.80-build
+cmake -DCMAKE_C_COMPILER=mpifccpx -DCMAKE_C_FLAGS="-Kfast -Xg -mt" \
+    -DCMAKE_CXX_COMPILER=mpiFCCpx -DCMAKE_CXX_FLAGS="-Kfast -Xg -mt" \
+    -DCMAKE_Fortran_COMPILER=mpifrtpx -DCMAKE_Fortran_FLAGS="-Kfast -mt" \
+    -DMATH_LIBS="-SSL2 --linkfortran" -DSHARED_LIBRARIES=ON \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX $HOME/build/elemental-0.80
 
-cmake -DCMAKE_C_COMPILER=mpifccpx -DCMAKE_C_FLAGS="-Kfast -Xg -mt" -DCMAKE_CXX_COMPILER=mpiFCCpx -DCMAKE_CXX_FLAGS="-Kfast -Xg -mt" -DCMAKE_Fortran_COMPILER=mpifrtpx -DCMAKE_Fortran_FLAGS="-Kfast -mt" -DMATH_LIBS="-SSL2 --linkfortran" -DSHARED_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX=$PREFIX $HOME/build/elemental-0.79-p1
-
-make -j8 2>&1 | tee make.log
+make -j4 all
 make install
