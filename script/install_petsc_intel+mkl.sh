69ad8d999482fe316b26c82a938fc422bea304c0
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-09-30 16:46:27 +0900

    Added install script of petsc and anasazi for intel+mkl

---
+++ script/install_petsc_intel+mkl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+PREFIX="$1"
+test -z "$PREFIX" && PREFIX=$HOME/opt/rokko
+echo "PREFIX = $PREFIX"
+
+mkdir -p $HOME/build
+cd $HOME/build
+rm -rf petsc-3.4.2
+if test -f $HOME/source/petsc-3.4.2.tar.gz; then
+  tar zxf $HOME/source/petsc-3.4.2.tar.gz
+else
+  wget -O - http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.2.tar.gz | tar zxf -
+fi
+
+cd $HOME/build/petsc-3.4.2
+unset PETSC_DIR
+./configure --prefix="$PREFIX" --with-cc="mpicc" --COPTFLAGS="-O3" --CXXOPTFLAGS="-O3" --FOPTFLAGS="-O3" --with-cxx="mpic++" --with-fc="mpif77" --with-mpiexec="mpiexec" \
+  --with-blas-lapack-dir=$MKLROOT/bin/intel64 --with-c++-support=1 --with-debugging=0
+
+make # -j option can not be specified
+make install
