79d5e84e631bc95f0061331071d3a0051d68be9a
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-09-30 20:49:47 +0900

    Updated cmake modules

--- config/FindScalapack.cmake
+++ config/FindScalapack.cmake
@@ -18,28 +18,53 @@ if(DEFINED SCALAPACK_LIB)
   return()
 endif(DEFINED SCALAPACK_LIB)
 
-# Standard search path
-set(_PATHS "")
-if(ROKKO_SOLVER_DIR)
-  list(APPEND _PATHS ${ROKKO_SOLVER_DIR})
-endif(ROKKO_SOLVER_DIR)
-list(APPEND _PATHS ${CMAKE_INSTALL_PREFIX} "/opt/nano/rokko" "/opt/rokko" "/opt" "$ENV{HOME}/opt/rokko" "$ENV{HOME}/opt")
+if(DEFINED MKL_INCLUDE_DIR)
 
-foreach (_PATH ${_PATHS})
-  list(APPEND _LIBPATHS "${_PATH}/lib")
-endforeach()
+  find_library(_SCALAPACK_LIBRARY
+    NAMES mkl_scalapack_lp64
+    PATHS $ENV{MKLROOT}/lib/intel64
+    DOC "The Scalapack library")
+  if(_SCALAPACK_LIBRARY)
+    list(APPEND SCALAPACK_LIBRARIES ${_SCALAPACK_LIBRARY})
+  else(_SCALAPACK_LIBRARY)
+    message(STATUS "Scalapack library: not found")
+    set(SCALAPACK_FOUND FALSE)
+    return()
+  endif(_SCALAPACK_LIBRARY)
+  find_library(_SCALAPACK_BLACS_LIBRARY
+    NAMES mkl_blacs_intelmpi_lp64
+    PATHS $ENV{MKLROOT}/lib/intel64
+    DOC "The BLACS library")
+  if(_SCALAPACK_BLACS_LIBRARY)
+    list(APPEND SCALAPACK_LIBRARIES ${_SCALAPACK_BLACS_LIBRARY})
+  endif(_SCALAPACK_BLACS_LIBRARY)
 
-find_library(_SCALAPACK_LIBRARY
-  NAMES scalapack
-  PATHS ${_LIBPATHS}
-  DOC "The Scalapack library")
-if(_SCALAPACK_LIBRARY)
-  list(APPEND SCALAPACK_LIBRARIES ${_SCALAPACK_LIBRARY})
-else(_SCALAPACK_LIBRARY)
-  message(STATUS "Scalapack library: not found")
-  set(SCALAPACK_FOUND FALSE)
-  return()
-endif(_SCALAPACK_LIBRARY)
+else(DEFINED MKL_INCLUDE_DIR)
+
+  # Standard search path
+  set(_PATHS "")
+  if(ROKKO_SOLVER_DIR)
+    list(APPEND _PATHS ${ROKKO_SOLVER_DIR})
+  endif(ROKKO_SOLVER_DIR)
+  list(APPEND _PATHS ${CMAKE_INSTALL_PREFIX} "/opt/nano/rokko" "/opt/rokko" "/opt" "$ENV{HOME}/opt/rokko" "$ENV{HOME}/opt")
+
+  foreach (_PATH ${_PATHS})
+    list(APPEND _LIBPATHS "${_PATH}/lib")
+  endforeach()
+
+  find_library(_SCALAPACK_LIBRARY
+    NAMES scalapack
+    PATHS ${_LIBPATHS}
+    DOC "The Scalapack library")
+  if(_SCALAPACK_LIBRARY)
+    list(APPEND SCALAPACK_LIBRARIES ${_SCALAPACK_LIBRARY})
+  else(_SCALAPACK_LIBRARY)
+    message(STATUS "Scalapack library: not found")
+    set(SCALAPACK_FOUND FALSE)
+    return()
+  endif(_SCALAPACK_LIBRARY)
+
+endif(DEFINED MKL_INCLUDE_DIR)
 
 set(SCALAPACK_FOUND TRUE)
 message(STATUS "Scalapack libraries: ${SCALAPACK_LIBRARIES}")
