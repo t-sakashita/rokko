09b137f30bc324fea509f824ccb10798c1584b30
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-10-01 16:26:25 +0900

    Fixed CMakeFile.txt

--- script/install_eigen_exa_mac+gcc47+veclib+openmpi.sh
+++ script/install_eigen_exa_mac+gcc47+veclib+openmpi.sh
@@ -20,7 +20,7 @@ patch -p1 < $SCRIPT_DIR/EigenExa-1.3.patch
 cd $HOME/build
 rm -rf EigenExa-1.3-build && mkdir -p EigenExa-1.3-build && cd EigenExa-1.3-build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_CXX_COMPILER=openmpicxx -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 \
+    -DCMAKE_C_COMPILER=openmpicc -DCMAKE_Fortran_COMPILER=openmpif90 \
     -DSCALAPACK_LIB="-L$PREFIX/lib -lscalapack -Wl,-framework -Wl,vecLib" \
     $HOME/build/EigenExa-1.3
 
