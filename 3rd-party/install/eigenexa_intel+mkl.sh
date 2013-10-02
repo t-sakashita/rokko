0c23d2acc2faaf7b1a6e6d11db793a96cb87d083
Synge Todo <synge.todo@gmail.com>
2013-10-02 11:06:16 +0900

    Added debug mode

--- 3rd-party/install/eigenexa_intel+mkl.sh
+++ 3rd-party/install/eigenexa_intel+mkl.sh
@@ -3,12 +3,15 @@
 PREFIX="$1"
 test -z "$PREFIX" && PREFIX=$HOME/opt/rokko
 echo "PREFIX = $PREFIX"
+DEBUG="$2"
+test -z "$DEBUG" && DEBUG=0
+echo "PREFIX = $PREFIX"
 dir=`dirname $0`
 SCRIPT_DIR=`cd $dir && pwd`
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf EigenExa-1.3
+rm -rf EigenExa-1.3 EigenExa-1.3-build
 if test -f $HOME/source/EigenExa-1.3.tgz; then
   tar zxf $HOME/source/EigenExa-1.3.tgz
 else
@@ -18,21 +21,26 @@ cd $HOME/build/EigenExa-1.3
 patch -p1 < $SCRIPT_DIR/EigenExa-1.3.patch
 
 cd $HOME/build
-rm -rf EigenExa-1.3-build && mkdir -p EigenExa-1.3-build && cd EigenExa-1.3-build
-if [ `which mpicxx > /dev/null 2>&1; echo $?` = 0 ]; then
-  cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
-      -DCMAKE_C_FLAGS="-O3 -xSSE3" -DCMAKE_Fortran_FLAGS="-O3 -xSSE3" \
-      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-      $HOME/build/EigenExa-1.3
+mkdir -p EigenExa-1.3-build && cd EigenExa-1.3-build
+if [ "$DEBUG" = "0" ]; then
+    OPTFLAGS="-O3 -xSSE3"
 else
-  cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_C_COMPILER=icc -DCMAKE_Fortran_COMPILER=ifort \
-      -DCMAKE_C_FLAGS="-O3 -xSSE3" -DCMAKE_Fortran_FLAGS="-O3 -xSSE3" \
-      -DMPI_C_INCLUDE_PATH="/usr/include" \
-      -DMPI_C_LIBRARIES="-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
-      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-      $HOME/build/EigenExa-1.3
+    OPTFLAGS="-O0 -g"
+fi
+if [ `which mpicxx > /dev/null 2>&1; echo $?` = 0 ]; then
+    cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+	-DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
+	-DCMAKE_C_FLAGS="$OPTFLAGS" -DCMAKE_Fortran_FLAGS="$OPTFLAGS" \
+	-DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+	$HOME/build/EigenExa-1.3
+else
+    cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+	-DCMAKE_C_COMPILER=icc -DCMAKE_Fortran_COMPILER=ifort \
+	-DCMAKE_C_FLAGS="$OPTFLAGS" -DCMAKE_Fortran_FLAGS="$OPTFLAGS" \
+	-DMPI_C_INCLUDE_PATH="/usr/include" \
+	-DMPI_C_LIBRARIES="-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
+	-DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+	$HOME/build/EigenExa-1.3
 fi
 make -j4 VERBOSE=1
 make install
