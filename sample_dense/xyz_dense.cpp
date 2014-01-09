bbbd20a84d7f5535282fae82dfcb145766fb85d3
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2014-01-10 01:08:47 +0900

    Clean up unused lines

--- sample_dense/xyz_dense.cpp
+++ sample_dense/xyz_dense.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2013 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
+* Copyright (C) 2012-2014 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
 *                            Synge Todo <wistaria@comp-phys.org>
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -12,32 +12,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <boost/tuple/tuple.hpp>
 
 #include <rokko/serial_solver.hpp>
 #include <rokko/localized_matrix.hpp>
 #include <rokko/localized_vector.hpp>
-
-#include <rokko/collective.hpp>
-
-#include <boost/tuple/tuple.hpp>
-
 #include <rokko/utility/xyz_hamiltonian.hpp>
 #include <rokko/utility/sort_eigenpairs.hpp>
 
-#include <boost/lexical_cast.hpp>
-
 int main(int argc, char *argv[]) {
-  //typedef rokko::matrix_row_major matrix_major;
   typedef rokko::matrix_col_major matrix_major;
 
   if (argc <= 2) {
-    std::cerr << "error: " << argv[0] << " solver_name matrix_size" << std::endl;
+    std::cerr << "error: " << argv[0] << " solver_name lattice_file" << std::endl;
     exit(34);
   }
-
   std::string solver_name(argv[1]);
-
-  std::ifstream ifs(argv[2]); //str);
+  std::ifstream ifs(argv[2]);
   if (!ifs) {
     std::cout << "can't open file" << std::endl;
     exit(1);
@@ -93,15 +84,13 @@ int main(int argc, char *argv[]) {
   
   std::cout.precision(3);
   std::cout << "Check the orthogonality of Eigenvectors:" << std::endl
-            << eigvec_sorted * eigvec_sorted.transpose() << std::endl;   // Is it equal to indentity matrix?
-  //<< eigvecs.transpose() * eigvecs << std::endl;   // Is it equal to indentity matrix?
+            << eigvec_sorted * eigvec_sorted.transpose() << std::endl;
   
   rokko::xyz_hamiltonian::generate(L, lattice, coupling, mat);
   std::cout << "residual := A x - lambda x = " << std::endl
          << mat * eigvec_sorted.col(1)  -  eigval_sorted(1) * eigvec_sorted.col(1) << std::endl;
   std::cout << "Are all the following values equal to some eigenvalue = " << std::endl
             << (mat * eigvec_sorted.col(0)).array() / eigvec_sorted.col(0).array() << std::endl;
-  //cout << "lmat=" << std::endl << lmat << std::endl;
 
   solver.finalize();
   return 0;
