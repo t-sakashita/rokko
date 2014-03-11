60136193369f89d0ca03937c3522b81de74a52b1
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-03-11 09:29:32 +0900

    Clean up frank_dense output

--- sample_dense/frank_dense.cpp
+++ sample_dense/frank_dense.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2013 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
+* Copyright (C) 2012-2014 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
 *                            Synge Todo <wistaria@comp-phys.org>
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -11,68 +11,58 @@
 *****************************************************************************/
 
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
 #include <rokko/serial_solver.hpp>
 #include <rokko/localized_matrix.hpp>
 #include <rokko/localized_vector.hpp>
-
-#include <rokko/collective.hpp>
-
 #include <rokko/utility/frank_matrix.hpp>
 #include <rokko/utility/sort_eigenpairs.hpp>
 
-#include <boost/lexical_cast.hpp>
+typedef rokko::matrix_col_major matrix_major;
 
 int main(int argc, char *argv[]) {
-  //typedef rokko::matrix_row_major matrix_major;
-  typedef rokko::matrix_col_major matrix_major;
-
   if (argc <= 2) {
     std::cerr << "error: " << argv[0] << " solver_name matrix_size" << std::endl;
     MPI_Abort(MPI_COMM_WORLD, 34);
   }
 
+  std::cout.precision(5);
   std::string solver_name(argv[1]);
   unsigned int dim = boost::lexical_cast<unsigned int>(argv[2]);
 
   rokko::serial_solver solver(solver_name);
   solver.initialize(argc, argv);
+  std::cout << "solver = " << solver_name << std::endl
+            << "dimension = " << dim << std::endl;
 
   rokko::localized_matrix<rokko::matrix_col_major> mat(dim, dim);
   rokko::frank_matrix::generate(mat);
-  std::cout << mat << std::endl;
+  std::cout << "Frank matrix:\n" << mat << std::endl;
 
   rokko::localized_vector w(dim);
   rokko::localized_matrix<rokko::matrix_col_major> eigvec(dim, dim);
 
   try {
     solver.diagonalize(mat, w, eigvec);
-    //    eigvec = mat;
   }
   catch (const char *e) {
     std::cout << "Exception : " << e << std::endl;
     exit(22);
   }
+  rokko::frank_matrix::generate(mat);
 
-  std::cout << "eigvec:" << eigvec << std::endl;
-  
-  std::cout.precision(20);
   rokko::localized_vector eigval_sorted(dim);
   rokko::localized_matrix<rokko::matrix_col_major> eigvec_sorted(dim, dim);
   rokko::sort_eigenpairs(w, eigvec, eigval_sorted, eigvec_sorted);
-  std::cout << "Computed Eigenvalues= " << eigval_sorted.transpose() << std::endl;
-  
-  std::cout.precision(3);
-  std::cout << "Check the orthogonality of Eigenvectors:" << std::endl
-            << eigvec_sorted * eigvec_sorted.transpose() << std::endl;   // Is it equal to indentity matrix?
-  //<< eigvecs.transpose() * eigvecs << std::endl;   // Is it equal to indentity matrix?
-  
-  rokko::frank_matrix::generate(mat);
-  std::cout << "residual := A x - lambda x = " << std::endl
-         << mat * eigvec_sorted.col(1)  -  eigval_sorted(1) * eigvec_sorted.col(1) << std::endl;
-  std::cout << "Are all the following values equal to some eigenvalue = " << std::endl
-            << (mat * eigvec_sorted.col(0)).array() / eigvec_sorted.col(0).array() << std::endl;
-  //cout << "lmat=" << std::endl << lmat << std::endl;
+  std::cout << "eigenvalues:\n" << eigval_sorted.transpose() << std::endl
+            << "eigvectors:\n" << eigvec_sorted << std::endl;
+
+  std::cout << "orthogonality of eigenvectors:" << std::endl
+            << eigvec_sorted.transpose() * eigvec_sorted << std::endl
+            << "residual of the largest eigenvalue/vector (A x - lambda x):" << std::endl
+            << (mat * eigvec_sorted.col(0) - eigval_sorted(0) * eigvec_sorted.col(0)).transpose()
+            << std::endl;
 
   solver.finalize();
   return 0;
