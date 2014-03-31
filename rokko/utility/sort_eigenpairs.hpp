5f27705aba8888c5a6aa746c67fd5c29ae4a7735
Yuichi Motoyama <yomichi@tsg.jp>
2014-03-31 18:33:01 +0900

    reduced complexity of sort_eigenpairs to O(N log N).
    implemented by std::sort and std::pair.

--- rokko/utility/sort_eigenpairs.hpp
+++ rokko/utility/sort_eigenpairs.hpp
@@ -1,13 +1,38 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2013 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
+* Copyright (C) 2014 by Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
 #ifndef ROKKO_UTILITY_SORT_EIGENPAIRS_H
 #define ROKKO_UTILITY_SORT_EIGENPAIRS_H
 
 #include <vector>
+#include <utility>
+#include <algorithm>
 #include <rokko/localized_vector.hpp>
 #include <rokko/localized_matrix.hpp>
+
+namespace {
+typedef std::pair<double, int> entry;
+struct less{
+  bool operator()(entry const& lhs, entry const& rhs) const { return lhs < rhs; }
+};
+struct more{
+  bool operator()(entry const& lhs, entry const& rhs) const { return lhs > rhs; }
+};
+} // end of unnamed namespace
 
 namespace rokko {
 
 // sort eivanvalue and eigenvectors 
+
 
 template<typename MATRIX_MAJOR>  
 void sort_eigenpairs(const localized_vector& eigval, const localized_matrix<MATRIX_MAJOR>& eigvec,
@@ -15,33 +40,32 @@ void sort_eigenpairs(const localized_vector& eigval, const localized_matrix<MATR
                      bool ascending = true) 
 {
   int dim = eigval.size();
-  std::vector<int> q(dim);
-  double tmp;
-  for (int i=0; i<dim; ++i) q[i] = i;
-  for (int k=0; k<dim; ++k) {
-    tmp = eigval(q[k]);
-    for (int i=k+1; i<dim; ++i) {
-      if(ascending){
-        if (tmp > eigval(q[i])) { 
-          tmp = eigval(q[i]);
-          int qq = q[k];
-          q[k] = q[i];
-          q[i] = qq;
-        }
-      }else{
-        if (tmp < eigval(q[i])) { 
-          tmp = eigval(q[i]);
-          int qq = q[k];
-          q[k] = q[i];
-          q[i] = qq;
-        }
-      }
+  std::vector< ::entry> entries;
+  entries.reserve(dim);
+  for(int i=0; i<dim; ++i){
+    entries.push_back(std::make_pair(eigval(i),i));
+  }
+  if(ascending)
+    std::sort(entries.begin(), entries.end(), ::less());
+  else
+    std::sort(entries.begin(), entries.end(), ::more());
+
+  if(eigval_sorted.size() != dim)
+    eigval_sorted.resize(dim);
+  
+  if(eigvec_sorted.is_col_major()){
+    if(eigvec_sorted.cols() != dim)
+      eigvec_sorted.resize(Eigen::NoChange, dim);
+    for(int i=0; i<dim; ++i){
+      eigval_sorted(i) = entries[i].first;
+      eigvec_sorted.col(i) = eigvec.col(entries[i].second);
     }
-    eigval_sorted(k) = eigval(q[k]);
-    if(eigvec_sorted.is_col_major()){
-      eigvec_sorted.col(k) = eigvec.col(q[k]);
-    }else{
-      eigvec_sorted.row(k) = eigvec.row(q[k]);
+  }else{
+    if(eigvec_sorted.rows() != dim)
+      eigvec_sorted.resize(dim, Eigen::NoChange);
+    for(int i=0; i<dim; ++i){
+      eigval_sorted(i) = entries[i].first;
+      eigvec_sorted.row(i) = eigvec.row(entries[i].second);
     }
   }
 }
