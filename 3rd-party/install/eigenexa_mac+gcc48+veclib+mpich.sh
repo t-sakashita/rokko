fee8fe9e2ff7b6129a17029ddc827d8423a5f119
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-07-15 19:59:40 +0900

    add eigenexa_mac+gcc48+veclib+mpich.sh for EigenExa2.2a

--- 3rd-party/install/eigenexa_mac+gcc48+veclib+mpich.sh
+++ 3rd-party/install/eigenexa_mac+gcc48+veclib+mpich.sh
@@ -8,21 +8,21 @@ SCRIPT_DIR=`cd $dir && pwd`
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf EigenExa-2.0
-if test -f $HOME/source/EigenExa-2.0.tgz; then
-  tar zxf $HOME/source/EigenExa-2.0.tgz
+rm -rf EigenExa-2.2a
+if test -f $HOME/source/EigenExa-2.2a.tgz; then
+  tar zxf $HOME/source/EigenExa-2.2a.tgz
 else
-  wget -O - http://www.aics.riken.jp/labs/lpnctrt/EigenExa-2.0.tgz | tar zxf -
+  wget -O - http://www.aics.riken.jp/labs/lpnctrt/EigenExa-2.2a.tgz | tar zxf -
 fi
-cd $HOME/build/EigenExa-2.0
-patch -p1 < $SCRIPT_DIR/EigenExa-2.0.patch
+cd $HOME/build/EigenExa-2.2a
+patch -p1 < $SCRIPT_DIR/EigenExa-2.2a.patch
 
 cd $HOME/build
-rm -rf EigenExa-2.0-build && mkdir -p EigenExa-2.0-build && cd EigenExa-2.0-build
+rm -rf EigenExa-2.2a-build && mkdir -p EigenExa-2.2a-build && cd EigenExa-2.2a-build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
     -DSCALAPACK_LIB="-L$PREFIX/lib -lscalapack -Wl,-framework -Wl,vecLib" \
-    $HOME/build/EigenExa-2.0
+    $HOME/build/EigenExa-2.2a
 
 make -j4 VERBOSE=1
 make install
