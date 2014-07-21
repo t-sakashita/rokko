ebc7c72a6b1a24cdccbdb01b4d86aa92c0c028bd
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-07-21 22:52:20 +0900

    add copyright

--- rokko/eigen3/diagonalize.hpp
+++ rokko/eigen3/diagonalize.hpp
@@ -1,8 +1,17 @@
+#
+# Rokko: Integrated Interface for libraries of eigenvalue decomposition
+#
+# Copyright (C) 2012-2014 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
 #ifndef ROKKO_EIGEN3_DIAGONALIZE_H
 #define ROKKO_EIGEN3_DIAGONALIZE_H
 
-#include "rokko/localized_matrix.hpp"
-#include "rokko/localized_vector.hpp"
+#include <rokko/localized_matrix.hpp>
+#include <rokko/localized_vector.hpp>
 
 #include <Eigen/Dense>
 
