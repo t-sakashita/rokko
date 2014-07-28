b5188baa0e338d80e37c6aabc4d168b1f72e5dbb
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-07-28 15:01:26 +0900

    remove statements commented out

--- sample/sparse/heisenberg_mfree_mpi.cpp
+++ sample/sparse/heisenberg_mfree_mpi.cpp
@@ -45,6 +45,7 @@ public:
   ~heisenberg_op() {}
 
   void multiply(const double* x, double* y) const {
+    std::cout << "multiply" << std::endl;
     rokko::heisenberg_hamiltonian::multiply(comm_, L_, lattice_, x, y, &(buffer_[0]));
   }
 private:
@@ -85,38 +86,9 @@ int main(int argc, char *argv[]) {
               << "dimension = " << dim << std::endl;
 
   rokko::mapping_1d map(dim, g);
-  //rokko::distributed_crs_matrix mat("anasazi", map);
   rokko::distributed_mfree mat("anasazi", map);
   heisenberg_op  aa(MPI_COMM_WORLD, L, lattice);
   mat.define_operator(aa);  //Teuchos::rcp( new heisenberg_op(MPI_COMM_WORLD, L, lattice) );
-  /*
-  std::vector<double> values;
-  std::vector<int> cols;
-  for (int k = 0; k < map.num_rows(); ++k) {
-    int row = map.rows()[k];
-    cols.clear();
-    values.clear();
-    double diag = 0;
-    for (int l = 0;  l < lattice.size(); ++l) {
-      int i = lattice[l].first;
-      int j = lattice[l].second;
-      int m1 = 1 << i;
-      int m2 = 1 << j;
-      int m3 = m1 + m2;
-      if (((row & m3) == m1) || ((row & m3) == m2)) {
-        cols.push_back(row^m3);
-        values.push_back(0.5);
-        diag += -0.25;
-      } else {
-        diag += 0.25;
-      }
-    }
-    cols.push_back(row);
-    values.push_back(diag);
-    mat.insert(row, cols, values);
-  }
-  mat.complete();
-  */
 
   rokko::distributed_multivector_anasazi ivec(map, blockSize);
   ivec.init_random();
