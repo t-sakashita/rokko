c9be5c818d868f7e04376501dc6dd6745b9904f0
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-07-30 18:14:55 +0900

    fix install script of petsc 3.5.1 / slepc 3.5.0 for Mac

---
+++ 3rd-party/install/petsc_mac+gcc48+veclib+mpi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+PREFIX="$1"
+test -z "$PREFIX" && PREFIX=$HOME/opt/rokko
+echo "PREFIX = $PREFIX"
+
+mkdir -p $HOME/build
+cd $HOME/build
+rm -rf petsc-3.5.1
+if test -f $HOME/source/petsc-3.5.1.tar.gz; then
+  tar zxf $HOME/source/petsc-3.5.1.tar.gz
+else
+  wget -O - http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.5.1.tar.gz | tar zxf -
+fi
+
+cd $HOME/build/petsc-3.5.1
+#unset PETSC_DIR
+PETSC_DIR=$PWD
+
+./configure --prefix=$PREFIX --with-cc=mpicc --COPTFLAGS="-O3" --with-cxx=mpicxx --CXXOPTFLAGS="-O3" --with-fc=mpif77 --FOPTFLAGS="-O3" --with-mpiexec=mpirun \
+    --with-blas-lapack-lib="-framework vecLib" \
+    --with-blacs-dir=$PREFIX --with-c++-support=1 --with-debugging=0
+
+make # -j option can not be specified
+make install
--- 3rd-party/install/slepc.sh
+++ 3rd-party/install/slepc.sh
@@ -6,17 +6,17 @@ echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf slepc-3.4.3
-if test -f $HOME/source/slepc-3.4.3.tar.gz; then
-  tar zxf $HOME/source/slepc-3.4.3.tar.gz
+rm -rf slepc-3.5.0
+if test -f $HOME/source/slepc-3.5.0.tar.gz; then
+  tar zxf $HOME/source/slepc-3.5.0.tar.gz
 else
-  wget -O - http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.4.3.tar.gz | tar zxf -
+  wget -O - http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.5.0.tar.gz | tar zxf -
 fi
 
-cd $HOME/build/slepc-3.4.3
+cd $HOME/build/slepc-3.5.0
 rm -rf $PREFIX/include/slepc*
 unset SLEPC_DIR PETSC_DIR PETSC_ARCH
 ./configure --prefix=$PREFIX
 
-make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX PETSC_ARCH=arch-installed-petsc
-make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX PETSC_ARCH=arch-installed-petsc install
+make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX
+make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX install
