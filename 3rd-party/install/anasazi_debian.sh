0aead5dd6297b52c1ff6a1d9d4af14be161e4a2a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-08-04 18:23:23 +0900

    Updated to 11.8.1

--- 3rd-party/install/anasazi_debian.sh
+++ 3rd-party/install/anasazi_debian.sh
@@ -6,11 +6,11 @@ echo "PREFIX = $PREFIX"
 
 mkdir -p $HOME/build
 cd $HOME/build
-rm -rf trilinos-11.4.1-Source trilinos-11.4.1-Source-build
-tar jxf $HOME/source/trilinos-11.4.1-Source.tar.bz2
+rm -rf trilinos-11.8.1-Source trilinos-11.8.1-Source-build
+tar jxf $HOME/source/trilinos-11.8.1-Source.tar.bz2
 
 cd $HOME/build
-mkdir -p trilinos-11.4.1-Source-build && cd trilinos-11.4.1-Source-build
+mkdir -p trilinos-11.8.1-Source-build && cd trilinos-11.8.1-Source-build
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF \
@@ -19,7 +19,7 @@ cmake \
     -DTrilinos_ENABLE_Anasazi=ON \
     -DTrilinos_ENABLE_Didasko=ON \
     -DTrilinos_ENABLE_EXAMPLES=ON -DTrilinos_ENABLE_TESTS=ON \
-    $HOME/build/trilinos-11.4.1-Source
+    $HOME/build/trilinos-11.8.1-Source
 
 make -j4 VERBOSE=1
 make test
