a399f860279c4c11622a10f358eee1aac4fe51a4
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-08-05 14:25:40 +0900

    give num_evals to slepc

--- rokko/slepc/core.hpp
+++ rokko/slepc/core.hpp
@@ -23,9 +23,13 @@ namespace rokko {
 
 class solver_slepc {
 public:
-  solver_slepc() {}
+  solver_slepc() {
+    SlepcInitialize(NULL, NULL, (char*)0, 0);
+  }
 
-  ~solver_slepc() {}
+  ~solver_slepc() {
+    //ierr = SlepcFinalize();
+  }
 
   void diagonalize(distributed_crs_matrix const& mat,
                    distributed_multivector_slepc const& ivec,
@@ -33,16 +37,16 @@ public:
     Mat            A = reinterpret_cast<slepc::distributed_crs_matrix*>(mat.matrix_impl_.get())->matrix_;          
     EPSType        type;
     PetscMPIInt    size;
-    PetscInt       N, nev;
-    SlepcInitialize(NULL, NULL, (char*)0, 0);
+    PetscInt       nev;
 
     ierr = EPSCreate(PETSC_COMM_WORLD, &eps); //CHKERRQ(ierr);
 
     /* Set operators. In this case, it is a standard eigenvalue problem */
     ierr = EPSSetOperators(eps, A, NULL); //CHKERRQ(ierr);
     ierr = EPSSetProblemType(eps, EPS_HEP); //CHKERRQ(ierr);
-    ierr = EPSSetDimensions(eps, 10, PETSC_DECIDE, PETSC_DECIDE); //CHKERRQ(ierr);
-
+    //ierr = EPSSetDimensions(eps, num_evals, block_size, PETSC_DECIDE); //CHKERRQ(ierr);
+    ierr = EPSSetDimensions(eps, num_evals, 2 * num_evals, PETSC_DECIDE); //CHKERRQ(ierr);
+    ierr = EPSSetTolerances(eps, (PetscScalar) tol, (PetscInt) max_iters);
     /* Set solver parameters at runtime */
     ierr = EPSSetFromOptions(eps); //CHKERRQ(ierr);
     
@@ -50,34 +54,36 @@ public:
     ierr = EPSSolve(eps); //CHKERRQ(ierr);
     
     /* Optional: Get some information from the solver and display it */
-    ierr = EPSGetType(eps,&type); //CHKERRQ(ierr);
+    ierr = EPSGetType(eps, &type); //CHKERRQ(ierr);
     ierr = PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type); //CHKERRQ(ierr);
-    ierr = EPSGetDimensions(eps,&nev,NULL,NULL); //CHKERRQ(ierr);
+    ierr = EPSGetDimensions(eps, &nev, NULL, NULL); //CHKERRQ(ierr);
     ierr = PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %D\n",nev); //CHKERRQ(ierr);
+
+    PetscInt nconv;
+    EPSGetConverged(eps, &nconv);
+
+    if (nconv == 0) {
+      std::cout << "doesn't converge" << std::endl;
+    }
+
     PetscScalar eval_r, eval_i;
     Vec evec_r, evec_i;
     MatGetVecs(A, NULL, &evec_r);
     MatGetVecs(A, NULL, &evec_i);
-
-    PetscInt nconv;
-    EPSGetConverged(eps, &nconv);
-    std::cout << "nconv=" << nconv << std::endl;
  
     for (PetscInt i = 0; i<nconv; ++i) {
       EPSGetEigenpair(eps, i, &eval_r, &eval_i, evec_r, evec_i);
       evals_.push_back((double)eval_r);
-      std::cout << "evec=";
-      //VecView(evec, PETSC_VIEWER_STDOUT_WORLD);
+      //VecView(evec_r, PETSC_VIEWER_STDOUT_WORLD);
     }
 
-    /*
     // Display solution and clean up
-    ierr = EPSPrintSolution(eps, NULL); //CHKERRQ(ierr);
-    */
+    //ierr = EPSPrintSolution(eps, NULL); //CHKERRQ(ierr);
 
     ierr = EPSDestroy(&eps); //CHKERRQ(ierr);
+    ierr = VecDestroy(&evec_r); //CHKERRQ(ierr);
+    ierr = VecDestroy(&evec_i); //CHKERRQ(ierr);
     ierr = MatDestroy(&A); //CHKERRQ(ierr);
-    ierr = SlepcFinalize();
   }
 
   void diagonalize(rokko::distributed_mfree* const mat,
