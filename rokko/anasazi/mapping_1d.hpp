cbdee8dc81a0b5bdd1efafb83cfeb28d2987d792
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-09-12 02:40:38 +0900

    add me to copyright

--- rokko/anasazi/mapping_1d.hpp
+++ rokko/anasazi/mapping_1d.hpp
@@ -2,7 +2,8 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2014-2014 by Synge Todo <wistaria@comp-phys.org>
+* Copyright (C) 2014-2014 by Synge Todo <wistaria@comp-phys.org>,
+*                            Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
