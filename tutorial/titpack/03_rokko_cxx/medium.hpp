d32d3206362b5260cede8e483ff566a4ee45f779
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-09-17 10:32:21 +0900

    sample-06 calculate correct eigenvalues for CRS matrix, but eigenvectors are not correct

--- tutorial/titpack/03_rokko_cxx/medium.hpp
+++ tutorial/titpack/03_rokko_cxx/medium.hpp
@@ -15,91 +15,45 @@
 #define TITPACK_MEDIUM_HPP
 
 #include "common.hpp"
-#include "crs_matrix.hpp"
-#include <vector>
+#include "hamiltonian.hpp"
 
-//
-// eigenvalues by the Lanczos method
-// --- dummy routine for simple working area
-//
+template<typename CRS_MATRIX>
+void elm2_mpi(hamiltonian const& hop, CRS_MATRIX& mat) {
+  int ibond = hop.num_bonds();
+  std::vector<int> cols;
+  std::vector<double> values;
+  for (int i = mat.start_row(); i <= mat.end_row(); ++i) {
+    cols.clear();
+    values.clear();
+    double diag = 0;
 
-int lnc2(crs_matrix const& mat, int nvec, int iv, std::vector<double>& E,
-         std::vector<double>& alpha, std::vector<double>& beta, matrix_type& coeff,
-         matrix_type& wk);
-// return value # number of iterations required for convergence
-// nvec      @ number of eigenvectors to calculate in lncvec
-// iv        @ location of the nonzero element of the initial vector
-// E         # eigenvalues
-// wk        working areas
+    // diagonal element
+    for (int k = 0; k < ibond; ++k) {
+      int isite1, isite2;
+      boost::tie(isite1, isite2) = hop.site_pair(k);
+      int is = (1 << isite1) + (1 << isite2);
+      double wght = -hop.bond_weight(k) * hop.z_ratio(k) * 0.5;
+      int ibit = hop.config(i) & is;
+      diag += (ibit == 0 || ibit == is) ? wght : -wght;
+    }
+    cols.push_back(i);
+    values.push_back(diag);
 
-//
-// eigenvalues by the Lanczos method
-//
-
-int lnc2z(crs_matrix const& mat, int nvec, int iv, std::vector<double>& E,
-          std::vector<double>& alpha, std::vector<double>& beta, matrix_type& coeff,
-          double *v1, double *v0);
-
-//
-// eigenvector by the Lanczos method
-//
-// nvec      @ number of eigenvectors to be calculated
-// iv        @ location of the nonzero element of the initial vector
-// x         # eigenvector
-// itr     @ number of interations for convergence
-// wk         working area
-
-void lncv2(crs_matrix const& mat, int nvec, int iv, std::vector<double> const& alpha,
-           std::vector<double> const& beta, matrix_type const& coeff, matrix_type& x,
-           int itr, matrix_type& wk);
-
-//
-// eigenvector by the Lanczos method
-//
-
-void lncv2z(crs_matrix const& mat, int nvec, int iv, std::vector<double> const& alpha,
-            std::vector<double> const& beta, matrix_type const& coeff, matrix_type& x,
-            int itr, double *v1, double *v0);
-
-//
-// check of the eigenvector and eigenvalue
-//
-// x           @ eigenvector to be checked
-// v           # H*x
-
-double check2(crs_matrix const& mat, const double *x, matrix_type& v, int vindex);
-
-double check2(crs_matrix const& mat, matrix_type const& x, int xindex, matrix_type& v, int vindex);
-
-double check2(crs_matrix const& mat, std::vector<double> const& x, matrix_type& v, int vindex);
-
-//
-// inverse iteration
-//   --- dummy routine for simple working area
-//
-
-void inv2(crs_matrix const& mat, double Eig, int iv, std::vector<double>& x, matrix_type& wk);
-
-//
-// inverse iteration
-//
-// b            working area for the rhs of (H-E(approx))*x=b
-// p,r,y        working area used in the routine cg1
-
-void inv2z(crs_matrix const& mat, double Eig, int iv, std::vector<double>& x, double *b,
-           double *p, double *r, double *y);
-
-//
-// solution of linear equations -- cg method
-//
-// return value # number of iterations required for convergence
-// Eig         @ eigenvalue
-// x           # eigenvector
-// b             working area for the rhs of (H-E(approx))*x=b
-// p,r,y         working area used in the routine cg
-// list1,list2 @ spin configurations generated in 'sz'
-
-int cg2(crs_matrix const& mat, double Eig, std::vector<double>& x, double *b, double *p,
-        double *r, double *y);
+    // off-diagonal elements
+    for (int k = 0; k < ibond; ++k) {
+      int isite1, isite2;
+      boost::tie(isite1, isite2) = hop.site_pair(k);
+      int is = (1 << isite1) + (1 << isite2);
+      double wght = -hop.bond_weight(k);
+      int ibit = hop.config(i) & is;
+      if (ibit != 0 && ibit != is) {
+        cols.push_back(hop.config2index(hop.config(i) ^ is));
+        values.push_back(wght);
+      }
+    }
+    mat.insert(i, cols, values);
+  }
+  mat.complete();
+}
 
 #endif
---
+++ tutorial/titpack/03_rokko_cxx/sample-06_mpi.cpp
@@ -0,0 +1,124 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2014 by Synge Todo <wistaria@comp-phys.org>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+// C++ version of TITPACK Ver.2 by H. Nishimori
+
+/************* Sample main program #6 *****************
+* 1d Heisenberg antiferromagnet with 16 spins
+* Eigenvalues and an eigenvector / lnc2, lncv2
+* Precision check and correlation functions
+******************************************************/
+
+#include <mpi.h>
+#include <iostream>
+#include <boost/timer.hpp>
+#include <rokko/grid_1d.hpp>
+#include <rokko/solver.hpp>
+#include <rokko/distributed_crs_matrix.hpp>
+#include "titpack.hpp"
+#include "options.hpp"
+
+typedef rokko::parallel_sparse_solver solver_type;
+
+int main(int argc, char** argv) {
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  rokko::grid_1d g;
+  
+  std::cout.precision(10);
+  options opt(argc, argv, 16, solver_type::default_solver(), g.get_myrank() == 0);
+  if (!opt.valid) MPI_Abort(MPI_COMM_WORLD, 1);
+  boost::timer tm;
+  MPI_Barrier(g.get_comm());
+  double t1 = tm.elapsed();
+
+  // lattice structure
+  int n = opt.N;
+  int ibond = n;
+  std::vector<int> ipair;
+  for (int i = 0; i < ibond; ++i) {
+    ipair.push_back(i);
+    ipair.push_back((i + 1) % n);
+  }
+
+  // Hamiltonian parameters
+  std::vector<double> bondwt(ibond, -1);
+  std::vector<double> zrtio(ibond, 1);
+
+  // table of configurations and Hamiltonian operator
+  subspace ss(n, 0);
+  hamiltonian hop(ss, ipair, bondwt, zrtio);
+  solver_type solver(opt.solver);
+  solver.initialize(argc, argv);
+
+  // Hamiltonian matrix
+  rokko::distributed_crs_matrix mat(hop.dimension(), hop.dimension(), solver);
+  elm2_mpi(hop, mat);
+  MPI_Barrier(g.get_comm());
+  double t2 = tm.elapsed();
+  
+  // Eigenvalues
+  int nev = 10;
+  int blockSize = 5;
+  int maxIters = 500;
+  double tol = 1.0e-8;
+  solver.diagonalize(mat, nev, blockSize, maxIters, tol);
+  double t3 = tm.elapsed();
+  
+  if (g.get_myrank() == 0) {
+    std::cout << "[Number of converged eigenpairs]\n\t" << solver.num_conv() << std::endl;
+    // std::cout << "[Iteration number]\n\t" << itr << std::endl;
+    std::cout << "[Eigenvalues]\n";
+    for (int i = 0; i < solver.num_conv(); ++i) std::cout << '\t' << solver.eigenvalue(i);
+    std::cout << std::endl;
+  }
+
+  // Ground-state eigenvector
+  std::vector<double> eigvec;
+  solver.eigenvector(0, eigvec);
+  if (g.get_myrank() == 0) std::cout << "[Eigenvector components (selected)]";
+  std::cout << std::flush;
+  MPI_Barrier(g.get_comm());
+  int count = 0;
+  for (int i = 12; i < ss.dimension(); i += ss.dimension()/20, ++count) {
+    if (i >= mat.start_row() && i <= mat.end_row()) {
+      if (count % 4 == 0) std::cout << std::endl;
+      std::cout << '\t' << eigvec[i];
+    }
+    std::cout << std::flush;
+    MPI_Barrier(g.get_comm());
+  }
+  if (g.get_myrank() == 0) std::cout << std::endl;
+  std::cout << std::flush;
+  MPI_Barrier(g.get_comm());
+
+  // Precision check and correlation functions
+  // double Hexpec = check2(mat, x, 0, v, 0);
+
+  // std::vector<int> npair;
+  // npair.push_back(1);
+  // npair.push_back(2);
+  // std::vector<double> sxx(1), szz(1);
+  // xcorr(ss, npair, x, 0, sxx);
+  // zcorr(ss, npair, x, 0, szz);
+  // std::cout << "[Nearest neighbor correlation functions]\n\t" 
+  //           << "sxx : " << sxx[0]
+  //           << ", szz : " << szz[0] << std::endl;
+
+  if (g.get_myrank() == 0) {
+    std::cerr << "initialize      " << (t2-t1) << " sec\n"
+              << "diagonalization " << (t3-t2) << " sec\n";
+    // << "check           " << (t4-t3) << " sec\n"
+    // << "correlation     " << (t5-t4) << " sec\n";
+  }
+
+  MPI_Finalize();
+}
