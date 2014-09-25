28f6dfc96b990aef39c17d9c2d37f0b2d58b14b8
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-09-25 22:53:50 +0900

    Renamed macos-gcc48 to macos-veclib

--- 3rd-party/install/elemental/macos-veclib.sh
+++ 3rd-party/install/elemental/macos-veclib.sh
@@ -15,7 +15,6 @@ if [ -f $HOME/source/Elemental-$ELEMENTAL_VERSION.tgz ]; then
 else
   check wget -O - http://elemental.googlecode.com/files/Elemental-$ELEMENTAL_VERSION.tgz | tar zxf -
 fi
-cd $BUILD_DIR/Elemental-$ELEMENTAL_VERSION
 
 BUILD_TYPES="Release Debug"
 
@@ -24,7 +23,7 @@ for build_type in $BUILD_TYPES; do
   rm -rf Elemental-$ELEMENTAL_VERSION-build-$build_type
   mkdir -p Elemental-$ELEMENTAL_VERSION-build-$build_type && cd Elemental-$ELEMENTAL_VERSION-build-$build_type
   check cmake -DCMAKE_BUILD_TYPE="Hybrid$build_type" -DCMAKE_INSTALL_PREFIX=$PREFIX_ROKKO/$build_type \
-    -DCMAKE_C_COMPILER=gcc-mp-4.8 -DCMAKE_Fortran_COMPILER=gfortran-mp-4.8 \
+    -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran \
     -DMATH_LIBS="-Wl,-framework -Wl,vecLib" \
     -DELEM_SHARED_LIBRARIES=ON \
     -DCMAKE_INSTALL_RPATH="$PREFIX_ROKKO/$build_type/lib" -DCMAKE_SKIP_BUILD_RPATH=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=OFF -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_MACOSX_RPATH=1 \
