24d3ada62a96c4bfe5d74c403f8038bd61d084c6
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-09-25 23:06:47 +0900

    Removed PREFIX_ALSP

--- 3rd-party/install/elemental/intel-mkl.sh
+++ 3rd-party/install/elemental/intel-mkl.sh
@@ -24,10 +24,21 @@ for build_type in $BUILD_TYPES; do
   cd $BUILD_DIR
   rm -rf Elemental-$ELEMENTAL_VERSION-build-$build_type
   mkdir -p Elemental-$ELEMENTAL_VERSION-build-$build_type && cd Elemental-$ELEMENTAL_VERSION-build-$build_type
-  check cmake -DCMAKE_BUILD_TYPE="Hybrid$build_type" -DCMAKE_INSTALL_PREFIX=$PREFIX_ROKKO/$build_type \
-    -DSHARED_LIBRARIES=ON \
+  if [ `which mpicxx > /dev/null 2>&1; echo $?` = 0 ]; then
+    check cmake -DCMAKE_BUILD_TYPE="Hybrid$build_type" -DCMAKE_INSTALL_PREFIX=$PREFIX_ROKKO/$build_type \
+      -DSHARED_LIBRARIES=ON \
+      -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
+      -DMATH_LIBS="-mkl=parallel;-lifcore" \
     $BUILD_DIR/Elemental-$ELEMENTAL_VERSION
+  else
+    check cmake -DCMAKE_BUILD_TYPE="Hybrid$build_type" -DCMAKE_INSTALL_PREFIX=$PREFIX_ROKKO/$build_type \
+      -DSHARED_LIBRARIES=ON \
+      -DCMAKE_CXX_COMPILER=icpc -DCMAKE_C_COMPILER=icc -DCMAKE_Fortran_COMPILER=ifort \
+      -DMPI_C_INCLUDE_PATH="/usr/include" -DMPI_CXX_INCLUDE_PATH="/usr/include" \
+      -DMPI_C_LIBRARIES="-lmpi" -DMPI_CXX_LIBRARIES="-lmpi++;-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
+      -DMATH_LIBS="-mkl=parallel;-lifcore" \
+    $BUILD_DIR/Elemental-$ELEMENTAL_VERSION
+  fi
   check make VERBOSE=1 -j4
   $SUDO make install
 done
-
--- 3rd-party/install/util.sh
+++ 3rd-party/install/util.sh
@@ -2,7 +2,6 @@
 
 set_prefix() {
   PREFIX_OPT_DEF="$HOME/opt"
-  PREFIX_ALPS_DEF="$HOME/alps"
   PREFIX_ROKKO_DEF="$HOME/rokko"
   SUDO=
 
@@ -13,69 +12,69 @@ set_prefix() {
     HOSTNAME=$(hostname)
   fi
 
-  if [ -d /opt/nano/alps ]; then
+  if [ -d /opt/rokko ]; then
+    SUDO="sudo"
+    PREFIX_ROKKO_DEF="/opt/rokko"
     if [ -d /opt/local ]; then
       PREFIX_OPT_DEF="/opt/local"
-      PREFIX_ALPS_DEF="/opt/nano/alps"
-      SUDO="sudo"
+    else
+      PREFIX_OPT_DEF="/opt/alps"
+    fi
+  elif [ -d /opt/nano/rokko ]; then
+    SUDO="sudo"
+    PREFIX_ROKKO_DEF="/opt/nano/rokko"
+    if [ -d /opt/local ]; then
+      PREFIX_OPT_DEF="/opt/local"
     else
       PREFIX_OPT_DEF="/opt/nano/alps"
-      PREFIX_ALPS_DEF="/opt/nano/alps"
     fi
   fi
 
   # for Mac OS X
   if [ $(uname) = Darwin ]; then
-    if [ -d /opt/alps ]; then
-      if [ -d /opt/rokko ]; then
-        PREFIX_OPT_DEF="/opt/alps"
-        PREFIX_ALPS_DEF="/opt/alps"
-        PREFIX_ROKKO_DEF="/opt/rokko"
-      fi
+    if [ -d /opt/rokko ]; then
+      PREFIX_OPT_DEF="/opt/alps"
+      PREFIX_ROKKO_DEF="/opt/rokko"
     fi
   fi
 
   # for camphor.kudpc.kyoto-u.ac.jp
   if [ -d /LARGE0/hp120237 ]; then
     PREFIX_OPT_DEF="/LARGE0/hp120237/opt"
-    PREFIX_ALPS_DEF="/LARGE0/hp120237/alps"
+    PREFIX_ROKKO_DEF="/LARGE0/hp120237/rokko"
   fi
 
   # for k.aics.riken.jp
   if [ -d /opt/spire/alps ]; then
     PREFIX_OPT_DEF="/opt/spire/alps"
-    PREFIX_ALPS_DEF="/opt/spire/alps"
+    PREFIX_ROKKO_DEF="/opt/spire/alps/rokko"
   fi
 
   # for kashiwa.issp.u-tokyo.ac.jp
   if [ -d /home/issp/materiapps ]; then
     PREFIX_OPT_DEF="/home/issp/materiapps/opt"
-    PREFIX_ALPS_DEF="/home/issp/materiapps/alps"
+    PREFIX_ROKKO_DEF="/home/issp/materiapps/alps/rokko"
   fi
 
   # for maki.issp.u-tokyo.ac.jp
   if [[ ! -z `echo "$HOSTNAME" | egrep "^maki.\.fx10hpc$"` ]]; then
     PREFIX_OPT_DEF="/global/app/materiapps/opt"
-    PREFIX_ALPS_DEF="/global/app/materiapps/alps"
+    PREFIX_ROKKO_DEF="/global/app/materiapps/alps/rokko"
   fi
 
   # for oakleaf-fx.cc.u-tokyo.ac.jp
   if [[ ! -z `echo "$HOSTNAME" | egrep "^oakleaf-fx.*$"` ]]; then
     PREFIX_OPT_DEF="/group/gc25/share/opt"
-    PREFIX_ALPS_DEF="/group/gc25/share/alps"
+    PREFIX_ROKKO_DEF="/group/gc25/share/rokko"
   fi
 
   if [ -z "$PREFIX_OPT" ]; then
     PREFIX_OPT="$PREFIX_OPT_DEF"
   fi
-  if [ -z "$PREFIX_ALPS" ]; then
-    PREFIX_ALPS="$PREFIX_ALPS_DEF"
-  fi
   if [ -z "$PREFIX_ROKKO" ]; then
     PREFIX_ROKKO="$PREFIX_ROKKO_DEF"
   fi
   echo "PREFIX_OPT = $PREFIX_OPT"
-  echo "PREFIX_ALPS = $PREFIX_ALPS"
   echo "PREFIX_ROKKO = $PREFIX_ROKKO"
   echo "SUDO = $SUDO"
   return 0
