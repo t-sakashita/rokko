211a912a4957271ee5acbd5d92a064e9cdccc15f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-09-27 00:10:53 +0900

    Added EigenExa/intel-mkl.sh

--- 3rd-party/install/EigenExa/intel-mkl.sh
+++ 3rd-party/install/EigenExa/intel-mkl.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/../util.sh
@@ -13,21 +13,20 @@ sh $SCRIPT_DIR/setup.sh
 BUILD_TYPES="Release Debug"
 for build_type in $BUILD_TYPES; do
   cd $BUILD_DIR
-  rm -rf elpa_lib-$ELPA_VERSION-build-$build_type
-  mkdir -p elpa_lib-$ELPA_VERSION-build-$build_type
-  cd elpa_lib-$ELPA_VERSION-build-$build_type
+  mkdir -p EigenExa-$EIGENEXA_VERSION-build-$build_type
+  cd EigenExa-$EIGENEXA_VERSION-build-$build_type
   if [ `which mpicxx > /dev/null 2>&1; echo $?` = 0 ]; then
     check cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$PREFIX_ROKKO/$build_type \
       -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
       -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-      $BUILD_DIR/elpa_lib-$ELPA_VERSION
+      $BUILD_DIR/EigenExa-$EIGENEXA_VERSION
   else
     check cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$PREFIX_ROKKO/$build_type \
       -DCMAKE_C_COMPILER=icc -DCMAKE_Fortran_COMPILER=ifort \
       -DMPI_C_INCLUDE_PATH="/usr/include" \
       -DMPI_C_LIBRARIES="-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
       -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-      $BUILD_DIR/elpa_lib-$ELPA_VERSION
+      $BUILD_DIR/EigenExa-$EIGENEXA_VERSION
   fi
   check make VERBOSE=1 -j4
   $SUDO make install
