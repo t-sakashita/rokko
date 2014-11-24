4d5c99313217ea6ef8af862fee1484ff511e7ba7
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2014-11-24 20:27:17 +0900

    remove commented out sentences

--- rokko/grid.hpp
+++ rokko/grid.hpp
@@ -25,12 +25,6 @@ extern struct grid_col_major_t {} grid_col_major;
 
 class grid {
 public:
-  // This constructor produces 2D grid whose row size is longer.  In
-  // EigenK's grid creation, the variables nprow, npcol are changed
-  // with this constructor's one, and in EigenK's sample main program,
-  // some strange substitution is done: "NPROW = size_of_col NPCOL =
-  // size_of_row".  So, this constructor's 2D grid size is consistent
-  // with EigenK's 2D grid one.
   explicit grid(MPI_Comm comm_in = MPI_COMM_WORLD) : comm(comm_in) {
     initialize(grid_row_major);
   }
@@ -38,26 +32,7 @@ public:
   grid(MPI_Comm comm_in, GRID_MAJOR const& grid_major) : comm(comm_in) {
     initialize(grid_major);
   }
-  // The following 2D grid size is consistent with EigenK's 2D grid
-  // creator's one, but it is actually changed with actual EigenK's 2D
-  // grid row/col size. So we don't use it.
-  /*
-  grid(MPI_Comm comm_in = MPI_COMM_WORLD) {
-    comm = comm_in;
-    MPI_Comm_size(comm, &nprocs);
-    MPI_Comm_rank(comm, &myrank);
 
-    npcol = int(std::sqrt(nprocs + 0.5));
-    while (1) {
-      if ( npcol == 1 ) break;
-      if ( (nprocs % npcol) == 0 ) break;
-      npcol = npcol - 1;
-    }
-    nprow = nprocs / npcol;
-    myrow = calculate_grid_row(myrank);
-    mycol = calculate_grid_col(myrank);
-  }
-  */
   MPI_Comm get_comm() const { return comm; }
   int get_nprocs() const { return nprocs; }
   int get_nprow() const { return nprow; }
