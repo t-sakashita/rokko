87a1234a8b4a0854188abe3b5cdf639c4c68ad2f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-01-19 19:47:44 +0900

    Update EigenExa install script for macos and intel-mkl

---
+++ 3rd-party/install/EigenExa/link.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+. $SCRIPT_DIR/../util.sh
+. $SCRIPT_DIR/version.sh
+set_prefix
+
+BUILD_TYPES="Release Debug"
+for build_type in $BUILD_TYPES; do
+  $SUDO rm -f $PREFIX_ROKKO/eigenexa
+  $SUDO ln -s $PREFIX_ROKKO/eigenexa-$EIGENEXA_VERSION-$EIGENEXA_PATCH_VERSION $PREFIX_ROKKO/eigenexa
+  $SUDO rm -f $PREFIX_ROKKO/env-$build_type.d/eigenexa.sh
+  $SUDO ln -s $PREFIX_ROKKO/eigenexa/$build_type/eigenexavars.sh $PREFIX_ROKKO/env-$build_type.d/eigenexavars.sh
+done
--- 3rd-party/install/EigenExa/setup.sh
+++ 3rd-party/install/EigenExa/setup.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/../util.sh
 . $SCRIPT_DIR/version.sh
-set_build_dir
+set_prefix
 
 cd $BUILD_DIR
 rm -rf EigenExa-$EIGENEXA_VERSION*
-if test -f $HOME/source/EigenExa-$EIGENEXA_VERSION.tgz; then
-  tar zxf $HOME/source/EigenExa-$EIGENEXA_VERSION.tgz
+if test -f $SOURCE_DIR/EigenExa-$EIGENEXA_VERSION.tgz; then
+  tar zxf $SOURCE_DIR/EigenExa-$EIGENEXA_VERSION.tgz
 else
   wget -O - http://www.aics.riken.jp/labs/lpnctrt/EigenExa-$EIGENEXA_VERSION.tgz | tar zxf -
 fi
