09b00fda726e3a2ddd9f9e0f3a96fc2fcc8f434d
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-01-20 10:29:30 +0900

    Fixed path to scalapack

--- 3rd-party/install/Elpa/macos.sh
+++ 3rd-party/install/Elpa/macos.sh
@@ -7,6 +7,7 @@ set_prefix
 
 sh $SCRIPT_DIR/setup.sh
 
+. $PREFIX_ROKKO/rokkoenv.d/scalapackvars.sh
 BUILD_TYPES="Release Debug"
 for build_type in $BUILD_TYPES; do
   PREFIX=$PREFIX_ROKKO/elpa-$ELPA_VERSION-$ELPA_PATCH_VERSION/$build_type
@@ -17,7 +18,7 @@ for build_type in $BUILD_TYPES; do
     -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran \
     -DCMAKE_Fortran_FLAGS_RELEASE="-O3 -ffree-line-length-none" \
     -DCMAKE_Fortran_FLAGS_DEBUG="-g -ffree-line-length-none" \
-    -DSCALAPACK_LIB="-L$PREFIX_ROKKO/$build_type/lib -lscalapack -Wl,-framework -Wl,Accelerate" \
+    -DSCALAPACK_LIB="-L$SCALAPACK_ROOT/$build_type/lib -lscalapack -Wl,-framework -Wl,Accelerate" \
     -DCMAKE_INSTALL_RPATH="$PREFIX/lib" -DCMAKE_SKIP_BUILD_RPATH=OFF -DCMAKE_BUILD_WITH_INSTALL_RPATH=OFF -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_MACOSX_RPATH=1 \
     $BUILD_DIR/elpa_lib-$ELPA_VERSION
   check make VERBOSE=1 -j4
