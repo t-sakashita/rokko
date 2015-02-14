0ed25351e87f589c1c39e3e91d42b43b1f1aa33a
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-02-14 20:04:34 +0900

    add ldd to mapping_bc

--- rokko/mapping_bc.hpp
+++ rokko/mapping_bc.hpp
@@ -23,12 +23,18 @@ public:
   explicit mapping_bc(grid const& g, int dim, int block_size) :
     g_(g), dim_(dim), block_size_(block_size) {
     dim_local_ = dim / block_size;
+    lld_ = dim_local_;   // default local leading dimension
+  }
+  explicit mapping_bc(grid const& g, int dim, int block_size, int lld) :
+    g_(g), dim_(dim), block_size_(block_size) {
+    dim_local_ = dim / block_size;
+    lld_ = lld;   // set non-default local leading dimension
   }
   int get_dim() const { return dim_; }
   int get_dim_local() const { return dim_local_; }
   int get_block_size() const { return block_size_; }
-  int get_lld() const { return lld; }
-  void set_lld(int value) { lld = value; }
+  int get_lld() const { return lld_; }
+  void set_lld(int value) { lld_ = value; }
   grid const& get_grid() const { return g_; }
 
 private:
@@ -36,7 +42,7 @@ private:
   int block_size_;
   // block_number is also needed?
   int dim_local_;
-  int lld;
+  int lld_;
   grid g_;
 };
 
