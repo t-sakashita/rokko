a2a006a5ac63a707bd6e786f4c691f743fb80af4
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-04-16 18:59:19 +0900

    New upstream 11.14.2

--- 3rd-party/install/Trilinos/setup.sh
+++ 3rd-party/install/Trilinos/setup.sh
@@ -6,7 +6,16 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 set_prefix
 
 cd $BUILD_DIR
+
 rm -rf trilinos-$TRILINOS_VERSION-Source*
-check tar jxf $HOME/source/trilinos-$TRILINOS_VERSION-Source.tar.bz2
-cd trilinos-$TRILINOS_VERSION-Source
-patch -p1 < $SCRIPT_DIR/trilinos-$TRILINOS_VERSION.patch
+if [ -f $HOME/source/trilinos-$TRILINOS_VERSION-Source.tar.bz2 ]; then
+  check tar jxf $HOME/source/trilinos-$TRILINOS_VERSION-Source.tar.bz2
+else
+  echo "Error: trilinos-$TRILINOS_VERSION-Source.tar.bz2 not found"
+  exit 127
+fi
+
+if [ -f $SCRIPT_DIR/trilinos-$TRILINOS_VERSION.patch ]; then
+  cd trilinos-$TRILINOS_VERSION-Source
+  check patch -p1 < $SCRIPT_DIR/trilinos-$TRILINOS_VERSION.patch
+fi
