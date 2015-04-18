23c65e090e5959b576824daf81dd980199f958ca
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-04-18 09:48:51 +0900

    Change matrix to rectangular in orthonormalizaton example

--- sample/lapack/orthonormalization.cpp
+++ sample/lapack/orthonormalization.cpp
@@ -17,20 +17,22 @@ typedef rokko::localized_matrix<rokko::matrix_col_major> matrix_t;
 
 int main(int argc, char *argv[]) {
   int info;
-  int dim = 6;
+  int m = 10; // m >= n
+  int n = 6;
+  int k = std::min(m, n);
 
   // generate random martix
-  matrix_t a = matrix_t::Random(dim, dim);
+  matrix_t a = matrix_t::Random(m, n);
   std::cout << "Input random column vectors A:\n" << a << std::endl;
 
   // orthonormalization
-  vector_t tau(dim);
-  info = LAPACKE_dgeqrf(LAPACK_COL_MAJOR, dim, dim, &a(0, 0), dim, &tau(0));
-  info = LAPACKE_dorgqr(LAPACK_COL_MAJOR, dim, dim, dim, &a(0, 0), dim, &tau(0));
+  vector_t tau(k);
+  info = LAPACKE_dgeqrf(LAPACK_COL_MAJOR, m, n, &a(0, 0), m, &tau(0));
+  info = LAPACKE_dorgqr(LAPACK_COL_MAJOR, m, n, n, &a(0, 0), m, &tau(0));
   std::cout << "Orthonormalized column vectors V:\n" << a << std::endl;
 
   // check orthonormality
   matrix_t check = a.transpose() * a;
   std::cout << "Check orthogonality:\n" << check << std::endl;
-  std::cout << "| I - Vt V | = " <<  (matrix_t::Identity(dim, dim) - check).norm() << std::endl;
+  std::cout << "| I - Vt V | = " <<  (matrix_t::Identity(n, n) - check).norm() << std::endl;
 }
