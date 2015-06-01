209422d27df7bbf4ef864da7d2e2ffd6f7462310
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-06-01 12:57:17 +0900

    add rokko/utility/helmert_matrix.hpp

---
+++ sample/dense/helmert.cpp
@@ -0,0 +1,73 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2015 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/rokko.hpp>
+#include <rokko/utility/helmert_matrix.hpp>
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+
+typedef rokko::matrix_col_major matrix_major;
+
+int main(int argc, char *argv[]) {
+  rokko::global_timer::registrate(10, "main");
+  rokko::global_timer::registrate(11, "generate_matrix");
+  rokko::global_timer::registrate(12, "output_results");
+
+  rokko::global_timer::start(10);
+  std::string solver_name(rokko::serial_dense_solver::default_solver());
+  unsigned int dim = 10;
+  if (argc >= 2) solver_name = argv[1];
+  if (argc >= 3) dim = boost::lexical_cast<unsigned int>(argv[2]);
+
+  std::cout.precision(5);
+
+  rokko::serial_dense_solver solver(solver_name);
+  solver.initialize(argc, argv);
+  std::cout << "Eigenvalue decomposition of Helmert matrix" << std::endl
+            << "solver = " << solver_name << std::endl
+            << "dimension = " << dim << std::endl;
+
+  rokko::global_timer::start(11);
+  rokko::localized_matrix<double, matrix_major> mat(dim, dim);
+  rokko::helmert_matrix::generate(mat);
+  std::cout << "Helmert matrix:\n" << mat << std::endl;
+  rokko::global_timer::stop(11);
+
+  rokko::localized_vector<double> eigval(dim);
+  rokko::localized_matrix<double, matrix_major> eigvec(dim, dim);
+  try {
+    solver.diagonalize(mat, eigval, eigvec);
+  }
+  catch (const char *e) {
+    std::cout << "Exception : " << e << std::endl;
+    exit(22);
+  }
+  rokko::global_timer::start(11);
+  rokko::helmert_matrix::generate(mat);
+  rokko::global_timer::stop(11);
+
+  bool sorted = true;
+  for (unsigned int i = 1; i < dim; ++i) sorted &= (eigval(i-1) <= eigval(i));
+  if (!sorted) std::cout << "Warning: eigenvalues are not sorted in ascending order!\n";
+
+  rokko::global_timer::start(12);
+  std::cout << "eigenvalues:\n" << eigval.transpose() << std::endl
+            << "eigvectors:\n" << eigvec << std::endl;
+  std::cout << "orthogonality of eigenvectors:" << std::endl
+            << eigvec.transpose() * eigvec << std::endl;
+  std::cout << "residual of the smallest eigenvalue/vector (A x - lambda x):" << std::endl
+            << (mat * eigvec.col(0) - eigval(0) * eigvec.col(0)).transpose() << std::endl;
+  rokko::global_timer::stop(12);
+
+  solver.finalize();
+  rokko::global_timer::stop(10);
+  rokko::global_timer::summarize();
+}
---
+++ sample/dense/helmert_mpi.cpp
@@ -0,0 +1,81 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2015 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/rokko.hpp>
+#include <rokko/collective.hpp>
+#include <rokko/utility/helmert_matrix.hpp>
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+
+typedef rokko::matrix_col_major matrix_major;
+
+int main(int argc, char *argv[]) {
+
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  MPI_Comm comm = MPI_COMM_WORLD;
+  std::string solver_name(rokko::parallel_dense_solver::default_solver());
+  int dim = 10;
+  if (argc >= 2) solver_name = argv[1];
+  if (argc >= 3) dim = boost::lexical_cast<int>(argv[2]);
+
+  rokko::grid g(comm);
+  int myrank = g.get_myrank();
+
+  std::cout.precision(5);
+
+  rokko::parallel_dense_solver solver(solver_name);
+  solver.initialize(argc, argv);
+  if (myrank == 0)
+    std::cout << "Eigenvalue decomposition of Helmert matrix" << std::endl
+              << "num_procs = " << g.get_nprocs() << std::endl
+              #ifdef _OPENMP
+              << "num_threads per process = " << omp_get_max_threads() << std::endl
+              #endif
+              << "solver = " << solver_name << std::endl
+              << "dimension = " << dim << std::endl;
+
+  rokko::distributed_matrix<double, matrix_major> mat(dim, dim, g, solver);
+  //rokko::helmert_matrix::generate(mat);
+  //mat.print();
+  rokko::localized_vector<double> diag(dim);
+  diag << 1.2, 2.1, 3.7, 4.5, 8.3, 1.5, 9.2, 3.4, 5.4, 2.9;
+  rokko::helmert_matrix::generate_for_given_eigenvalues(mat, diag);
+  mat.print();
+
+  rokko::localized_matrix<double, matrix_major> mat_loc(dim, dim);
+  rokko::gather(mat, mat_loc, 0);
+
+  rokko::localized_vector<double> eigval(dim);
+  rokko::distributed_matrix<double, matrix_major> eigvec(dim, dim, g, solver);
+  try {
+    solver.diagonalize(mat, eigval, eigvec);
+  }
+  catch (const char *e) {
+    if (myrank == 0) std::cout << "Exception : " << e << std::endl;
+    MPI_Abort(MPI_COMM_WORLD, 22);
+  }
+
+  rokko::localized_matrix<double, matrix_major> eigvec_loc(dim, dim);
+  rokko::gather(eigvec, eigvec_loc, 0);
+  if (myrank == 0) {
+    std::cout << "largest eigenvalues:";
+    for (int i = 0; i < std::min(dim, 10); ++i) std::cout << ' ' << eigval(dim - 1 - i);
+    std::cout << std::endl;
+    std::cout << "residual of the largest eigenvalue/vector: |x A x - lambda| = "
+              << std::abs(eigvec_loc.col(dim - 1).transpose() * mat_loc * eigvec_loc.col(dim - 1)
+                          - eigval(dim - 1))
+              << std::endl;
+  }
+
+  solver.finalize();
+  MPI_Finalize();
+}
