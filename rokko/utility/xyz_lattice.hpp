acf74000a5a351726ab29ddec561f6cb6d80a4bb
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-06-02 17:39:46 +0900

    add throw sentence instead returning error value

--- rokko/utility/xyz_lattice.hpp
+++ rokko/utility/xyz_lattice.hpp
@@ -13,6 +13,7 @@
 #define ROKKO_UTILITY_XYZ_LATTICE_HPP
 
 #include <string>
+#include <list>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -21,17 +22,38 @@
 
 namespace rokko {
 
-int read_lattice_stream(std::ifstream& ifs, int& num_sites, std::vector<std::pair<int, int> >& lattice, std::vector<boost::tuple<double, double, double> >& coupling) {
-  int num_bonds;
+namespace detail {
 
+bool read_line_with_comment(std::ifstream& ifs, std::istringstream& is) {
   std::string str_line;
-  std::ifstream::pos_type file_pos;
   getline(ifs, str_line);
-  std::istringstream is(str_line);
-  if ((str_line.find("#") != 0) && (str_line.size() != 0)) { // not comment
+  std::list<std::string> list_string;
+  boost::split(list_string, str_line, boost::is_any_of("#"));
+  std::string trimed_str = list_string.front();
+  boost::trim(trimed_str);
+  is.clear();
+  is.str(trimed_str);
+  //std::cout << "string:" << trimed_str << std::endl;
+  //std::cout << "comment:" << list_string.back() << std::endl;
+  if (!trimed_str.empty()) { // the sentece is not just comment
+    return true;
+  } else { // the sentence is just comment
+    return false;
+  }
+}
+
+} // namespace detail
+
+void read_lattice_stream(std::ifstream& ifs, int& num_sites, std::vector<std::pair<int, int> >& lattice, std::vector<boost::tuple<double, double, double> >& coupling) {
+  std::string str_line;
+  int num_bonds;
+  std::istringstream is;
+  std::ifstream::pos_type file_pos;
+  if (detail::read_line_with_comment(ifs, is)) {
     is >> num_sites >> num_bonds;
   }
   std::cout << "num_sites=" << num_sites << " num_bonds=" << num_bonds << std::endl;
+
   bool start_index1 = false;
   do {
     file_pos = ifs.tellg();
@@ -45,52 +67,41 @@ int read_lattice_stream(std::ifstream& ifs, int& num_sites, std::vector<std::pai
     start_index1 = false;
     std::cout << "start_index = 0" << std::endl;
   } else {
-    std::cout << "else file_pos" << std::endl;
+    //std::cout << "else file_pos" << std::endl;
     ifs.seekg(file_pos);
   }
 
   do {
     int j, k;
-    getline(ifs, str_line);
-    boost::trim(str_line);
-    std::istringstream is(str_line);
-    if ((str_line.find("#") != 0) && (str_line.size() != 0)) { // not comment
+    if (detail::read_line_with_comment(ifs, is)) {
       is >> j >> k;
       std::cout << "j=" << j << " k=" << k << std::endl;
       if (start_index1)  lattice.push_back(std::make_pair(j-1, k-1));
       else  lattice.push_back(std::make_pair(j, k));
       if (lattice.back().first >= num_sites) {
 	std::cerr << "error: first index of"  << lattice.size() - 1 << "-th bond \"" << lattice.back().first << "\" is out of range" << std::endl;
-	return 1;
+	throw 1;
       } else if (lattice.back().second >= num_sites) {
 	std::cerr << "error: second index of " << lattice.size() - 1 << "-th bond \"" << lattice.back().second << "\" is out of range" << std::endl;
-	return 2;
+	throw 2;
       }
-    } else {
-      std::cout << "comment:" << str_line << std::endl;
     }
   } while (lattice.size() < num_bonds);
   do {
     double jx, jy, jz;
-    getline(ifs, str_line);
-    boost::trim(str_line);
-    std::istringstream is(str_line);
-    if ((str_line.find("#") != 0) && (str_line.size() != 0)) { // not comment
+    if (detail::read_line_with_comment(ifs, is)) {
       is >> jx >> jy >> jz;
       std::cout << "jx=" << jx << " jy=" << jy << " jz=" << jz << std::endl;
       coupling.push_back(boost::make_tuple(jx, jy, jz));
-    } else {      
-      std::cout << "comment:" << str_line << std::endl;
     }
   } while (coupling.size() < num_bonds);
-  return 0;
 }
 
-int read_lattice_file(std::string const& filename, int& num_sites, std::vector<std::pair<int, int> >& lattice, std::vector<boost::tuple<double, double, double> >& coupling) {
+void read_lattice_file(std::string const& filename, int& num_sites, std::vector<std::pair<int, int> >& lattice, std::vector<boost::tuple<double, double, double> >& coupling) {
   std::ifstream ifs(filename);
   if (!ifs) {
-    std::cout << "can't open file" << std::endl;
-    return 1;
+    std::cerr << "can't open file" << std::endl;
+    throw 1;
   }
   return read_lattice_stream(ifs, num_sites, lattice, coupling);
 }
