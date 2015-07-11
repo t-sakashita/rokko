f6654e02bbe7fcd5886e59fc26bb3c3c98d393c7
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-07-12 01:28:38 +0900

    delete type of several variables in module

--- example/fortran/sparse/laplacian_mfree_mpi.f90
+++ example/fortran/sparse/laplacian_mfree_mpi.f90
@@ -32,50 +32,46 @@ MODULE laplacian
   use mfree! only : rokko_distributed_mfree_construct_f
   USE, INTRINSIC :: ISO_C_BINDING
   IMPLICIT NONE
-  INTEGER(C_INT) myrank, nprocs
-  TYPE type_vars
-     integer, private :: comm
-     integer(c_int) :: nprocs, myrank
-     integer(c_int) :: dim, local_offset, num_local_rows
-     integer(c_int) :: start_row, end_row
-     integer(c_int) :: start_k, end_k
-     logical, private :: is_first_proc, is_last_proc
-     double precision, private :: buf_m, buf_p
-     INTEGER, private :: status_m(MPI_STATUS_SIZE), status_p(MPI_STATUS_SIZE)
-  END TYPE type_vars
-  TYPE(type_vars), private :: vars
+  integer, private :: comm
+  integer :: nprocs, myrank
+  integer(c_int), private :: dim, local_offset, num_local_rows
+  integer(c_int), private :: start_row, end_row
+  integer(c_int), private :: start_k, end_k
+  logical, private :: is_first_proc, is_last_proc
+  double precision, private :: buf_m, buf_p
+  INTEGER, private :: status_m(MPI_STATUS_SIZE), status_p(MPI_STATUS_SIZE)
 CONTAINS
   SUBROUTINE initialize (mat, dim_in)
     type(rokko_distributed_mfree), intent(inout) :: mat
     integer(c_int), intent(in) :: dim_in
     integer :: ierr
     integer :: tmp, rem
-    vars%comm = MPI_COMM_WORLD
-    call MPI_comm_rank(MPI_COMM_WORLD, vars%myrank, ierr)
-    call MPI_comm_size(MPI_COMM_WORLD, vars%nprocs, ierr)
-    vars%dim = dim_in
-    tmp = vars%dim / vars%nprocs
-    rem = mod(vars%dim, vars%nprocs)
-    vars%num_local_rows = (vars%dim + vars%nprocs - vars%myrank - 1) / vars%nprocs
-    vars%start_row = tmp * vars%myrank + min(rem, vars%myrank)
-    vars%end_row = vars%start_row + vars%num_local_rows - 1
+    comm = MPI_COMM_WORLD
+    call MPI_comm_rank(MPI_COMM_WORLD, myrank, ierr)
+    call MPI_comm_size(MPI_COMM_WORLD, nprocs, ierr)
+    dim = dim_in
+    tmp = dim / nprocs
+    rem = mod(dim, nprocs)
+    num_local_rows = (dim + nprocs - myrank - 1) / nprocs
+    start_row = tmp * myrank + min(rem, myrank)
+    end_row = start_row + num_local_rows - 1
     
-    if (vars%start_row == 1) then
-       vars%is_first_proc = .true.
+    if (start_row == 1) then
+       is_first_proc = .true.
     else
-       vars%is_first_proc = .false.
+       is_first_proc = .false.
     endif
  
-    if (vars%end_row == (vars%dim-1)) then
-       vars%is_last_proc = .true.
+    if (end_row == (dim-1)) then
+       is_last_proc = .true.
     else
-       vars%is_last_proc = .false.
+       is_last_proc = .false.
     endif
 
-    vars%end_k = vars%num_local_rows - 1
-    print*, "myrank=", vars%myrank, "start_row=", vars%start_row, "end_row=", vars%end_row
-    print*, "myrank=", vars%myrank, "num_local_rows=", vars%num_local_rows
-    call rokko_distributed_mfree_construct_f(mat, multiply, vars%dim, vars%num_local_rows)
+    end_k = num_local_rows - 1
+    print*, "myrank=", myrank, "start_row=", start_row, "end_row=", end_row
+    print*, "myrank=", myrank, "num_local_rows=", num_local_rows
+    call rokko_distributed_mfree_construct_f(mat, multiply, dim, num_local_rows)
   END SUBROUTINE initialize
   ! subroutine passed to C function.
   ! It must be interoperable!
@@ -86,56 +82,56 @@ CONTAINS
     integer :: ierr
     integer :: k
     
-    if (vars%num_local_rows == 0) then
+    if (num_local_rows == 0) then
        return
     endif
     
-    if (.not.(vars%is_first_proc) .and. (vars%nprocs /= 1)) then
-       !std::cout << "recv myrank=" << vars%myrank << std::endl
-       call MPI_Send(x(1), 1, MPI_DOUBLE, vars%myrank-1, 0, vars%comm)
-       call MPI_Recv(vars%buf_m, 1, MPI_DOUBLE, vars%myrank-1, 0, vars%comm, vars%status_m, ierr)
+    if (.not.(is_first_proc) .and. (nprocs /= 1)) then
+       !std::cout << "recv myrank=" << myrank << std::endl
+       call MPI_Send(x(1), 1, MPI_DOUBLE, myrank-1, 0, comm)
+       call MPI_Recv(buf_m, 1, MPI_DOUBLE, myrank-1, 0, comm, status_m, ierr)
        !std::cout << "buffff=" << buf << std::endl
     endif
     
-    if (.not.(vars%is_last_proc) .and. (vars%nprocs /= 1)) then
-       !std::cout << "send myrank=" << vars%myrank << std::endl
-       call MPI_Recv(vars%buf_p, 1, MPI_DOUBLE, vars%myrank+1, 0, vars%comm, vars%status_p)
-       call MPI_Send(x(vars%end_k), 1, MPI_DOUBLE, vars%myrank+1, 0, vars%comm)
+    if (.not.(is_last_proc) .and. (nprocs /= 1)) then
+       !std::cout << "send myrank=" << myrank << std::endl
+       call MPI_Recv(buf_p, 1, MPI_DOUBLE, myrank+1, 0, comm, status_p)
+       call MPI_Send(x(end_k), 1, MPI_DOUBLE, myrank+1, 0, comm)
        !std::cout << "buffff=" << buf2 << std::endl
     endif
     
-    if (vars%is_first_proc) then
-       if (vars%num_local_rows /= 1) then
+    if (is_first_proc) then
+       if (num_local_rows /= 1) then
           y(2) = x(2) - x(1)
-          if (vars%nprocs /= 1) then
-             y(vars%end_k) = - x(vars%end_k - 1) + 2 * x(vars%end_k) - vars%buf_p
+          if (nprocs /= 1) then
+             y(end_k) = - x(end_k - 1) + 2 * x(end_k) - buf_p
           endif
        else 
-          y(2) = x(2) - vars%buf_p
+          y(2) = x(2) - buf_p
        endif
     endif
     
-    if (vars%is_last_proc) then
-       if (vars%num_local_rows /= 1) then
-          if (vars%nprocs /= 1) then
-             y(1) = - vars%buf_m + 2 * x(1) - x(2)
+    if (is_last_proc) then
+       if (num_local_rows /= 1) then
+          if (nprocs /= 1) then
+             y(1) = - buf_m + 2 * x(1) - x(2)
           endif
-          y(vars%end_k) = 2 * x(vars%end_k) - x(vars%end_k - 1)      
+          y(end_k) = 2 * x(end_k) - x(end_k - 1)      
        else
-          y(vars%end_k) = 2 * x(vars%end_k) - vars%buf_m
+          y(end_k) = 2 * x(end_k) - buf_m
        endif
     endif
-    if (.not.(vars%is_first_proc .or. vars%is_last_proc)) then ! neither first or last process
-       if (vars%num_local_rows /= 1) then
-          y(1) = - vars%buf_m + 2 * x(1) - x(2)
-          y(vars%end_k) = - x(vars%end_k - 1) + 2 * x(vars%end_k) - vars%buf_p
+    if (.not.(is_first_proc .or. is_last_proc)) then ! neither first or last process
+       if (num_local_rows /= 1) then
+          y(1) = - buf_m + 2 * x(1) - x(2)
+          y(end_k) = - x(end_k - 1) + 2 * x(end_k) - buf_p
        endif
     else
-       y(1) = - vars%buf_m + 2 * x(2) - vars%buf_p
+       y(1) = - buf_m + 2 * x(2) - buf_p
     endif
     
     ! from 2 to end
-    do k=2, vars%end_k-1
+    do k=2, end_k-1
        y(k) = - x(k-1) + 2 * x(k) - x(k+1)
     enddo
   END SUBROUTINE multiply
