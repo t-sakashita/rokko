e43040bcc83410e27a1d34910c39824f4093bed8
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-07-25 02:06:47 +0900

    add B^{-1/2} A B^{-1/2} version for general eigenproblem

--- example/cxx/dense/gev_fixedB_mpi.cpp
+++ example/cxx/dense/gev_fixedB_mpi.cpp
@@ -22,47 +22,60 @@
 typedef rokko::matrix_col_major matrix_major;
 
 template<typename T, typename MATRIX_MAJOR>
-void diagonalize_fixedB(rokko::parallel_dense_solver& solver, rokko::distributed_matrix<T, MATRIX_MAJOR>& A, rokko::distributed_matrix<T, MATRIX_MAJOR>& B, rokko::localized_vector<double>& eigval, rokko::distributed_matrix<T, MATRIX_MAJOR>& eigvec) {
-  rokko::localized_vector<double> eigval_inv(eigval.size());
-  rokko::distributed_matrix<double, matrix_major> tmp(A.get_mapping()), Binv(B.get_mapping());
-
-  int myrank = A.get_myrank();
-  // diagonalization of B
-  std::string routine = "";
-  try {
-    solver.diagonalize(A, eigval, eigvec);
-  }
-  catch (const char *e) {
-    if (myrank == 0) std::cout << "Exception : " << e << std::endl;
-    MPI_Abort(MPI_COMM_WORLD, 22);
-  }
-
-  // computation of B^{-1}
-  for(int i=0; i<eigval.size(); ++i) {
-    eigval_inv(i) = 1;// / eigval(i);
-  }
+void function_matrix(rokko::localized_vector<double> const& eigval_tmp, rokko::distributed_matrix<T, MATRIX_MAJOR> const& eigvec, rokko::distributed_matrix<T, MATRIX_MAJOR>& result, rokko::distributed_matrix<T, MATRIX_MAJOR>& tmp) {
   for (int local_j=0; local_j<eigvec.get_n_local(); ++local_j) {
     int global_j = eigvec.translate_l2g_col(local_j);
-    double coeff = eigval_inv(global_j);  //1 / eigval(global_j);
+    double coeff = eigval_tmp(global_j);
     for (int local_i=0; local_i<eigvec.get_m_local(); ++local_i) {
       double value = eigvec.get_local(local_i, local_j);
       tmp.set_local(local_i, local_j, coeff * value); 
     }
   }
-  product(1, tmp, false, eigvec, true, 0, Binv);
-  std::cout << "inverseB:" << std::endl << Binv << std::endl;
-  //Binv.print();
+  product(1, tmp, false, eigvec, true, 0, result);
+}
 
-  // computation of inverse B^{-1} A
-  product(1, Binv, false, A, false, 0, tmp);
-  // diagonalization of B^{-1} A
+template<typename T, typename MATRIX_MAJOR>
+void diagonalize_fixedB(rokko::parallel_dense_solver& solver, rokko::distributed_matrix<T, MATRIX_MAJOR>& A, rokko::distributed_matrix<T, MATRIX_MAJOR>& B, rokko::localized_vector<double>& eigval, rokko::distributed_matrix<T, MATRIX_MAJOR>& eigvec) {
+  rokko::localized_vector<double> eigval_tmp(eigval.size());
+  rokko::distributed_matrix<double, matrix_major> tmp(A.get_mapping()), Broot(A.get_mapping()), mat(A.get_mapping());
+
+  int myrank = A.get_myrank();
+  std::string routine = "";
+  // diagonalization of B
   try {
-    solver.diagonalize(tmp, eigval, eigvec);
+    solver.diagonalize(B, eigval, eigvec);
   }
   catch (const char *e) {
     if (myrank == 0) std::cout << "Exception : " << e << std::endl;
     MPI_Abort(MPI_COMM_WORLD, 22);
   }
+  std::cout << "eigvec:" << std::endl << eigvec << std::endl;
+
+  // computation of B^{-1/2}
+  for(int i=0; i<eigval.size(); ++i) {
+    eigval_tmp(i) = sqrt(1/eigval(i));
+  }
+  function_matrix(eigval_tmp, eigvec, Broot, tmp);
+  std::cout << "root_inverseB:" << std::endl << Broot << std::endl;
+  for(int i=0; i<eigval.size(); ++i) {
+    eigval_tmp(i) = sqrt(eigval(i));
+  }
+
+  // computation of B^{-1/2} A B^{-1/2}
+  product(1, Broot, false, A, false, 0, tmp);
+  product(1, tmp, false, Broot, false, 0, mat);
+  // diagonalization of B^{-1/2} A B^{-1/2}
+  try {
+    solver.diagonalize(mat, eigval, tmp);
+  }
+  catch (const char *e) {
+    if (myrank == 0) std::cout << "Exception : " << e << std::endl;
+    MPI_Abort(MPI_COMM_WORLD, 22);
+  }
+  // computation of B^{1/2}
+  function_matrix(eigval_tmp, tmp, Broot, mat);
+  // computation of {eigvec of Ax=lambda Bx} = B^{1/2} {eigvec of B^{-1/2} A B^{-1/2}}
+  product(1, Broot, false, tmp, false, 0, eigvec);
 }
 
 int main(int argc, char *argv[]) {
@@ -98,6 +111,7 @@ int main(int argc, char *argv[]) {
   rokko::distributed_matrix<double, matrix_major> A(map);
   rokko::distributed_matrix<double, matrix_major> B(map);
   rokko::frank_matrix::generate(A);
+  rokko::frank_matrix::generate(B);
   rokko::localized_vector<double> eigval(dim);
   rokko::distributed_matrix<double, matrix_major> eigvec(map);
   diagonalize_fixedB(solver, A, B, eigval, eigvec);
