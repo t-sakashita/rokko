85d1ea94ad87c38e3eb75ed9c3b8d9bba2b6eeb5
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-11-21 19:22:33 +0900

    add & fix crs and mfree for sparse_heisenberg_ladder

--- benchmark/use_rokko/sparse_heisenberg_ladder_mpi/mfree.cpp
+++ benchmark/use_rokko/sparse_heisenberg_ladder_mpi/mfree.cpp
@@ -10,7 +10,44 @@
 *****************************************************************************/
 
 #include <rokko/rokko.hpp>
+#include <rokko/utility/heisenberg_hamiltonian_mpi.hpp>
 #include <rokko/utility/lattice.hpp>
+
+class heisenberg_op : public rokko::distributed_mfree {
+public:
+  heisenberg_op(int L, const std::vector<std::pair<int, int> >& lattice)
+    : L_(L), lattice_(lattice) {
+    comm_ = MPI_COMM_WORLD;
+    int size, rank;
+    MPI_Comm_size(comm_, &size);
+    MPI_Comm_rank(comm_, &rank);
+    int n = size;
+    int p = -1;
+    do {
+      n /= 2;
+      ++p;
+    } while (n > 0);
+    dim_ = 1 << L;
+    num_local_rows_ = 1 << (L-p);
+    local_offset_ = num_local_rows_ * rank;
+    buffer_.assign(num_local_rows_, 0);
+  }
+  ~heisenberg_op() {}
+
+  void multiply(const double* x, double* y) const {
+    rokko::heisenberg_hamiltonian::multiply(comm_, L_, lattice_, x, y, &(buffer_[0]));
+  }
+  int get_dim() const { return dim_; }
+  int get_local_offset() const { return local_offset_; }
+  int get_num_local_rows() const { return num_local_rows_; }
+
+private:
+  MPI_Comm comm_;
+  int L_;
+  std::vector<std::pair<int, int> > lattice_;
+  int dim_, local_offset_, num_local_rows_;
+  mutable std::vector<double> buffer_;
+};
 
 int main(int argc, char *argv[]) {
   int provided;
@@ -31,7 +68,7 @@ int main(int argc, char *argv[]) {
     rokko::print_lattice(lattice);
   int dim = 1 << L;
   if (rank == 0)
-    std::cout << "Eigenvalue decomposition of antiferromagnetic Heisenberg 1D ladderlattice" << std::endl
+    std::cout << "Eigenvalue decomposition of antiferromagnetic Heisenberg 1D ladder lattice" << std::endl
 	      << "solver = " << name << std::endl
 	      << "L = " << L << std::endl
 	      << "dimension = " << dim << std::endl;
@@ -39,34 +76,9 @@ int main(int argc, char *argv[]) {
   init_tick = MPI_Wtime();
   rokko::parallel_sparse_solver solver(name);
   initend_tick = MPI_Wtime();
-
+  
   gen_tick = MPI_Wtime();
-  rokko::distributed_crs_matrix mat(dim, dim, solver);
-  std::vector<double> values;
-  std::vector<int> cols;
-  for (int row = mat.start_row(); row <= mat.end_row(); ++row) {
-    cols.clear();
-    values.clear();
-    double diag = 0;
-    for (int l = 0;  l < lattice.size(); ++l) {
-      int i = lattice[l].first;
-      int j = lattice[l].second;
-      int m1 = 1 << i;
-      int m2 = 1 << j;
-      int m3 = m1 + m2;
-      if (((row & m3) == m1) || ((row & m3) == m2)) {
-	cols.push_back(row^m3);
-	values.push_back(0.5);
-	diag += -0.25;
-      } else {
-	diag += 0.25;
-      }
-    }
-    cols.push_back(row);
-    values.push_back(diag);
-    mat.insert(row, cols, values);
-  }
-  mat.complete();
+  heisenberg_op mat(L, lattice);
   
   diag_tick = MPI_Wtime();
   rokko::parameters params;
@@ -76,7 +88,7 @@ int main(int argc, char *argv[]) {
   params.set("num_eigenvalues", 10);
   rokko::parameters info = solver.diagonalize(mat, params);
   end_tick = MPI_Wtime();
-  
+
   int num_conv = info.get<int>("num_conv");
   if (num_conv == 0) MPI_Abort(MPI_COMM_WORLD, -1);
   std::vector<double> eigvec;
