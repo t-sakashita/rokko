1169d7bd67b85441d8d99e8100c5660e50e11837
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2015-12-31 18:58:56 +0900

    fix intel-mkl-sgimpt.sh

--- 3rd-party/install/ELPA/intel-mkl-sgimpt.sh
+++ 3rd-party/install/ELPA/intel-mkl-sgimpt.sh
@@ -11,26 +11,17 @@ BUILD_TYPES="Release Debug"
 for build_type in $BUILD_TYPES; do
   PREFIX=$PREFIX_ROKKO/elpa-$ELPA_VERSION-$ELPA_RK_REVISION/$build_type
   cd $BUILD_DIR
-  mkdir -p elpa_lib-$ELPA_VERSION-build-$build_type
-  cd elpa_lib-$ELPA_VERSION-build-$build_type
-  if [ `which mpicxx > /dev/null 2>&1; echo $?` = 0 ]; then
-    check cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
-      -DCMAKE_C_FLAGS="-mt" -DCMAKE_Fortran_FLAGS="-mt" \
-      -DMPI_C_COMPILER=mpicc -DMPI_Fortran_COMPILER=mpif90 \
-      -DMPI_C_INCLUDE_PATH="/home/app/mpt/mpt-2.12-p11218/include" -DMPI_Fortran_INCLUDE_PATH="/home/app/mpt/mpt-2.12-p11218/include" \
-      -DMPI_C_LIBRARIES="-mt" -DMPI_Fortran_LIBRARIES="-mt" \
-      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-      $BUILD_DIR/elpa_lib-$ELPA_VERSION
+  cp -rp elpa-$ELPA_VERSION elpa-$ELPA_VERSION-build-$build_type
+  cd elpa-$ELPA_VERSION-build-$build_type
+  if [ $build_type == "Release" ]; then
+      FLAGS="-O3 -mt"
   else
-    check cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_C_COMPILER=icc -DCMAKE_Fortran_COMPILER=ifort \
-      -DMPI_C_INCLUDE_PATH="/usr/include" \
-      -DMPI_C_LIBRARIES="-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
-      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-      $BUILD_DIR/elpa_lib-$ELPA_VERSION
+      FLAGS="-g -mt"
   fi
-  check make VERBOSE=1 -j4
+  check ./configure FC=mpif90 CC=mpicc CXX=mpicxx SCALAPACK_LDFLAGS="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+	      FCFLAGS="$FLAGS" CFLAGS="$FLAGS" CXXFLAGS="$FLAGS" \
+	      --enable-openmp  --prefix=$PREFIX
+  check make -j4
   $SUDO make install
 done
 
