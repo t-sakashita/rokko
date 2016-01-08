e1824f4eb4132636e8bda6bc62dcb4d4d333ebfb
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-01-08 17:20:31 +0900

    use arabitrary dimension in example/cxx/dense/helmert_mpi.cpp

--- example/cxx/dense/helmert_mpi.cpp
+++ example/cxx/dense/helmert_mpi.cpp
@@ -11,6 +11,7 @@
 
 #include <rokko/rokko.hpp>
 #include <rokko/collective.hpp>
+#include <rokko/utility/solver_name.hpp>
 #include <rokko/utility/helmert_matrix.hpp>
 #include <boost/lexical_cast.hpp>
 #include <iostream>
@@ -22,17 +23,19 @@ int main(int argc, char *argv[]) {
   int provided;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
   MPI_Comm comm = MPI_COMM_WORLD;
-  std::string solver_name(rokko::parallel_dense_ev::default_solver());
+  std::string library_routine(rokko::parallel_dense_ev::default_solver());
+  std::string library, routine;
   int dim = 10;
-  if (argc >= 2) solver_name = argv[1];
+  if (argc >= 2) library_routine = argv[1];
   if (argc >= 3) dim = boost::lexical_cast<int>(argv[2]);
+  rokko::split_solver_name(library_routine, library, routine);
 
   rokko::grid g(comm);
   int myrank = g.get_myrank();
 
-  std::cout.precision(5);
+  std::cout.precision(8);
 
-  rokko::parallel_dense_ev solver(solver_name);
+  rokko::parallel_dense_ev solver(library);
   solver.initialize(argc, argv);
   if (myrank == 0)
     std::cout << "Eigenvalue decomposition of Helmert matrix" << std::endl
@@ -40,24 +43,27 @@ int main(int argc, char *argv[]) {
               #ifdef _OPENMP
               << "num_threads per process = " << omp_get_max_threads() << std::endl
               #endif
-              << "solver = " << solver_name << std::endl
+      	      << "library:routine = " << library_routine << std::endl
               << "dimension = " << dim << std::endl;
 
   rokko::distributed_matrix<double, matrix_major> mat(dim, dim, g, solver);
   //rokko::helmert_matrix::generate(mat);
   //mat.print();
   rokko::localized_vector<double> diag(dim);
-  diag << 1.2, 2.1, 3.7, 4.5, 8.3, 1.5, 9.2, 3.4, 5.4, 2.9;
+  //diag.setLinSpaced(diag.size(), diag.size(), 1); // diag = [1, 2, 3, ..., dim]
+  diag.setLinSpaced(diag.size(), 1, diag.size()); // diag = [1, 2, 3, ..., dim]
   rokko::helmert_matrix::generate_for_given_eigenvalues(mat, diag);
-  mat.print();
+  //mat.print();
 
   rokko::localized_matrix<double, matrix_major> mat_loc(dim, dim);
   rokko::gather(mat, mat_loc, 0);
 
   rokko::localized_vector<double> eigval(dim);
   rokko::distributed_matrix<double, matrix_major> eigvec(dim, dim, g, solver);
+  rokko::parameters params;
+  params.set("routine", routine);
   try {
-    solver.diagonalize(mat, eigval, eigvec);
+    solver.diagonalize(mat, eigval, eigvec, params);
   }
   catch (const char *e) {
     if (myrank == 0) std::cout << "Exception : " << e << std::endl;
@@ -68,7 +74,8 @@ int main(int argc, char *argv[]) {
   rokko::gather(eigvec, eigvec_loc, 0);
   if (myrank == 0) {
     std::cout << "largest eigenvalues:";
-    for (int i = 0; i < std::min(dim, 10); ++i) std::cout << ' ' << eigval(dim - 1 - i);
+    //for (int i = 0; i < std::min(dim, 10); ++i) std::cout << ' ' << eigval(dim - 1 - i);
+    for (int i = 0; i < dim; ++i) std::cout << ' ' << eigval(dim - 1 - i);
     std::cout << std::endl;
     std::cout << "residual of the largest eigenvalue/vector: |x A x - lambda| = "
               << std::abs(eigvec_loc.col(dim - 1).transpose() * mat_loc * eigvec_loc.col(dim - 1)
