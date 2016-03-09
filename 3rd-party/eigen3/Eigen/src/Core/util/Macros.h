042710a411df14d50a141f54ef6373663726ca6a
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-03-09 14:37:00 +0900

    update modification for Eigen3.2.8 to resolve compiling error

--- 3rd-party/eigen3/Eigen/src/Core/util/Macros.h
+++ 3rd-party/eigen3/Eigen/src/Core/util/Macros.h
@@ -298,8 +298,7 @@
   #define EIGEN_OS_SOLARIS 0
 #endif
 
-
-#if EIGEN_GNUC_AT_MOST(4,3) && !defined(__clang__)
+#if EIGEN_GNUC_AT_MOST(4,3) && !defined(__clang__) && defined(__FCC_VERSION)
   // see bug 89
   #define EIGEN_SAFE_TO_USE_STANDARD_ASSERT_MACRO 0
 #else
