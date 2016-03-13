dbdcc5760f2ea1b41e986ad3a405ebb2f3a669b4
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-03-13 23:55:06 +0900

    restore indexing in rokko_distributed_matrix_generate_from_array

--- rokko/distributed_matrix.f90
+++ rokko/distributed_matrix.f90
@@ -221,12 +221,12 @@ contains
     integer :: m_local, n_local, local_i, local_j, global_i, global_j
     m_local = rokko_distributed_matrix_get_m_local(matrix)
     n_local = rokko_distributed_matrix_get_n_local(matrix)
-    do local_i = 1, m_local
-       do local_j = 1, n_local
+    do local_i = 0, m_local-1
+       do local_j = 0, n_local-1
           global_i = rokko_distributed_matrix_translate_l2g_row(matrix, local_i)
           global_j = rokko_distributed_matrix_translate_l2g_col(matrix, local_j)
           call rokko_distributed_matrix_set_local(matrix, local_i, local_j, &
-               array(global_i, global_j))
+               array(global_i+1, global_j+1))
        enddo
     enddo
   end subroutine rokko_distributed_matrix_generate_from_array
