22c853f61ed52106fd336004dad0eea6e457365a
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-03-28 22:57:40 +0900

    fix copyright in Fortran examples

--- example/fortran/dense/frank_function_array_mpi.f90
+++ example/fortran/dense/frank_function_array_mpi.f90
@@ -1,13 +1,13 @@
-!/*****************************************************************************
-!*
-!* Rokko: Integrated Interface for libraries of eigenvalue decomposition
-!*
-!* Copyright (C) 2012-2016 Rokko Developers https://github.com/t-sakashita/rokko
-!*
-!* Distributed under the Boost Software License, Version 1.0. (See accompanying
-!* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-!*
-!*****************************************************************************/
+!*****************************************************************************
+!
+! Rokko: Integrated Interface for libraries of eigenvalue decomposition
+!
+! Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+!
+! Distributed under the Boost Software License, Version 1.0. (See accompanying
+! file LICENSE_1_0.txt or copy at http://www.boost.org/license_1_0.txt)
+!
+!*****************************************************************************
 
 module mod_frank
   use iso_c_binding
--- example/fortran/sparse/laplacian_mfree_mpi.f90
+++ example/fortran/sparse/laplacian_mfree_mpi.f90
@@ -1,3 +1,14 @@
+!*****************************************************************************
+!
+! Rokko: Integrated Interface for libraries of eigenvalue decomposition
+!
+! Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+!
+! Distributed under the Boost Software License, Version 1.0. (See accompanying
+! file LICENSE_1_0.txt or copy at http://www.boost.org/license_1_0.txt)
+!
+!*****************************************************************************
+
 module laplacian
   use mpi
   use rokko
