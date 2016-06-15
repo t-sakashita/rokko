368f5c0cc8d0330045d05e0b5243a87051a67388
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-06-15 18:39:14 +0900

    fix sample -> example

--- misc/rokko_build_script/job_frank_elemental_fx10_maki.sh
+++ misc/rokko_build_script/job_frank_elemental_fx10_maki.sh
@@ -4,12 +4,12 @@
 #PJM -L "node=4"
 #PJM --mpi "proc=4"
 #PJM -L "elapse=10:00"
-###PJM -o "${HOME}/sample_elemental.o"
+###PJM -o "${HOME}/example_elemental.o"
 #PJM -j
 
 #------- Program execution -------#
 export OMP_NUM_THREADS=8
-cd $HOME/build/rokko/sample_elemental
+cd $HOME/build/rokko/example_elemental
 
 mpiexec ./diagonalize_elemental
 
