286e573c160dce0069cae61ed401f1cf611afaaf
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-06-19 19:47:02 +0900

    add C exapmle for variable block size

---
+++ example/c/dense/frank_block_mpi.c
@@ -0,0 +1,77 @@
+/*****************************************************************************
+ *
+ * Rokko: Integrated Interface for libraries of eigenvalue decomposition
+ *
+ * Copyright (C) 2012-2014 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
+ *                            Synge Todo <wistaria@comp-phys.org>,
+ *                            Tsuyoshi Okubo <t-okubo@issp.u-tokyo.ac.jp>
+ *    
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ *****************************************************************************/
+
+#include <mpi.h>
+#include <rokko/rokko.h>
+#include <rokko/utility/frank_matrix_c.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int dim;
+  struct rokko_parallel_dense_ev solver;
+  struct rokko_grid grid;
+  struct rokko_mapping_bc map;
+  struct rokko_distributed_matrix mat, Z;
+  struct rokko_localized_vector w;
+  struct rokko_parameters params;
+  char* library_routine, *library, *routine;
+
+  int provided, ierr, myrank, nprocs, i;
+
+  ierr = MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  ierr = MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  library_routine = rokko_parallel_dense_ev_default_solver();
+  if (argc >= 2) library_routine = argv[1];
+  if (argc >= 3) dim = atoi(argv[2]);
+  rokko_split_solver_name(library_routine, &library, &routine);
+
+  if (myrank == 0) {
+    printf("library = %s\n", library);
+    printf("routine = %s\n", routine);
+    printf("dimension = %d\n", dim);
+  }
+  
+  rokko_parallel_dense_ev_construct(&solver, library, argc, argv);
+  rokko_grid_construct(&grid, MPI_COMM_WORLD, rokko_grid_row_major);
+  rokko_mapping_bc_construct_block_size(&map, dim, 1);
+  rokko_distributed_matrix_construct(&mat, map);
+  rokko_distributed_matrix_construct(&Z, map);
+  rokko_localized_vector_construct(&w, dim);
+
+  /* generate frank matrix */
+  rokko_frank_matrix_generate_distributed_matrix(mat);
+  rokko_distributed_matrix_print(mat);
+  
+  rokko_parameters_construct(&params);
+  rokko_parameters_set_string(params, "routine", routine);
+  rokko_parallel_dense_ev_diagonalize(solver, mat, w, Z, params);
+
+  if (myrank == 0) {
+    printf("Computed Eigenvalues =\n");
+    for (i = 0; i < dim; ++i)
+      printf("%30.20f\n", rokko_localized_vector_get(w, i));
+  }
+
+  rokko_distributed_matrix_destruct(&mat);
+  rokko_distributed_matrix_destruct(&Z);
+  rokko_localized_vector_destruct(&w);
+  rokko_parallel_dense_ev_destruct(&solver);
+  rokko_mapping_bc_destruct(&map);
+  rokko_grid_destruct(&grid);
+
+  MPI_Finalize();
+  return 0;
+}
