2d7f731bfd2c41e7544c6bb5720fff4f90b472b9
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-06-27 22:08:11 +0900

    add frank_only_eigvals_mpi.py

---
+++ example/python/dense/frank_only_eigvals_mpi.py
@@ -0,0 +1,42 @@
+#
+# Rokko: Integrated Interface for libraries of eigenvalue decomposition
+#
+# Copyright (C) 2015-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
+from mpi4py import MPI
+from rokko import *
+
+#solver = rokko_parallel_dense_ev("scalapack", 0, None)
+solver = rokko_parallel_dense_ev("elemental", 0, None)
+
+dim = 50
+
+rokko_grid_row_major = rokko.grid_row_major
+rokko_grid_col_major = rokko.grid_col_major
+rokko_matrix_col_major = rokko.matrix_col_major
+
+grid = rokko_grid(MPI.COMM_WORLD, rokko_grid_col_major)
+map = solver.default_mapping(dim, grid)
+mat = rokko_distributed_matrix(map)
+w = rokko_localized_vector(dim)
+
+rokko_generate_frank_matrix(mat)
+mat.show()
+
+
+params = rokko_parameters()
+solver.diagonalize(mat, w, params)
+
+if (MPI.COMM_WORLD.Get_rank() == 0):
+	print("Computed Eigenvalues =\n");
+
+	for i in range(0, dim):
+		print(w.get(i))
+
+# print "rank = ", MPI.COMM_WORLD.Get_rank()
+
+MPI.Finalize()
