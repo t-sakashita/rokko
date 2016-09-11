0e713ddb8f8f9b367165a61767187eca6de9d41c
Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>
2016-09-12 07:39:03 +0900

    fix install script of EigenExa to use SGIMPT-2.14

--- 3rd-party/install/EigenExa/intel-mkl-sgimpt.sh
+++ 3rd-party/install/EigenExa/intel-mkl-sgimpt.sh
@@ -7,30 +7,34 @@ set_prefix
 
 sh $SCRIPT_DIR/setup.sh
 
+cd $BUILD_DIR/EigenExa-$EIGENEXA_VERSION
+ln -s CSTAB.h_in C.c
+icc -E C.c | gawk '/^#/{ next }{print }' > CSTAB.h
+
 BUILD_TYPES="Release Debug"
 for build_type in $BUILD_TYPES; do
   PREFIX=$PREFIX_ROKKO/eigenexa-$EIGENEXA_VERSION-$EIGENEXA_RK_REVISION/$build_type
   cd $BUILD_DIR
   mkdir -p EigenExa-$EIGENEXA_VERSION-build-$build_type
   cd EigenExa-$EIGENEXA_VERSION-build-$build_type
-  if [ `which mpicxx > /dev/null 2>&1; echo $?` = 0 ]; then
+  if [ `which mpicc > /dev/null 2>&1; echo $?` = 0 ]; then
     check cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
+      -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 \
       -DCMAKE_C_FLAGS="-mt" -DCMAKE_Fortran_FLAGS="-mt" \
       -DMPI_C_COMPILER=mpicc -DMPI_Fortran_COMPILER=mpif90 \
-      -DMPI_C_INCLUDE_PATH="/home/app/mpt/mpt-2.12-p11218/include" -DMPI_Fortran_INCLUDE_PATH="/home/app/mpt/mpt-2.12-p11218/include" \
+      -DMPI_C_INCLUDE_PATH="/home/app/mpt/mpt-2.14-p11333/include" -DMPI_Fortran_INCLUDE_PATH="/home/app/mpt/mpt-2.14-p11333/include" \
       -DMPI_C_LIBRARIES="-mt" -DMPI_Fortran_LIBRARIES="-mt" \
-      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_sgimpt_lp64 -mkl=parallel" \
       $BUILD_DIR/EigenExa-$EIGENEXA_VERSION
   else
     check cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_C_COMPILER=icc -DCMAKE_Fortran_COMPILER=ifort \
-      -DMPI_C_INCLUDE_PATH="/usr/include" \
-      -DMPI_C_LIBRARIES="-lmpi" -DMPI_Fortran_LIBRARIES="-lmpi" \
-      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+      -DMPI_C_INCLUDE_PATH="/home/app/mpt/mpt-2.14-p11333/include" -DMPI_Fortran_INCLUDE_PATH="/home/app/mpt/mpt-2.14-p11333/include" \
+      -DMPI_C_LIBRARIES="-lmp_mt" -DMPI_Fortran_LIBRARIES="-lmpi_mt" \
+      -DSCALAPACK_LIB="-lmkl_scalapack_lp64 -lmkl_blacs_sgimpt_lp64 -mkl=parallel" \
       $BUILD_DIR/EigenExa-$EIGENEXA_VERSION
   fi
-  check make VERBOSE=1 -j4
+  check make VERBOSE=1 -j2
   $SUDO make install
 done
 
