8c3d915d43fb81139034a7bd9c71cd9690455bee
Tatsuya Sakashita <tatsuya.sakashita@apchem.nagoya-u.ac.jp>
2016-09-19 13:17:59 +0900

    add SCALAPACK_FCFLAGS to install script of ELPA

--- 3rd-party/install/ELPA/intel-mkl.sh
+++ 3rd-party/install/ELPA/intel-mkl.sh
@@ -18,7 +18,9 @@ for build_type in $BUILD_TYPES; do
   else
       FLAGS="-g"
   fi
-  check ./configure SCALAPACK_LDFLAGS="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+  check ./configure SCALAPACK_FCFLAGS="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+              SCALAPACK_LDFLAGS="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
+              CC=mpiicc FC=mpiifort \
 	      FCFLAGS=$FLAGS CFLAGS=$FLAGS CXXFLAGS=$FLAGS \
 	      --enable-openmp  --prefix=$PREFIX
   check make -j4
