0fdd18aaa9fec0c94e30f712b89bf59bb6a2c024
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-12-07 16:22:23 +0900

    Fixed for Intel compiler

--- 3rd-party/install/ELPA/intel-mkl.sh
+++ 3rd-party/install/ELPA/intel-mkl.sh
@@ -20,7 +20,7 @@ for build_type in $BUILD_TYPES; do
   fi
   check ./configure SCALAPACK_FCFLAGS="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
               SCALAPACK_LDFLAGS="-lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -mkl=parallel" \
-              CC=mpiicc FC=mpiifort \
+              CC=mpicc FC=mpif90 \
 	      FCFLAGS=$FLAGS CFLAGS=$FLAGS CXXFLAGS=$FLAGS \
 	      --enable-openmp  --prefix=$PREFIX
   check make -j4
