df5bf3685e7f6c1f5cce58431a05ce00d41661d7
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-06-23 09:38:10 +0900

    Added example for clmatrix and slmatrix

---
+++ example/lapack/heev_c.cpp
@@ -0,0 +1,49 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2017 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/blas.hpp>
+#include <rokko/lapack.hpp>
+#include <rokko/localized_vector.hpp>
+#include <rokko/localized_matrix.hpp>
+#include <boost/lexical_cast.hpp>
+
+int main(int argc, char** argv) {
+  int n = 5;
+  if (argc > 1) n = boost::lexical_cast<int>(argv[1]);
+
+  // generate matrix
+  rokko::clmatrix a = rokko::clmatrix::Random(n, n);
+  a += a.adjoint().eval(); // eval() is required to avoid aliasing issue
+  std::cout << "Matrix A: " << std::endl << a << std::endl;
+
+  // diagonalization
+  rokko::clmatrix u = a;
+  rokko::slvector w(n);
+  int info = rokko::lapack::heev('V', 'U', u, w);
+  if (info) throw std::runtime_error("Error: heev failed");
+ std::cout << "Eigenvalues: " << std::endl << w << std::endl;
+  std::cout << "Eigenvectors: " << std::endl << u << std::endl;
+
+  // orthogonality check
+  rokko::clmatrix check1 = u.adjoint() * u - rokko::clmatrix::Identity(n, n);
+  double norm1 = rokko::lapack::lange('F', check1);
+  std::cout << "|| U^t U - I || = " << norm1 << std::endl;
+  if (norm1 > 1e-10) throw std::runtime_error("Error: orthogonality check");
+
+  // eigenvalue check
+  rokko::clmatrix check2 = u.adjoint() * a * u;
+  for (int i = 0; i < n; ++i) check2(i, i) -= w(i);
+  double norm2 = rokko::lapack::lange('F', check2);
+  std::cout << "|| U^t A U - diag(w) || = " << norm2 << std::endl;
+  if (norm2 > 1e-10) throw std::runtime_error("Error: eigenvalue check");
+
+  return 0;
+}
---
+++ example/lapack/syev_s.cpp
@@ -0,0 +1,53 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2017 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/blas.hpp>
+#include <rokko/lapack.hpp>
+#include <rokko/localized_vector.hpp>
+#include <rokko/localized_matrix.hpp>
+#include <boost/lexical_cast.hpp>
+
+int main(int argc, char** argv) {
+  int n = 5;
+  if (argc > 1) n = boost::lexical_cast<int>(argv[1]);
+
+  // generate matrix
+  rokko::slmatrix a(n, n);
+  for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < n; ++i) {
+      a(i, j) = std::min(i, j) + 1;
+    }
+  }
+  std::cout << "Matrix A: " << std::endl << a << std::endl;
+
+  // diagonalization
+  rokko::slmatrix u = a;
+  rokko::slvector w(n);
+  int info = rokko::lapack::syev('V', 'U', u, w);
+  if (info) throw std::runtime_error("Error: syev failed");
+  std::cout << "Eigenvalues: " << std::endl << w << std::endl;
+  std::cout << "Eigenvectors: " << std::endl << u << std::endl;
+
+  // orthogonality check
+  rokko::slmatrix check1 = u.adjoint() * u - rokko::slmatrix::Identity(n, n);
+  double norm1 = rokko::lapack::lange('F', check1);
+  std::cout << "|| U^t U - I || = " << norm1 << std::endl;
+  if (norm1 > 1e-10) throw std::runtime_error("Error: orthogonality check");
+
+  // eigenvalue check
+  rokko::slmatrix check2 = u.adjoint() * a * u;
+  for (int i = 0; i < n; ++i) check2(i, i) -= w(i);
+  double norm2 = rokko::lapack::lange('F', check2);
+  std::cout << "|| U^t A U - diag(w) || = " << norm2 << std::endl;
+  if (norm2 > 1e-10) throw std::runtime_error("Error: eigenvalue check");
+
+  return 0;
+}
