0937b34fa3c9ee26960b1c85486691255d41def9
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-07-11 23:02:22 +0900

    Updated UseRokko file and skelton of user's application

--- config/UseRokko.cmake
+++ config/UseRokko.cmake
@@ -10,6 +10,9 @@ if(NOT ROKKO_USE_FILE_INCLUDED)
 
   # compilers and common options
   if(NOT PREVENT_ROKKO_COMPILERS)
+    set(CMAKE_BUILD_TYPE ${ROKKO_CMAKE_BUILD_TYPE} CACHE STRING "Type of build" FORCE)
+    message(STATUS "Build type: " ${CMAKE_BUILD_TYPE})
+
     set(CMAKE_C_COMPILER ${ROKKO_CMAKE_C_COMPILER} CACHE FILEPATH "C compiler." FORCE)
     set(CMAKE_C_FLAGS ${ROKKO_CMAKE_C_FLAGS} CACHE STRING "Flags used by the compiler during all build types." FORCE)
     set(CMAKE_C_FLAGS_DEBUG ${ROKKO_CMAKE_C_FLAGS_DEBUG} CACHE STRING "Flags used by the compiler during debug builds." FORCE)
@@ -22,20 +25,28 @@ if(NOT ROKKO_USE_FILE_INCLUDED)
     set(CMAKE_Fortran_FLAGS ${ROKKO_CMAKE_Fortran_FLAGS} CACHE STRING "Flags used by the compiler during all build types." FORCE)
     set(CMAKE_Fortran_FLAGS_DEBUG ${ROKKO_CMAKE_Fortran_FLAGS_DEBUG} CACHE STRING "Flags used by the compiler during debug builds." FORCE)
     set(CMAKE_Fortran_FLAGS_RELEASE ${ROKKO_CMAKE_Fortran_FLAGS_RELEASE} CACHE STRING "Flags used by the compiler during release builds." FORCE)
+    set(CMAKE_BUILD_TYPE ${ROKKO_CMAKE_BUILD_TYPE} CACHE STRING "Type of build" FORCE)
+
+    if(ROKKO_Fortran_BINDING)
+      enable_language(CXX C Fortran)
+    else(ROKKO_Fortran_BINDING)
+      enable_language(CXX C)
+    endif(ROKKO_Fortran_BINDING)
+
+    # OpenMP
+    find_package(OpenMP)
+    if(OPENMP_FOUND)
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+      set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${OpenMP_C_FLAGS}")
+    endif(OPENMP_FOUND)
+
+    # MPI library
+    find_package(MPI)
+    set(CMAKE_EXE_LINKER_FLAGS ${MPI_CXX_LINK_FLAGS})
+    set(ROKKO_LIBRARIES ${ROKKO_LIBRARIES} ${MPI_CXX_LIBRARIES})
+
   endif(NOT PREVENT_ROKKO_COMPILERS)
-
-  # OpenMP
-  find_package(OpenMP)
-  if(OPENMP_FOUND)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${OpenMP_C_FLAGS}")
-  endif(OPENMP_FOUND)
-
-  # MPI library
-  find_package(MPI)
-  set(CMAKE_EXE_LINKER_FLAGS ${MPI_CXX_LINK_FLAGS})
-  set(ROKKO_LIBRARIES ${ROKKO_LIBRARIES} ${MPI_CXX_LIBRARIES})
 
   # Add include directories needed to use ROKKO and dependent libraries
   include_directories(${MPI_CXX_INCLUDE_PATH} ${MPI_Fortran_INCLUDE_PATH} ${ROKKO_INCLUDE_DIR} ${ROKKO_Boost_INCLUDE_DIR} ${ROKKO_EIGENEXA_INCLUDE_DIR} ${ROKKO_ELEMENTAL_INCLUDE_DIR} ${ROKKO_ANASAZI_INCLUDE_DIR} ${ROKKO_PETSC_INCLUDE_DIR} ${ROKKO_SLEPC_INCLUDE_DIR})
---
+++ tutorial/skel/dsyev.c
@@ -0,0 +1,81 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2017 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <cblas.h>
+#include <lapacke.h>
+#include <cmatrix.h>
+
+int imax(int x, int y) { return (x > y) ? x : y; }
+
+int main(int argc, char** argv) {
+  int n = 5;
+  int i, j, info;
+  double norm;
+  double *w;
+  double **a, **u, **t;
+
+  if (argc > 1) n = atoi(argv[1]);
+
+  /* generate matrix */
+  a = alloc_dmatrix(n, n);
+  for (j = 0; j < n; ++j) {
+    for (i = 0; i < n; ++i) {
+      mat_elem(a, i, j) = n - imax(i, j);
+    }
+  }
+  printf("Matrix A: ");
+  fprint_dmatrix(stdout, n, n, a);
+
+  /* diagonalization */
+  u = alloc_dmatrix(n, n);
+  cblas_dcopy(n * n, mat_ptr(a), 1, mat_ptr(u), 1);
+  w = alloc_dvector(n);
+  info = LAPACKE_dsyev(LAPACK_COL_MAJOR, 'V', 'U', n, mat_ptr(u), n, vec_ptr(w));
+  if (info != 0) {
+    fprintf(stderr, "Error: dsyev fails with error code %d\n", info);
+    exit(255);
+  }
+  printf("Eigenvalues: ");
+  fprint_dvector(stdout, n, w);
+  printf("Eigenvectors: ");
+  fprint_dmatrix(stdout, n, n, u);
+
+  /* orthogonality check */
+  t = alloc_dmatrix(n, n);
+  cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, n, n, n, 1, mat_ptr(u), n,
+              mat_ptr(u), n, 0, mat_ptr(t), n);
+  for (i = 0; i < n; ++i) mat_elem(t, i, i) -= 1;
+  norm = LAPACKE_dlange(LAPACK_COL_MAJOR, 'F', n, n, mat_ptr(t), n);
+  printf("|| U^t U - I || = %e\n", norm);
+  if (norm > 1e-10) {
+    fprintf(stderr, "Error: orthogonality check\n");
+    exit(255);
+  }
+
+  /* eigenvalue check */
+  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1, mat_ptr(a), n,
+              mat_ptr(u), n, 0, mat_ptr(t), n);
+  cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, n, n, n, 1, mat_ptr(u), n,
+              mat_ptr(t), n, 0, mat_ptr(a), n);
+  for (i = 0; i < n; ++i) mat_elem(a, i, i) -= w[i];
+  norm = LAPACKE_dlange(LAPACK_COL_MAJOR, 'F', n, n, mat_ptr(a), n);
+  printf("|| U^t A U - diag(w) || = %e\n", norm);
+  if (norm > 1e-10) {
+    fprintf(stderr, "Error: eigenvalue check\n");
+    exit(255);
+  }
+
+  free_dmatrix(a);
+  free_dmatrix(u);
+  free_dvector(w);
+  free_dmatrix(t);
+  return 0;
+}
