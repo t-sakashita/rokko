ff27bb29ed8dec5fdd753c2cf0b2eccdf120c8b0
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-07-21 20:49:27 +0900

    update URL of Elemental

--- 3rd-party/install/Elemental/setup.sh
+++ 3rd-party/install/Elemental/setup.sh
@@ -10,7 +10,7 @@ rm -rf Elemental-$ELEMENTAL_VERSION*
 if [ -f $SOURCE_DIR/Elemental-$ELEMENTAL_VERSION.tgz ]; then
   check tar zxf $SOURCE_DIR/Elemental-$ELEMENTAL_VERSION.tgz
 else
-  check wget -O - http://libelemental.org/pub/releases/Elemental-$ELEMENTAL_VERSION.tgz | tar zxf -
+  check wget -O - https://github.com/elemental/Elemental/releases/download/$ELEMENTAL_VERSION/Elemental-$ELEMENTAL_VERSION.tgz | tar zxf -
 fi
 cd $BUILD_DIR/Elemental-$ELEMENTAL_VERSION
 # patch -p1 < $SCRIPT_DIR/elemental-$ELEMENTAL_VERSION.patch
