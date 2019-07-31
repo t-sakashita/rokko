f9c48f582a39e1e591931ca87d945f51cf808fc1
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-07-31 19:24:34 +0900

    Update no_rokko for EigenExa-2.4b

--- benchmark/no_rokko/dense_frank_mpi/eigen_exa.f90
+++ benchmark/no_rokko/dense_frank_mpi/eigen_exa.f90
@@ -1,7 +1,54 @@
+      subroutine mat_set(n, a, nm)
+
+      use eigen_libs_mod
+      use mpi
+!$    use omp_lib
+
+      implicit none
+
+      integer, intent(in)    :: n, nm
+      real(8), intent(out)   :: a(1:nm,*)
+
+      real(8), parameter     :: ZERO= 0.0D0
+      real(8), parameter     :: ONE = 1.0D0
+
+      integer                :: COMM, x_COMM, y_COMM
+      integer                :: nnod, x_nnod, y_nnod
+      integer                :: inod, x_inod, y_inod
+      integer                :: iloop_sta, iloop_end
+      integer                :: jloop_sta, jloop_end
+      integer                :: i, i_1
+      integer                :: j, j_1
+
+      call eigen_get_comm (COMM, x_COMM, y_COMM)
+      call eigen_get_procs(nnod, x_nnod, y_nnod)
+      call eigen_get_id   (inod, x_inod, y_inod)
+
+      jloop_sta = eigen_loop_start(1, x_nnod, x_inod)
+      jloop_end = eigen_loop_end  (n, x_nnod, x_inod)
+      iloop_sta = eigen_loop_start(1, y_nnod, y_inod)
+      iloop_end = eigen_loop_end  (n, y_nnod, y_inod)
+
+      if (iloop_sta <= iloop_end) then
+!$OMP PARALLEL DO &
+!$OMP& PRIVATE(i,j,i_1,j_1)
+         do i_1 = iloop_sta, iloop_end
+            i = eigen_translate_l2g(i_1, y_nnod, y_inod)
+            do j_1 = jloop_sta, jloop_end
+               j = eigen_translate_l2g(j_1, x_nnod, x_inod)
+               a(j_1, i_1) = (n+1-max(i,j)) * ONE
+            end do
+         end do
+!$OMP END PARALLEL DO
+      end if
+
+      end subroutine  mat_set
+
+
 program EigenExa
-  use eigen_libs
+  use eigen_libs_mod
+  use mpi
   implicit none
-  include 'mpif.h'
   double precision   init_tick, gen_tick, diag_tick, end_tick
   integer :: n, m, nm, ny, mtype, i, i_inod, ierr
   real(8), pointer :: a(:,:),z(:,:),w(:)
@@ -23,7 +70,7 @@ program EigenExa
   gen_tick = mpi_wtime()
   call eigen_get_matdims( n, nm, ny )
   allocate( a(nm, ny), z(nm, ny), w(n))
-  call mat_set( n, a(1,1), nm, 3 ) ! Frank matrix: mtype=3
+  call mat_set( n, a, nm )
 
   diag_tick = mpi_wtime()
   call eigen_sx( n, n, a, nm, w, z, nm, 48, 128, 'A')
