ddc7f1b3e73467798ac4e4f955b96aa34a373f4b
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-09-11 14:40:59 +0900

    Add copyright

--- test/python/split_sover_name.py
+++ test/python/split_sover_name.py
@@ -1,3 +1,12 @@
+#
+# Rokko: Integrated Interface for libraries of eigenvalue decomposition
+#
+# Copyright (C) 2015-2019 by Rokko Developers https://github.com/t-sakashita/rokko
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
 from pyrokko import *
 
 library, routine = split_solver_name("scalapack:pdsyevd")
