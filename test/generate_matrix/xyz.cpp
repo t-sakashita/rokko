89957b0f0763bf2ee8bc4556b55d365da4de3529
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-09-30 13:09:25 +0900

    Fix spelling mistake

--- test/generate_matrix/xyz.cpp
+++ test/generate_matrix/xyz.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -82,7 +82,7 @@ int main() {
   if (diagonal == mat2.diagonal()) {
     std::cout << "OK: diagonal by 'fill_diagonal' equals to diagonal elementas of a matrix by 'genertate'."<< std::endl;
   } else {
-    std::cout << "ERROR: diagonal by 'fill_diagonal' is differnet from diagonal elementas of a matrix by 'genertate'."<< std::endl;
+    std::cout << "ERROR: diagonal by 'fill_diagonal' is differnet from diagonal elements of a matrix by 'genertate'."<< std::endl;
     exit(1);
   }
 
