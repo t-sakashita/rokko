2fb2fdb1568f749baa6577b3ec183d0b5957d604
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-09-30 22:01:06 +0900

    Use Google Test in localized_matrix.cpp

--- test/localized_matrix_fixed_size.cpp
+++ test/localized_matrix_fixed_size.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2013-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2013-2019 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,14 +12,9 @@
 #include <rokko/localized_matrix.hpp>
 #include <rokko/localized_vector.hpp>
 
-#define BOOST_TEST_MODULE test_distributed_matrix
-#ifndef BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
-#else
-#include <boost/test/unit_test.hpp>
-#endif
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_CASE(test_localized_matrix) {
+TEST(localized_matrix, fixed_size) {
 
   // M = 1 2 3
   //     4 5 6
@@ -49,12 +44,17 @@ BOOST_AUTO_TEST_CASE(test_localized_matrix) {
   v << 4,5,6;
 
   rokko::localized_vector<double> w = a*u+M*v;
-  BOOST_CHECK_EQUAL(w[0],37.);
-  BOOST_CHECK_EQUAL(w[1],87.);
-  BOOST_CHECK_EQUAL(w[2],137.);
+  ASSERT_EQ(w[0], 37.);
+  ASSERT_EQ(w[1], 87.);
+  ASSERT_EQ(w[2], 137.);
 
   // u^t M = 30 36 42
-  BOOST_CHECK_EQUAL( (u.transpose()*M.col(0)), 30.);
-  BOOST_CHECK_EQUAL( (u.transpose()*M.col(1)), 36.);
-  BOOST_CHECK_EQUAL( (u.transpose()*M.col(2)), 42.);
+  ASSERT_EQ( (u.transpose()*M.col(0)), 30.);
+  ASSERT_EQ( (u.transpose()*M.col(1)), 36.);
+  ASSERT_EQ( (u.transpose()*M.col(2)), 42.);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
