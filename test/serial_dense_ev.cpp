6785d5cadd40749cf82a6144879cdb74a237ed78
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-09-30 21:52:14 +0900

    Use Google Test in serial_dense_ev.cpp

--- test/serial_dense_ev.cpp
+++ test/serial_dense_ev.cpp
@@ -2,25 +2,24 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2014-2019 by Synge Todo <wistaria@comp-phys.org>
+* Copyright (C) 2012-2019 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 *****************************************************************************/
 
-
 #include <rokko/solver.hpp>
 
-#define BOOST_TEST_MODULE test_serial_dense_evs
-#ifndef BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
-#else
-#include <boost/test/unit_test.hpp>
-#endif
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_CASE(test_serial_dense_evs) {
+TEST(serial_dense_ev, names) {
   for(auto name : rokko::serial_dense_ev::solvers()) {
     std::cerr << name << std::endl;
   }
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
