81410c677bcf9933f8b9fd1644613d8a407e589c
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-09-30 12:04:45 +0900

    Use Google Test in generate_matrix/frank.cpp

--- test/generate_matrix/frank.cpp
+++ test/generate_matrix/frank.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2013-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2013-2019 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,21 +11,17 @@
 
 #include <rokko/rokko.hpp>
 #include <rokko/utility/frank_matrix.hpp>
-#define BOOST_TEST_MODULE test_generator
-#ifndef BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
-#else
-#include <boost/test/unit_test.hpp>
-#endif
+
+#include <gtest/gtest.h>
 
 template<typename T, typename MATRIX_MAJOR>
 void test(int dim) {
   rokko::localized_matrix<T, MATRIX_MAJOR> mat(dim, dim);
   rokko::frank_matrix::generate(mat);  
-  BOOST_CHECK_CLOSE(mat.trace(), dim * (dim+1) * 0.5, 10e-5);
+  EXPECT_NEAR(mat.trace(), dim * (dim+1) * 0.5, 10e-5);
 }
 
-BOOST_AUTO_TEST_CASE(test_generator) {
+TEST(matrix_generator, frank_matrix) {
   const int dim = 100;
   std::cout << "dimension = " << dim << std::endl;
 
@@ -33,4 +29,11 @@ BOOST_AUTO_TEST_CASE(test_generator) {
   test<double, rokko::matrix_row_major>(dim);
   std::cout << "  test for column major" << std::endl;
   test<double, rokko::matrix_col_major>(dim);
+}
+
+int main(int argc, char** argv) {
+  int result = 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  result = RUN_ALL_TESTS();
+  return result;
 }
