e5c6146d22bbff75011370ca0e0402be7f2db814
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-10-01 11:40:26 +0900

    Use Google Test in mpi_thread_multiple.cpp

--- test/mpi_thread_multiple.cpp
+++ test/mpi_thread_multiple.cpp
@@ -2,35 +2,30 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2013 by Tatsuya Sakashita <t-sakashita@issp.u-tokyo.ac.jp>,
-*                            Synge Todo <wistaria@comp-phys.org>
+* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 *****************************************************************************/
 
-#define BOOST_TEST_MODULE test_mpi_thread_multiple
-#ifndef BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
-#else
-#include <boost/test/unit_test.hpp>
-#endif
-
 #include <mpi.h>
 #include <iostream>
 
-BOOST_AUTO_TEST_CASE(test_mpi_thread_multiple) {
+#include <gtest/gtest.h>
+
+int global_argc;
+char** global_argv;
+
+TEST(mpi, mpi_thread_multiple) {
   const int required = MPI_THREAD_MULTIPLE;
   int provided;
-  MPI_Init_thread(&boost::unit_test::framework::master_test_suite().argc,
-                  &boost::unit_test::framework::master_test_suite().argv,
-                  required, &provided);
-  // BOOST_CHECK_EQUAL(provided, required);
-  if (provided == required) {
-    std::cerr << "MPI_THREAD_MULTIPLE is supported\n";
-  } else {
-    std::cerr << "MPI_THREAD_MULTIPLE is NOT supported\n";
-  }
+  MPI_Init_thread(&global_argc, &global_argv, required, &provided);
+  ASSERT_EQ(provided, required) << "MPI_THREAD_MULTIPLE is NOT supported";
   MPI_Finalize();
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
