cf669f03823b3823092dd3390e742541287869f7
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-10-01 10:26:35 +0900

    Fix Google Test in gather.cpp

--- test/gather.cpp
+++ test/gather.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2013-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2013-2019 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -13,13 +13,12 @@
 #include <rokko/collective.hpp>
 #include <rokko/utility/frank_matrix.hpp>
 
-#define BOOST_TEST_MODULE test_gather
-#ifndef BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
-#else
-#include <boost/test/unit_test.hpp>
-#endif
 #include <random>
+
+#include <gtest/gtest.h>
+
+int global_argc;
+char** global_argv;
 
 template<typename GRID_MAJOR, typename DIST_MAT_MAJOR, typename LOC_MAT_MAJOR>
 bool run_test(MPI_Comm comm, int dim, GRID_MAJOR const& grid_major, DIST_MAT_MAJOR const&, LOC_MAT_MAJOR const&) {
@@ -66,25 +65,22 @@ bool run_test(MPI_Comm comm, int dim, GRID_MAJOR const& grid_major, DIST_MAT_MAJ
 
   int success;
   MPI_Allreduce(&success_local, &success, 1, MPI_INT, MPI_PROD, comm);
-  BOOST_CHECK_EQUAL(success, true);
   return success;
 }
 
-BOOST_AUTO_TEST_CASE(test_gather) {
-  MPI_Init(&boost::unit_test::framework::master_test_suite().argc,
-           &boost::unit_test::framework::master_test_suite().argv);
+TEST(mpi_communication, gather) {
   MPI_Comm comm = MPI_COMM_WORLD;
   int rank;
   MPI_Comm_rank(comm, &rank);
 
   int dim = 100;
-  if (boost::unit_test::framework::master_test_suite().argc > 1) {
-    dim = boost::lexical_cast<int>(boost::unit_test::framework::master_test_suite().argv[1]);
+  if (global_argc > 1) {
+    dim = boost::lexical_cast<int>(global_argv[1]);
   }
   if (rank == 0) std::cout << "dimension = " << dim << std::endl;
 
   if (rank == 0) std::cout << "test for grid = col, dist = col, loc = col\n";
-  run_test(comm, dim, rokko::grid_col_major, rokko::matrix_col_major(), rokko::matrix_col_major());
+  ASSERT_TRUE( run_test(comm, dim, rokko::grid_col_major, rokko::matrix_col_major(), rokko::matrix_col_major()) );
 
   /*
   if (rank == 0) std::cout << "test for grid = col, dist = col, loc = row\n";
@@ -98,7 +94,7 @@ BOOST_AUTO_TEST_CASE(test_gather) {
   */
 
   if (rank == 0) std::cout << "test for grid = row, dist = col, loc = col\n";
-  run_test(comm, dim, rokko::grid_row_major, rokko::matrix_col_major(), rokko::matrix_col_major());
+  ASSERT_TRUE( run_test(comm, dim, rokko::grid_row_major, rokko::matrix_col_major(), rokko::matrix_col_major()) );
 
   /*
   if (rank == 0) std::cout << "test for grid = row, dist = col, loc = row\n";
@@ -110,6 +106,15 @@ BOOST_AUTO_TEST_CASE(test_gather) {
   if (rank == 0) std::cout << "test for grid = row, dist = row, loc = row\n";
   run_test(comm, dim, rokko::grid_row_major, rokko::matrix_row_major(), rokko::matrix_row_major());
   */
+}
 
+int main(int argc, char** argv) {
+  int result = 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
+  global_argc = argc;
+  global_argv = argv;
+  result = RUN_ALL_TESTS();
   MPI_Finalize();
+  return result;
 }
