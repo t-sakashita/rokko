0770b7f063ce6c638fa38f0edc32f2568f88c4c1
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-10-09 21:56:32 +0900

    Add test program matrix_major_template_transfomation.cpp

---
+++ test/generate_matrix/matrix_major_template_transfomation.cpp
@@ -0,0 +1,24 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2014-2019 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/eigen3.hpp>
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+TEST(matrix, major_template_transformation) {
+  static_assert( std::is_same<rokko::matrix_major<Eigen::MatrixXd>, rokko::matrix_col_major>::value, "rokko::matrix_major<Eigen::MatrixXd> != rokko::matrix_col_major");
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
