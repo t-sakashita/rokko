97068e16f6e78c423e64becf6746c17d25b18978
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-10-12 12:54:30 +0900

    Add C/Fortran interfaces & examples for minij

--- rokko/eigen_matrix.F90
+++ rokko/eigen_matrix.F90
@@ -166,6 +166,20 @@ module rokko_eigen_matrix_mod
      end subroutine rokko_frank_matrix_generate_eigen_matrix
   end interface
 
+  ! rokko_minij_matrix for eigen_matrix
+  interface rokko_minij_matrix_generate
+     procedure rokko_minij_matrix_generate_eigen_matrix
+  end interface rokko_minij_matrix_generate
+
+  interface
+     subroutine rokko_minij_matrix_generate_eigen_matrix(matrix) bind(c)
+       use iso_c_binding
+       import rokko_eigen_matrix
+       implicit none
+       type(rokko_eigen_matrix), value, intent(in) :: matrix
+     end subroutine rokko_minij_matrix_generate_eigen_matrix
+  end interface
+
 contains
 
   subroutine rokko_eigen_matrix_generate_function(matrix, func_in)
