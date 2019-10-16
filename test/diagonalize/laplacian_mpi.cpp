d61cfb19773d7c9e861fb7248fc4c8cf1efb89eb
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-10-16 14:13:33 +0900

    Add test/diagonalize/minij_mpi.cpp & laplacian_mpi.cpp

---
+++ test/diagonalize/laplacian_mpi.cpp
@@ -0,0 +1,85 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2013-2019 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/rokko.hpp>
+#include <rokko/utility/laplacian_matrix.hpp>
+
+#include <gtest/gtest.h>
+
+int global_argc;
+char** global_argv;
+
+TEST(diagonalize, laplacian_mpi) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  const int dim = 10;
+
+  std::vector<std::string> names;
+  if (global_argc == 1) {
+    names = rokko::parallel_dense_ev::solvers();
+  } else {
+    for (int num=1; num < global_argc; ++num) {
+      names.push_back(global_argv[num]);
+    }
+  }
+
+  for(auto name : names) {
+    std::cout << "solver=" << name << std::endl;
+    rokko::parallel_dense_ev solver(name);
+    if (solver.is_available_grid_major(rokko::grid_col_major)) {
+      solver.initialize(global_argc, global_argv);
+      rokko::grid g(comm, rokko::grid_col_major);
+      rokko::mapping_bc<rokko::matrix_col_major> map = solver.default_mapping(dim, g);
+      rokko::distributed_matrix<double, rokko::matrix_col_major> mat(map);
+      rokko::laplacian_matrix::generate(mat);
+      Eigen::VectorXd w(dim);
+      rokko::distributed_matrix<double, rokko::matrix_col_major> Z(map);
+
+      solver.diagonalize(mat, w, Z);
+
+      EXPECT_NEAR(w.array().inverse().sum(), dim * (dim+1) * 0.5, 10e-5);
+      
+      solver.finalize();
+    }
+  }
+
+
+  for(auto name : names) {
+    std::cout << "solver=" << name << std::endl;
+    rokko::parallel_dense_ev solver(name);
+    if (solver.is_available_grid_major(rokko::grid_row_major)) {
+      solver.initialize(global_argc, global_argv);
+      rokko::grid g(comm, rokko::grid_row_major);
+      rokko::mapping_bc<rokko::matrix_col_major> map = solver.default_mapping(dim, g);
+      rokko::distributed_matrix<double, rokko::matrix_col_major> mat(map);
+      rokko::laplacian_matrix::generate(mat);
+      Eigen::VectorXd w(dim);
+      rokko::distributed_matrix<double, rokko::matrix_col_major> Z(map);
+      
+      solver.diagonalize(mat, w, Z);
+
+      EXPECT_NEAR(w.array().inverse().sum(), dim * (dim+1) * 0.5, 10e-5);
+
+      solver.finalize();
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  int result = 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  global_argc = argc;
+  global_argv = argv;
+  result = RUN_ALL_TESTS();
+  MPI_Finalize();
+  return result;
+}
