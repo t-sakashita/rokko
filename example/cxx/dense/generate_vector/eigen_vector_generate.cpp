9a12836930b3a450c7ce50e313bf138a601776e9
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-10-22 12:38:32 +0900

    Add generate function for Eigen3's vector

---
+++ example/cxx/dense/generate_vector/eigen_vector_generate.cpp
@@ -0,0 +1,24 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/eigen3/generate_vector.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  const auto f = [](int i) { return double(2*i); };
+
+  unsigned int dim = 10;
+  Eigen::VectorXd vec(dim);
+  rokko::generate(vec, f);
+
+  std::cout << "vec:\n"
+            << vec.transpose() << std::endl;
+}
---
+++ rokko/eigen3/generate_vector.hpp
@@ -0,0 +1,36 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#ifndef ROKKO_EIGEN3_GENERATE_VECTOR_HPP
+#define ROKKO_EIGEN3_GENERATE_VECTOR_HPP
+
+#include <iostream>
+#include <rokko/eigen3.hpp>
+
+namespace rokko {
+
+template<typename T, int ROWS, class FUNC>
+void generate(Eigen::Vector<T,ROWS>& vec, FUNC func) {
+  for(int i = 0; i < vec.size(); ++i) {
+    vec(i) = func(i);
+  }
+}
+
+template<typename T, int ROWS>
+void generate(Eigen::Vector<T,ROWS>& vec, std::function<T(int)> const& func) {
+  for(int i = 0; i < vec.size(); ++i) {
+    vec(i) = func(i);
+  }
+}
+
+} // namespace rokko
+
+#endif // ROKKO_EIGEN3_GENERATE_VECTOR_HPP
