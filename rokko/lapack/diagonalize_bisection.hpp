0de8171b7c2cca0259bfe1ade0a2d44b74075914
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-11-04 13:52:37 +0900

    Call diagonalize_dsyevx in diagonalize_bisection

--- rokko/lapack/diagonalize_bisection.hpp
+++ rokko/lapack/diagonalize_bisection.hpp
@@ -16,6 +16,7 @@
 #include <rokko/eigen3.hpp>
 #include <rokko/utility/timer.hpp>
 #include <rokko/lapack.hpp>
+#include <rokko/lapack/diagonalize_dsyevx.hpp>
 
 namespace rokko {
 namespace lapack {
@@ -24,48 +25,20 @@ namespace lapack {
 template<int MATRIX_MAJOR>
 parameters diagonalize_bisection(Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,MATRIX_MAJOR>& mat, double* eigvals,
 				 rokko::parameters const& params) {
-  rokko::parameters params_out;
-  const char jobz = 'N';  // only eigenvalues
-  const int dim = mat.outerSize();
-  const int ldim_mat = mat.innerSize();
-  lapack_int m;  // output: found eigenvalues
-  double abstol = params.defined("abstol") ? params.get<double>("abstol") : 2*LAPACKE_dlamch('S');
-  if (abstol < 0) {
-    std::stringstream msg;
-    msg << "lapack::diagonalize_bisection() : " << std::endl
-	<< "abstol is negative value, which means QR method." << std::endl
-	<< "To use dsyevx as bisection solver, set abstol a positive value" << std::endl;
-    throw std::invalid_argument(msg.str());
-  }
-  params_out.set("abstol", abstol);
-  const char uplow = get_matrix_part(params);
-
-  lapack_int il, iu;
-  double vl, vu;
-  const char range = get_eigenvalues_range(params, vl, vu, il, iu);
-
-  std::vector<lapack_int> ifail(dim);
-  int info;
-  if(MATRIX_MAJOR == Eigen::ColMajor)
-    info = LAPACKE_dsyevx(LAPACK_COL_MAJOR, jobz, range, uplow, dim, &mat(0,0), ldim_mat, vl, vu, il, iu, abstol, &m, eigvals, NULL, ldim_mat, &ifail[0]);
-  else
-    info = LAPACKE_dsyevx(LAPACK_ROW_MAJOR, jobz, range, uplow, dim, &mat(0,0), ldim_mat, vl, vu, il, iu, abstol, &m, eigvals, NULL, ldim_mat, &ifail[0]);
-  if (info) {
-    std::stringstream msg;
-    msg << "lapack::diagonalize_bisection() : " << std::endl
-        << "error at dsyevx function. info=" << info << std::endl;
-    if (info < 0) {
-      msg << "This means that "
-          << "the " << abs(info) << "-th argument had an illegal value." << std::endl;
+  if (params.defined("abstol")) {
+    if (params.get<double>("abstol") < 0) {
+      std::stringstream msg;
+      msg << "lapack::diagonalize_bisection() : " << std::endl
+          << "abstol is negative value, which means QR method." << std::endl
+          << "To use dsyevx as bisection solver, set abstol a positive value" << std::endl;
+      throw std::invalid_argument(msg.str());
     }
-    throw std::invalid_argument(msg.str());
   }
-  params_out.set("info", info);
-  params_out.set("m", m);
-  params_out.set("ifail", ifail);
-  
+
+  parameters params_out = diagonalize_dsyevx(mat, eigvals, params);
+
   if (params.get_bool("verbose")) {
-    print_verbose("dsyevx (bisection)", jobz, range, uplow, vl, vu, il, iu, params_out);
+    std::cout << "finished dsyevx (bisection)" << std::endl;
   }
 
   return params_out;
@@ -77,61 +50,20 @@ template<int MATRIX_MAJOR>
 parameters diagonalize_bisection(Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,MATRIX_MAJOR>& mat, double* eigvals,
 				 Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,MATRIX_MAJOR>& eigvecs,
 				 parameters const& params) {
-  rokko::parameters params_out;
-  const char jobz = 'V';  // eigenvalues / eigenvectors
-  const int dim = mat.outerSize();
-  const int ldim_mat = mat.innerSize();
-  const int ldim_eigvec = eigvecs.innerSize();
-  std::vector<lapack_int> ifail(dim);
-
-  lapack_int m;  // output: found eigenvalues
-  double abstol = params.defined("abstol") ? params.get<double>("abstol") : 2*LAPACKE_dlamch('S');
-  if (abstol < 0) {
-    std::stringstream msg;
-    msg << "lapack::diagonalize_bisection() : " << std::endl
-	<< "abstol is negative value, which means QR method." << std::endl
-	<< "To use dsyevx as bisection solver, set abstol a positive value" << std::endl;
-    throw std::invalid_argument(msg.str());
-  }
-  params_out.set("abstol", abstol);
-  char uplow = get_matrix_part(params);
-
-  lapack_int il, iu;
-  double vl, vu;
-  const char range = get_eigenvalues_range(params, vl, vu, il, iu);
-
-  int info;
-  if(MATRIX_MAJOR == Eigen::ColMajor)
-    info = LAPACKE_dsyevx(LAPACK_COL_MAJOR, jobz, range, uplow, dim, &mat(0,0), ldim_mat, vl, vu, il, iu, abstol, &m, eigvals, &eigvecs(0,0), ldim_eigvec, &ifail[0]);
-  else
-    info = LAPACKE_dsyevx(LAPACK_ROW_MAJOR, jobz, range, uplow, dim, &mat(0,0), ldim_mat, vl, vu, il, iu, abstol, &m, eigvals, &eigvecs(0,0), ldim_eigvec, &ifail[0]);
-
-  if (info) {
-    std::stringstream msg;
-    msg << "lapack::diagonalize_bisection() : "
-        << "error at dsyevx function. info=" << info << std::endl;
-    if (params.get_bool("verbose")) {
-      msg << "This means that ";
-      if (info < 0) {
-        msg << "the " << abs(info) << "-th argument had an illegal value." << std::endl;
-      } else {
-        msg << "This means that " << info << " eigenvectors failed to converge." << std::endl;
-        msg << "The indices of the eigenvectors that failed to converge:" << std::endl;
-        for (std::size_t i = 0; i < ifail.size(); ++i) {
-          if (ifail[i] == 0) break;
-          msg << ifail[i] << " ";
-        }
-        msg << std::endl;
-      }
+  if (params.defined("abstol")) {
+    if (params.get<double>("abstol") < 0) {
+      std::stringstream msg;
+      msg << "lapack::diagonalize_bisection() : " << std::endl
+          << "abstol is negative value, which means QR method." << std::endl
+          << "To use dsyevx as bisection solver, set abstol a positive value" << std::endl;
+      throw std::invalid_argument(msg.str());
     }
-    throw std::invalid_argument(msg.str());
   }
-  params_out.set("info", info);
-  params_out.set("m", m);
-  params_out.set("ifail", ifail);
-  
+
+  parameters params_out = diagonalize_dsyevx(mat, eigvals, eigvecs, params);
+
   if (params.get_bool("verbose")) {
-    print_verbose("dsyevx (bisecition)", jobz, range, uplow, vl, vu, il, iu, params_out);
+    std::cout << "finished dsyevx (bisection)" << std::endl;
   }
 
   return params_out;
