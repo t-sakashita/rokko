d09c1141a9f65786dee7a84e65bebe51c05f9532
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-11-08 09:44:21 +0900

    Use OpenMP_Fortran_FLAGS, instead of OpenMP_C_FLAGS in use-rokko.cmake

--- config/use-rokko.cmake
+++ config/use-rokko.cmake
@@ -38,7 +38,7 @@ if(NOT ROKKO_USE_FILE_INCLUDED)
     if(OPENMP_FOUND)
       set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-      set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${OpenMP_C_FLAGS}")
+      set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${OpenMP_Fortran_FLAGS}")
     endif(OPENMP_FOUND)
 
     # MPI library
