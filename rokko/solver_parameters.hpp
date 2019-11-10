d8d24d43b268745a12baf97c3300ba4d09b6b864
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-11-10 10:29:02 +0900

    Simplify to calculate true/false in is_rokko_solver_key

--- rokko/solver_parameters.hpp
+++ rokko/solver_parameters.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2019 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -24,13 +24,7 @@ char (*ARRAY_SIZE_(T (*)[N]))[N];
 static const std::vector<std::string> rokko_solver_keys{ "num_eigenvalues", "routine" };
 
 bool is_rokko_solver_key(std::string const& key) {
-  if (std::find(rokko_solver_keys.begin(), rokko_solver_keys.end(), key) != rokko_solver_keys.end()) {
-    //std::cout << key << " is rokko key" << std::endl;
-    return true;
-  } else {
-    //std::cout << key << " is NOT rokko key" << std::endl;
-    return false;
-  }
+  return std::find(rokko_solver_keys.begin(), rokko_solver_keys.end(), key) != rokko_solver_keys.end();
 }
 
 } // end namespace rokko
