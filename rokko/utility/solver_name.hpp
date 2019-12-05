c3581f43e3dc4eb3b6644a1c2c9db9caf66ec502
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-05 10:50:32 +0900

    Replace boost::algorithm::split by std::sregex_token_iterator

--- rokko/utility/solver_name.hpp
+++ rokko/utility/solver_name.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,24 +12,25 @@
 #ifndef ROKKO_UTILITY_SOLVER_NAME_HPP
 #define ROKKO_UTILITY_SOLVER_NAME_HPP
 
-#include <iostream>
+#include <regex>
 #include <vector>
-#include <boost/algorithm/string.hpp>
 
 namespace rokko {
 
 void split_solver_name(std::string const& str, std::string& library, std::string& routine) {
-  std::vector<std::string> v;
-  boost::algorithm::split(v, str, boost::is_any_of(":"));
-  if (v.size() > 2) {
-    throw std::invalid_argument("split_solver_name() : More than colon were detected from given string.");
-  }
-  if (v.size() >= 1)
-    library = v[0];
-  if (v.size() == 2)
-    routine = v[1];
+  std::regex separator{":"};
+  auto it = std::sregex_token_iterator{str.begin(), str.end(), separator, -1};
+  auto end_it = std::sregex_token_iterator{};
+  if (it != end_it)
+    library = *it++;
+
+  if (it != end_it)
+    routine = *it++;
+
+  if (it != end_it)
+    throw std::invalid_argument("split_solver_name() : More than one colon were detected from given string.");
 }
-    
+
 } // namespace rokko
 
 #endif // ROKKO_UTILITY_SOLVER_NAME_HPP
