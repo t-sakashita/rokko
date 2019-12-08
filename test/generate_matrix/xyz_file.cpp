4f5f603f1090f1664eb2427193eb3e59bd0bbec5
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-08 16:29:54 +0900

    Fix {

--- test/generate_matrix/xyz_file.cpp
+++ test/generate_matrix/xyz_file.cpp
@@ -17,8 +17,7 @@
 #include <rokko/utility/xyz_hamiltonian.hpp>
 #include <rokko/eigen3.hpp>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc <= 1) {
     std::cerr << "error: " << argv[0] << " xyz.ip" << std::endl;
     exit(1);
