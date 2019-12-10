5a66d4c5da20a03fdc0e52f7a4322a30b9a8d917
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-10 09:35:12 +0900

    Define wrapper for MPI_Scatter&MPI_Scatterv and use it in test laplacian_mpi.cpp

---
+++ rokko/utility/mpi_vector.hpp
@@ -0,0 +1,89 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#ifndef ROKKO_UTILITY_MPI_VECTOR_HPP
+#define ROKKO_UTILITY_MPI_VECTOR_HPP
+
+#include <vector>
+#include <rokko/eigen3.hpp>
+
+namespace rokko {
+
+class mpi_vector {
+public:
+  mpi_vector(int dim, MPI_Comm comm_in = MPI_COMM_WORLD, int root_in = 0) : comm(comm_in), root(root_in) {
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    divisible = (dim % nprocs) == 0;
+    set_num_local_rows(dim);
+    set_counts_displacements(dim);
+  }
+
+  int get_num_local_rows(int dim, int rank) const {
+    return (dim + nprocs - rank - 1) / nprocs;
+  }
+
+  int get_num_local_rows(int dim) const {
+    return (dim + nprocs - myrank - 1) / nprocs;
+  }
+
+  void set_num_local_rows(int dim) {
+    num_local_rows = get_num_local_rows(dim);
+  }
+
+  void set_counts_displacements(int dim) {
+    counts.resize(nprocs);
+    displacements.resize(nprocs);
+    int offset = 0;
+    for (int proc=0; proc<nprocs; ++proc) {
+      displacements[proc] = offset;
+      counts[proc] = get_num_local_rows(dim, proc);
+      offset += counts[proc];
+    }
+  }
+
+  template<int SIZE>
+  void scatter(const Eigen::Vector<double, SIZE>& source,
+               Eigen::Vector<double, SIZE>& target) const {
+    if (divisible)
+      MPI_Scatter(source.data(), num_local_rows, MPI_DOUBLE, target.data(), num_local_rows, MPI_DOUBLE,
+                  root, comm);
+    else
+      MPI_Scatterv(source.data(), counts.data(), displacements.data(), MPI_DOUBLE,
+                   target.data(), num_local_rows, MPI_DOUBLE,
+                   root, comm);
+  }
+
+  template<int SIZE>
+  void gather(const Eigen::Vector<double, SIZE>& source,
+              Eigen::Vector<double, SIZE>& target) const {
+    if (divisible)
+      MPI_Gather(source.data(), num_local_rows, MPI_DOUBLE, target.data(), num_local_rows, MPI_DOUBLE,
+                 root, comm);
+    else
+      MPI_Gatherv(source.data(), num_local_rows, MPI_DOUBLE,
+                  target.data(), counts.data(), displacements.data(), MPI_DOUBLE,
+                  root, comm);
+  }
+
+private:
+  MPI_Comm comm;
+  int myrank, nprocs;
+  int num_local_rows;
+  std::vector<int> counts, displacements;
+  bool divisible;
+  int root;
+};
+
+} // namespace rokko
+
+#endif // ROKKO_UTILITY_MPI_VECTOR_HPP
--- test/generate_matrix/laplacian_mpi.cpp
+++ test/generate_matrix/laplacian_mpi.cpp
@@ -12,113 +12,39 @@
 #include <iostream>
 #include <vector>
 
-#include <rokko/solver.hpp>
-#include <rokko/grid.hpp>
-#include <rokko/distributed_mfree.hpp>
-
-#include <rokko/collective.hpp>
-
 #include <rokko/rokko.hpp>
 #include <rokko/utility/laplacian_matrix.hpp>
-
-class laplacian_op : public rokko::distributed_mfree {
-public:
-  laplacian_op(int dim) : dim_(dim) {
-    comm_ = MPI_COMM_WORLD;
-    MPI_Comm_size(comm_, &nprocs);
-    MPI_Comm_rank(comm_, &myrank);
-
-    int tmp = dim_ / nprocs;
-    int rem = dim % nprocs;
-    num_local_rows_ = (dim + nprocs - myrank - 1) / nprocs;
-    start_row_ = tmp * myrank + std::min(rem, myrank);
-    end_row_ = start_row_ + num_local_rows_ - 1;
-
-    if (start_row_ == 0)  is_first_proc = true;
-    else is_first_proc = false;
-    
-    if (end_row_ == (dim-1))  is_last_proc = true;
-    else is_last_proc = false;
-
-    end_k_ = num_local_rows_ - 1;
-    
-    std::cout << "myrank=" << myrank << " start_row=" << start_row_ << " end_row=" << end_row_ << std::endl;
-    std::cout << "myrank=" << myrank << " num_local_rows_=" << num_local_rows_ << std::endl;
-  }
-  ~laplacian_op() {}
-
-  void multiply(const double* x, double* y) const {
-    MPI_Status status;
-    if (!is_first_proc) {
-      //std::cout << "recv myrank=" << myrank << std::endl;
-      MPI_Send(x, 1, MPI_DOUBLE, myrank-1, 0, comm_);
-      MPI_Recv(&buf, 1, MPI_DOUBLE, myrank-1, 0, comm_, &status);
-      y[0] = - buf + 2 * x[0] - x[1];
-    }
-    else { // for the first process 0
-      y[0] = x[0] - x[1];
-    }
-    if (!is_last_proc) {
-      //std::cout << "send myrank=" << myrank << std::endl;
-      MPI_Send(&x[end_k_], 1, MPI_DOUBLE, myrank+1, 0, comm_);
-      MPI_Recv(&buf, 1, MPI_DOUBLE, myrank+1, 0, comm_, &status);
-      y[end_k_] = - x[end_k_ - 1] + 2 * x[end_k_] - buf;      
-    }
-    else { // for the last process
-      y[end_k_] = 2 * x[end_k_] - x[end_k_ - 1];
-    }
- 
-    for (int k=1; k<end_k_; ++k) {  //  from 1 to end-1
-      y[k] = - x[k-1] + 2 * x[k] - x[k+1];
-    }
-  }
-  int get_dim() const { return dim_; }
-  int get_local_offset() const { return local_offset_; }
-  int get_num_local_rows() const { return num_local_rows_; }
-  int get_start_row() const { return start_row_; }
-  int get_end_row() const { return end_row_; }
-
-private:
-  MPI_Comm comm_;
-  int nprocs, myrank;
-  int dim_, local_offset_, num_local_rows_;
-  int start_row_, end_row_;
-  int start_k_, end_k_;
-  mutable double buf;
-  bool is_first_proc, is_last_proc;
-};
+#include <rokko/utility/laplacian_mfree.hpp>
+#include <rokko/utility/mpi_vector.hpp>
 
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
-  rokko::parallel_dense_ev solver;
-  solver.initialize(argc, argv);
-  rokko::grid g(MPI_COMM_WORLD);
-
-
   int myrank, nprocs;
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   constexpr int root = 0;
 
-  // creating column vectors which forms a heisenberg hamiltonian.
   constexpr int N_seq = 20;
-  int N = N_seq / nprocs;
-  Eigen::VectorXd v_seq(N_seq), w_seq(N_seq), recv_buffer(N_seq);
+  const int N_seq_proc = (myrank == root) ? N_seq : 0;
+  Eigen::VectorXd v_seq(N_seq_proc), w_seq(N_seq_proc), recv_buffer(N_seq_proc);
+
+  rokko::laplacian_mfree op(N_seq);
+  const int N = op.get_num_local_rows();
   Eigen::VectorXd v(N), w(N);
-  laplacian_op op(N_seq);
+
+  rokko::mpi_vector mpi(N_seq);
+
   for (int i=0; i<N_seq; ++i) {
     if (myrank == root) {
       v_seq.setZero();
       v_seq(i) = 1;
       w_seq.setZero();
       rokko::laplacian_matrix::multiply(N_seq, v_seq.data(), w_seq.data());
-      std::cout << "sequential version:" << std::endl;
-      std::cout << w_seq.transpose() << std::endl;
     }
     MPI_Barrier(MPI_COMM_WORLD);
 
     // MPI version
-    MPI_Scatter(v_seq.data(), N, MPI_DOUBLE, v.data(), N, MPI_DOUBLE, root, MPI_COMM_WORLD);
+    mpi.scatter(v_seq, v);
     w.setZero();
     op.multiply(v.data(), w.data());
     for (int proc=0; proc<nprocs; ++proc) {
@@ -130,11 +56,17 @@ int main(int argc, char *argv[]) {
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Gather(w.data(), N, MPI_DOUBLE, recv_buffer.data(), N, MPI_DOUBLE, root, MPI_COMM_WORLD);
+    mpi.gather(w, recv_buffer);
     MPI_Barrier(MPI_COMM_WORLD);
     if (myrank == 0) {
       std::cout << "i=" << i << std::endl;
+      std::cout << "w_seq=" << w_seq.transpose() << std::endl;
       std::cout << "recv=" << recv_buffer.transpose() << std::endl;
+      if (w_seq != recv_buffer) {
+        std::cout << "ERROR: w_seq != recv_buffer" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 22);
+        exit(1);
+      }
     }
     MPI_Barrier(MPI_COMM_WORLD);
   }
