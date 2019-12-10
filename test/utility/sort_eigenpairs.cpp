5338d77354974ef130d838d6e9b4195ad554ba3b
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-10 17:10:39 +0900

    Fix display format to 'i='

--- test/utility/sort_eigenpairs.cpp
+++ test/utility/sort_eigenpairs.cpp
@@ -36,7 +36,7 @@ void test(bool ascending) {
   }
   rokko::sort_eigenpairs(eigvals, eigvecs, eigvals_sorted, eigvecs_sorted, ascending);
   for(int i=0; i<num; ++i){
-    std::cout << "dim: " << i << std::endl;
+    std::cout << "i=" << i << std::endl;
     const double e = ascending ? 1.0 * i : 1.0*(num-i-1);
     if (MAJOR == Eigen::RowMajor) {
       ASSERT_EQ( eigvals_sorted(i), e);
