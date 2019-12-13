eb2c60d448ceae93a687b94d57e1b782e0c4c431
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-13 09:41:15 +0900

    Cont. Add minij.cpp in test/generate_matrix

---
+++ test/generate_matrix/minij.cpp
@@ -0,0 +1,41 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2013-2019 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/rokko.hpp>
+#include <rokko/utility/minij_matrix.hpp>
+
+#include <gtest/gtest.h>
+
+constexpr double eps = 1e-5;
+
+template<typename T, typename MATRIX_MAJOR>
+void test(int dim) {
+  Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,rokko::eigen3_major<MATRIX_MAJOR>> mat(dim, dim);
+  rokko::minij_matrix::generate(mat);
+  EXPECT_NEAR(mat.trace(), dim * (dim+1) * 0.5, eps);
+}
+
+TEST(generate_matrix, minij_matrix) {
+  const int dim = 100;
+  std::cout << "dimension = " << dim << std::endl;
+
+  std::cout << "  test for row major" << std::endl;
+  test<double, rokko::matrix_row_major>(dim);
+  std::cout << "  test for column major" << std::endl;
+  test<double, rokko::matrix_col_major>(dim);
+}
+
+int main(int argc, char** argv) {
+  int result = 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  result = RUN_ALL_TESTS();
+  return result;
+}
