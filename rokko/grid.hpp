4a612c206918b2e22d04dee26dfe6a9b448aafba
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-22 11:44:22 +0900

    Fix MPI_Comm& to MPI_Comm for simplicity

--- rokko/grid.hpp
+++ rokko/grid.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2019 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -48,7 +48,7 @@ public:
   }
 
   template <typename GRID_MAJOR>
-  grid(MPI_Comm& comm_in, std::array<int,2> size_in, GRID_MAJOR const& grid_major = grid_row_major) : comm(comm_in) {
+  grid(MPI_Comm comm_in, std::array<int,2> size_in, GRID_MAJOR const& grid_major = grid_row_major) : comm(comm_in) {
     MPI_Comm_size(comm, &nprocs);
     if ((std::get<0>(size_in) * std::get<1>(size_in)) != nprocs) {
       throw std::invalid_argument("grid::grid() : (rows * cols) != nprocs");
