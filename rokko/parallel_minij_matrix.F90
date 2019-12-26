4f7a413a3a75a005f179efbd07227af8979a015f
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-26 17:53:54 +0900

    Continued: Declare Fortran interfaces for Frank and minij in new files

--- rokko/parallel_minij_matrix.F90
+++ rokko/parallel_minij_matrix.F90
@@ -9,25 +9,25 @@
 !
 !*****************************************************************************
 
-module rokko_serial_minij_matrix_mod
-  use rokko_eigen_matrix_mod
+module rokko_parallel_minij_matrix_mod
+  use rokko_distributed_matrix_mod
   implicit none
 
   interface rokko_minij_matrix_generate
-     procedure rokko_minij_matrix_generate_eigen_matrix
+     procedure rokko_minij_matrix_generate_distributed_matrix
   end interface rokko_minij_matrix_generate
 
   interface
 
-     subroutine rokko_minij_matrix_generate_eigen_matrix(matrix) bind(c)
+     subroutine rokko_minij_matrix_generate_distributed_matrix(matrix) bind(c)
        use iso_c_binding
-       import rokko_eigen_matrix
+       import rokko_distributed_matrix
        implicit none
-       type(rokko_eigen_matrix), value, intent(in) :: matrix
-     end subroutine rokko_minij_matrix_generate_eigen_matrix
+       type(rokko_distributed_matrix), value, intent(in) :: matrix
+     end subroutine rokko_minij_matrix_generate_distributed_matrix
 
   end interface
 
 contains
 
-end module rokko_serial_minij_matrix_mod
+end module rokko_parallel_minij_matrix_mod
--- rokko/serial_frank_matrix.F90
+++ rokko/serial_frank_matrix.F90
@@ -9,25 +9,25 @@
 !
 !*****************************************************************************
 
-module rokko_serial_minij_matrix_mod
+module rokko_serial_frank_matrix_mod
   use rokko_eigen_matrix_mod
   implicit none
 
-  interface rokko_minij_matrix_generate
-     procedure rokko_minij_matrix_generate_eigen_matrix
-  end interface rokko_minij_matrix_generate
+  interface rokko_frank_matrix_generate
+     procedure rokko_frank_matrix_generate_eigen_matrix
+  end interface rokko_frank_matrix_generate
 
   interface
 
-     subroutine rokko_minij_matrix_generate_eigen_matrix(matrix) bind(c)
+     subroutine rokko_frank_matrix_generate_eigen_matrix(matrix) bind(c)
        use iso_c_binding
        import rokko_eigen_matrix
        implicit none
        type(rokko_eigen_matrix), value, intent(in) :: matrix
-     end subroutine rokko_minij_matrix_generate_eigen_matrix
+     end subroutine rokko_frank_matrix_generate_eigen_matrix
 
   end interface
 
 contains
 
-end module rokko_serial_minij_matrix_mod
+end module rokko_serial_frank_matrix_mod
