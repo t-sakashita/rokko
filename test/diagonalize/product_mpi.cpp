06884e93fccd7dc85f90683d10c26fcdc4c39581
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2019-12-27 15:00:24 +0900

    Rewrite test product_mpi.cpp

--- test/diagonalize/product_mpi.cpp
+++ test/diagonalize/product_mpi.cpp
@@ -35,29 +35,22 @@ TEST(product_mpi, product_mpi) {
   rokko::grid g(comm);
   rokko::mapping_bc<rokko::matrix_col_major> map = solver.default_mapping(dim, g);
   rokko::distributed_matrix<double, rokko::matrix_col_major> matA(map);
-  rokko::distributed_matrix<double, rokko::matrix_col_major> matB(map);
   rokko::distributed_matrix<double, rokko::matrix_col_major> matC(map);
   rokko::frank_matrix::generate(matA);
-  rokko::frank_matrix::generate(matB);
-  rokko::product(1.0, matA, false, matB, false, 0, matC);
+  rokko::product(1.0, matA, false, matA, false, 0, matC);
   matC.print();
   // calculate trace
-  double sum_local = 0;
-  for (int i = 0; i < dim; ++i) {
-    if (matC.is_gindex({i, i})) sum_local += matC.get_global(i, i);
+  double sum_global = rokko::trace(matC);
+  if (rank == 0) {
+    std::cout << "trace of distributed matrix = " << sum_global << std::endl;
+    Eigen::MatrixXd lmatA(dim, dim);
+    rokko::frank_matrix::generate(lmatA);
+    const Eigen::MatrixXd lmatC = lmatA * lmatA;
+    std::cout << lmatC << std::endl;
+    const double sum = lmatC.trace();
+    std::cout << "trace of eigen matrix = " << sum << std::endl;
+    EXPECT_NEAR(sum_global, sum, eps);
   }
-  double sum_global = 0;
-  MPI_Allreduce(&sum_local, &sum_global, 1, MPI_DOUBLE, MPI_SUM, comm);
-  if (rank == 0) std::cout << "trace of distributed matrix = " << sum_global << std::endl;
-
-  Eigen::MatrixXd lmatA(dim, dim);
-  rokko::frank_matrix::generate(lmatA);
-  Eigen::MatrixXd lmatC = lmatA * lmatA;
-  if (rank == 0) std::cout << lmatC << std::endl;
-  double sum = lmatC.trace();
-  if (rank == 0) std::cout << "trace of eigen matrix = " << sum << std::endl;
-
-  if (rank == 0) EXPECT_NEAR(sum_global, sum, eps);
 }
 
 int main(int argc, char** argv) {
