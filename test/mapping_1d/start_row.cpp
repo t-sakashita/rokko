24596251393ae856d32b5c1df5f293e5ef57fb55
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-01-17 09:41:39 +0900

    Add test/mapping_1d/start_row.cpp

---
+++ test/mapping_1d/start_row.cpp
@@ -0,0 +1,43 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/skel/mapping_1d.hpp>
+
+#include <gtest/gtest.h>
+
+int global_argc;
+char** global_argv;
+
+int start_row(rokko::skel::mapping_1d const& map) {
+  int index = 0;
+  for (int proc=0; proc<map.get_mpi_comm().get_myrank(); ++proc)
+    index += map.calculate_num_local_rows(proc);
+  return index;
+}
+
+TEST(mapping_1d, start_row) {
+  constexpr int dim = 100;
+  rokko::skel::mapping_1d map(dim);
+
+  ASSERT_EQ(map.start_row(), start_row(map));
+}
+
+int main(int argc, char** argv) {
+  int result = 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  global_argc = argc;
+  global_argv = argv;
+  result = RUN_ALL_TESTS();
+  MPI_Finalize();
+  return result;
+}
