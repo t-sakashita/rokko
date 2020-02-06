976c7fe88fbbcef75b323a9fdaffa33888a5edda
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-07 08:32:10 +0900

    Remove include guards from .cpp

--- rokko/eigen3/eigen3.cpp
+++ rokko/eigen3/eigen3.cpp
@@ -2,19 +2,14 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2014 by Synge Todo <wistaria@comp-phys.org>,
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 *****************************************************************************/
 
-#ifndef ROKKO_EIGEN3_CPP
-#define ROKKO_EIGEN3_CPP
-
 #include <rokko/serial_dense_ev.hpp>
 #include <rokko/eigen3/core.hpp>
 
 ROKKO_REGISTER_SERIAL_DENSE_SOLVER(rokko::eigen3::solver, "eigen3", 10)
-
-#endif
--- rokko/lapack/lapack.cpp
+++ rokko/lapack/lapack.cpp
@@ -2,19 +2,14 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2014 by Synge Todo <wistaria@comp-phys.org>,
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 *****************************************************************************/
 
-#ifndef ROKKO_LAPACK_CPP
-#define ROKKO_LAPACK_CPP
-
 #include <rokko/serial_dense_ev.hpp>
 #include <rokko/lapack/core.hpp>
 
 ROKKO_REGISTER_SERIAL_DENSE_SOLVER(rokko::lapack::solver, "lapack", 20)
-
-#endif
