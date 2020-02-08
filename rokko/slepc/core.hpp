57ea7b6c336e2364c8bda3b738329e92b923cd17
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-08 13:17:39 +0900

    Update copyright year

--- rokko/slepc/core.hpp
+++ rokko/slepc/core.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -195,6 +195,7 @@ private:
 };
 
 } // namespace slepc
+
 } // namespace rokko
 
 #endif // ROKKO_SLEPC_CORE_HPP
