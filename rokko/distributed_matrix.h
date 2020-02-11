11d1685edcdac53576a172bcb5b01324f5baf887
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-11 19:53:06 +0900

    Remove unnecessary '#include <rokko/matrix_major.h>'

--- rokko/distributed_matrix.h
+++ rokko/distributed_matrix.h
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -13,7 +13,6 @@
 #define ROKKO_DISTRIBUTED_MATRIX_H
 
 #include <rokko/mapping_bc.h>
-#include <rokko/matrix_major.h>
 
 #ifdef __cplusplus
 extern "C" {
--- rokko/serial_dense.h
+++ rokko/serial_dense.h
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -15,8 +15,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <rokko/matrix_major.h>
 
 #include <rokko/eigen_matrix.h>
 #include <rokko/eigen_vector.h>
