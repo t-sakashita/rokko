7619584f2cfcc13cf73bc34b0bbe0bafc3594178
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-11 20:28:42 +0900

    Revise rokko.h

--- rokko/rokko.h
+++ rokko/rokko.h
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2015 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -14,17 +14,11 @@
 #ifdef __cplusplus
 
 #include <rokko/solver.hpp>
-#if defined(ROKKO_HAVE_PARALLEL_SPARSE_SOLVER)
-# include <rokko/distributed_vector.hpp>
-#endif /* defined(ROKKO_HAVE_PARALLEL_SPARSE_SOLVER) */
 
 #else /* __cplus */
 
 #include <rokko/solver_name.h>
 #include <rokko/dense.h>
-#if defined(ROKKO_HAVE_PARALLEL_SPARSE_SOLVER)
-# include <rokko/grid.h>
-# include <rokko/sparse.h>
-#endif
+#include <rokko/sparse.h>
 
 #endif /* __cplus */
