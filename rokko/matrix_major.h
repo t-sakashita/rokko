6e113f5b26eae015fc96367a6c859a35f3141224
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-11 14:50:28 +0900

    Create separated file matrix_major.h

--- rokko/matrix_major.h
+++ rokko/matrix_major.h
@@ -2,30 +2,26 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2016 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 *****************************************************************************/
 
-#ifndef ROKKO_SERIAL_DENSE_H
-#define ROKKO_SERIAL_DENSE_H
+#ifndef ROKKO_MATRIX_MAJOR_H
+#define ROKKO_MATRIX_MAJOR_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <rokko/matrix_major.h>
-
-#include <rokko/eigen_matrix.h>
-#include <rokko/eigen_vector.h>
-
-#include <rokko/serial_dense_ev.h>
-
+enum {
+  rokko_matrix_col_major = 3, rokko_matrix_row_major = 4
+};
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* ROKKO_SERIAL_DENSE_H */
+#endif /* ROKKO_MATRIX_MAJOR_H */
