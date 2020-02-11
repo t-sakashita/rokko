90940b73260e0d3f75b173f2a2ea0d03b017cc77
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-11 12:56:59 +0900

    Add #include in mpi_vector.hpp

--- rokko/utility/mpi_vector.hpp
+++ rokko/utility/mpi_vector.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -14,6 +14,7 @@
 
 #include <vector>
 #include <rokko/eigen3.hpp>
+#include <rokko/distributed_vector.hpp>
 #include <rokko/mpi/mpi_type_traits.hpp>
 
 namespace rokko {
