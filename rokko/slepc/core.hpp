1a922f5cc8a2f9c2b511dcb034deb5589889eebf
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-02-29 21:10:50 +0900

    Move SLEPc's multiply & diagonal to static member functions

--- rokko/slepc/core.hpp
+++ rokko/slepc/core.hpp
@@ -112,6 +112,43 @@ public:
     return diagonalize_common(A, params);
   }
 
+  static PetscErrorCode MatMult_myMat(Mat A, Vec x, Vec y) {
+    PetscFunctionBeginUser;
+    PetscErrorCode ierr;
+
+    rokko::distributed_mfree_slepc *op_ctx;
+    ierr = MatShellGetContext(A, &op_ctx); CHKERRQ(ierr);
+
+    PetscScalar const* px;
+    PetscScalar * py;
+
+    ierr = VecGetArrayRead(x, &px);  CHKERRQ(ierr);
+    ierr = VecGetArray(y, &py);  CHKERRQ(ierr);
+
+    op_ctx->multiply(px, py);
+
+    ierr = VecRestoreArrayRead(x,&px);  CHKERRQ(ierr);
+    ierr = VecRestoreArray(y,&py);  CHKERRQ(ierr);
+
+    PetscFunctionReturn(0);
+  }
+
+  static PetscErrorCode MatGetDiagonal_myMat(Mat A, Vec diag) {
+    PetscFunctionBeginUser;
+    PetscErrorCode ierr;
+
+    rokko::distributed_mfree_slepc *op_ctx;
+    ierr = MatShellGetContext(A, &op_ctx); CHKERRQ(ierr);
+
+    PetscScalar *pd;
+
+    ierr = VecGetArray(diag, &pd); CHKERRQ(ierr);
+    op_ctx->diagonal(pd);
+    ierr = VecRestoreArray(diag ,&pd); CHKERRQ(ierr);
+
+    PetscFunctionReturn(0);
+  }
+
   parameters diagonalize_common(Mat const& A, rokko::parameters const& params) {
     parameters params_out;
 
--- rokko/slepc/distributed_mfree.hpp
+++ rokko/slepc/distributed_mfree.hpp
@@ -20,47 +20,6 @@ namespace rokko {
 
 namespace slepc {
 
-#undef __FUNCT__
-#define __FUNCT__ "MatMult_myMat"
-PetscErrorCode MatMult_myMat(Mat A, Vec x, Vec y) {
-  PetscFunctionBeginUser;
-  PetscErrorCode ierr;
-  
-  rokko::distributed_mfree_slepc *op_ctx;
-  ierr = MatShellGetContext(A, &op_ctx); CHKERRQ(ierr);
-  
-  PetscScalar const* px;
-  PetscScalar * py;
-
-  ierr = VecGetArrayRead(x, &px);  CHKERRQ(ierr);
-  ierr = VecGetArray(y, &py);  CHKERRQ(ierr);
-  
-  op_ctx->multiply(px, py);
-  
-  ierr = VecRestoreArrayRead(x,&px);  CHKERRQ(ierr);
-  ierr = VecRestoreArray(y,&py);  CHKERRQ(ierr);
-  
-  PetscFunctionReturn(0);
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "MatGetDiagonal_myMat"
-PetscErrorCode MatGetDiagonal_myMat(Mat A, Vec diag) {
-  PetscFunctionBeginUser;
-  PetscErrorCode ierr;
-
-  rokko::distributed_mfree_slepc *op_ctx;
-  ierr = MatShellGetContext(A, &op_ctx); CHKERRQ(ierr);
-
-  PetscScalar *pd;
-  
-  ierr = VecGetArray(diag, &pd); CHKERRQ(ierr);
-  op_ctx->diagonal(pd);
-  ierr = VecRestoreArray(diag ,&pd); CHKERRQ(ierr);
-  
-  PetscFunctionReturn(0);
-}
-
 } // end namespace slepc
 
 } // end namespace rokko
