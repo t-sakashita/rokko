fd593868adf44dba8578ffb10fec080a28442f0c
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-03-07 12:54:01 +0900

    Add check_nprocs in heisenberg_mfree

--- rokko/utility/heisenberg_hamiltonian_mfree.hpp
+++ rokko/utility/heisenberg_hamiltonian_mfree.hpp
@@ -22,9 +22,17 @@ class heisenberg_mfree : public rokko::distributed_mfree_default {
 public:
   heisenberg_mfree(int L, const std::vector<std::pair<int, int>>& lattice, MPI_Comm comm = MPI_COMM_WORLD)
     : L_(L), lattice_(lattice), distributed_mfree_default{1 << L, rokko::mpi_comm{comm}} {
+    check_nprocs(rokko::mpi_comm{comm});
     buffer_.resize(get_num_local_rows());
   }
   ~heisenberg_mfree() = default;
+
+  void check_nprocs(rokko::mpi_comm const& mpi_comm) const {
+    const int nprocs = mpi_comm.get_nprocs();
+    const int p = rokko::find_power_of_two(nprocs);
+    if (nprocs != (1 << p))
+      throw std::invalid_argument("This program can be run only with 2^n MPI processes");
+  }
 
   void multiply(const double *const x, double *const y) const override {
     rokko::heisenberg_hamiltonian::multiply(get_comm(), L_, lattice_, x, y, buffer_.data());
