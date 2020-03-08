0c71d28e1635b40b77c487ca29c7457c9ed13089
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-03-08 13:24:06 +0900

    Add rqcg in test/sparse/laplacian_crs|mfree_mpi

--- test/sparse/laplacian_mfree_mpi.cpp
+++ test/sparse/laplacian_mfree_mpi.cpp
@@ -39,7 +39,7 @@ void run_test(std::string const& library, MPI_Comm comm) {
     if (library == "anasazi")
       routines = { {"lobpcg", "largest_real"}, {"block_krylov_schur", "largest"}, {"block_davidson", "largest_real"}, {"rtr", "largest_real"} };
     else if (library == "slepc")
-      routines = { {"krylovschur", "largest"}, {"lanczos", "largest"}, {"lobpcg", "largest_real"}, {"subspace", "largest"} };  // excludes not converging "power"
+      routines = { {"krylovschur", "largest"}, {"lanczos", "largest"}, {"lobpcg", "largest_real"}, {"rqcg", "smallest_real"}, {"subspace", "largest"} };  // excludes not converging "power"
 
     for (auto routine : routines) {
       std::cout << "routine=" << routine[0] << std::endl;
@@ -55,7 +55,8 @@ void run_test(std::string const& library, MPI_Comm comm) {
         throw std::runtime_error("num_conv=0: solver did not converge");
 
       double eigval = solver.eigenvalue(0);
-      double th_eigval = rokko::laplacian_matrix::eigenvalue(dim, dim-1);  // largest one
+      double th_eigval = (routine[0] == "rqcg") ? rokko::laplacian_matrix::eigenvalue(dim, 0)  // smallest one
+        : rokko::laplacian_matrix::eigenvalue(dim, dim-1);  // largest one
       EXPECT_NEAR(eigval, th_eigval, eigval*eps);
     }
   } // end if comm != MPI_COMM_NULL
