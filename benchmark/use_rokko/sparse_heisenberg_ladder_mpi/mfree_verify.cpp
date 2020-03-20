4c2c23b8629af2f79e789aa4ccff971b7d8f5820
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-03-20 09:05:10 +0900

    Use rokko::heisenberg_mfree in benchmark/use_rokko

--- benchmark/use_rokko/sparse_heisenberg_ladder_mpi/mfree_verify.cpp
+++ benchmark/use_rokko/sparse_heisenberg_ladder_mpi/mfree_verify.cpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,41 +10,9 @@
 *****************************************************************************/
 
 #include <rokko/rokko.hpp>
-#include <rokko/utility/heisenberg_hamiltonian_mpi.hpp>
+#include <rokko/utility/heisenberg_hamiltonian_mfree.hpp>
 #include <rokko/utility/lattice.hpp>
-#include <rokko/utility/math.hpp>
 #include <rokko/utility/machine_info.hpp>
-
-class heisenberg_op : public rokko::distributed_mfree_default {
-public:
-  heisenberg_op(int L, const std::vector<std::pair<int, int>>& lattice)
-    : L_(L), lattice_(lattice) {
-    comm_ = MPI_COMM_WORLD;
-    int size, rank;
-    MPI_Comm_size(comm_, &size);
-    MPI_Comm_rank(comm_, &rank);
-    const int p = rokko::find_power_of_two(size);
-    dim_ = 1 << L;
-    num_local_rows_ = 1 << (L-p);
-    local_offset_ = num_local_rows_ * rank;
-    buffer_.assign(num_local_rows_, 0);
-  }
-  ~heisenberg_op() {}
-
-  void multiply(const double* x, double* y) const {
-    rokko::heisenberg_hamiltonian::multiply(comm_, L_, lattice_, x, y, buffer_.data());
-  }
-  int get_dim() const { return dim_; }
-  int get_local_offset() const { return local_offset_; }
-  int get_num_local_rows() const { return num_local_rows_; }
-
-private:
-  MPI_Comm comm_;
-  int L_;
-  std::vector<std::pair<int, int>> lattice_;
-  int dim_, local_offset_, num_local_rows_;
-  mutable std::vector<double> buffer_;
-};
 
 int main(int argc, char *argv[]) {
   int provided;
@@ -78,8 +46,8 @@ int main(int argc, char *argv[]) {
   
   MPI_Barrier(MPI_COMM_WORLD);
   gen_tick = MPI_Wtime();
-  heisenberg_op mat(L, lattice);
-  
+  rokko::heisenberg_mfree mat(L, lattice);
+
   MPI_Barrier(MPI_COMM_WORLD);
   diag_tick = MPI_Wtime();
   rokko::parameters params;
