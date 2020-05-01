ea87da1a4606d18c0f0f555fa41346146c07f83e
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-05-01 16:17:54 +0900

    Add dispatch for LAPACKE of steqr and hteqr

---
+++ example/lapack/steqr.cpp
@@ -0,0 +1,51 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2020 Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/blas.hpp>
+#include <rokko/lapack/hteqr.hpp>
+#include <rokko/lapack/lange.hpp>
+#include <rokko/eigen3.hpp>
+#include <rokko/utility/laplacian_matrix.hpp>
+
+int main(int argc, char** argv) {
+  constexpr double eps = 1e-10;
+  int n = 5;
+  if (argc > 1) n = std::stoi(argv[1]);
+
+  // generate matrix
+  Eigen::VectorXd d(n), e(n);
+  rokko::laplacian_matrix::generate(d, e);
+
+  // diagonalization
+  Eigen::VectorXd w = d;
+  Eigen::MatrixXd u = Eigen::MatrixXd::Identity(n,n);
+  int info = rokko::lapack::steqr('V', w, e, u);
+  std::cout << "Eigenvalues: " << std::endl << w << std::endl;
+  std::cout << "Eigenvectors: " << std::endl << u << std::endl;
+
+  // orthogonality check
+  Eigen::MatrixXd check1 = u.adjoint() * u - Eigen::MatrixXd::Identity(n, n);
+  double norm1 = rokko::lapack::lange('F', check1);
+  std::cout << "|| U^t U - I || = " << norm1 << std::endl;
+  if (norm1 > eps) throw std::runtime_error("Error: orthogonality check");
+
+  // eigenvalue check
+  Eigen::MatrixXd a(n,n);
+  rokko::laplacian_matrix::generate(a);
+  Eigen::MatrixXd check2 = u.adjoint() * a * u;
+  check2.diagonal() -= w;
+  double norm2 = rokko::lapack::lange('F', check2);
+  std::cout << check2 << std::endl;
+  std::cout << "|| U^t A U - diag(w) || = " << norm2 << std::endl;
+  if (norm2 > eps) throw std::runtime_error("Error: eigenvalue check");
+
+  return 0;
+}
---
+++ rokko/lapack/hteqr.hpp
@@ -0,0 +1,89 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2017-2020 by Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#ifndef ROKKO_LAPACK_HSTEQR_HPP
+#define ROKKO_LAPACK_HSTEQR_HPP
+
+#include <complex>
+#include <lapacke.h>
+#undef I
+#include <rokko/traits/norm_t.hpp>
+#include <rokko/traits/value_t.hpp>
+#include "complex_cast.hpp"
+
+namespace rokko {
+namespace lapack {
+
+namespace {
+
+template<typename T> struct hsteqr_dispatch;
+
+template<>
+struct hsteqr_dispatch<float> {
+  template<typename MATRIX, typename VECTOR>
+  static lapack_int hsteqr(int matrix_layout, char compz, lapack_int n,
+                           VECTOR& d, VECTOR& e, MATRIX& z) {
+    return LAPACKE_ssteqr(matrix_layout, compz, n, storage(d), storage(e), storage(z), lda(z));
+  }
+};
+
+template<>
+struct hsteqr_dispatch<double> {
+  template<typename MATRIX, typename VECTOR>
+  static lapack_int hsteqr(int matrix_layout, char compz, lapack_int n,
+                           VECTOR& d, VECTOR& e, MATRIX& z) {
+    return LAPACKE_dsteqr(matrix_layout, compz, n, storage(d), storage(e), storage(z), lda(z));
+  }
+};
+
+template<>
+struct hsteqr_dispatch<std::complex<float>> {
+  template<typename MATRIX, typename VECTOR>
+  static lapack_int hsteqr(int matrix_layout, char compz, lapack_int n,
+                           VECTOR& d, VECTOR& e, MATRIX& z) {
+    return LAPACKE_csteqr(matrix_layout, compz, n, storage(d), storage(e), storage(z), lda(z));
+  }
+};
+
+template<>
+struct hsteqr_dispatch<std::complex<double>> {
+  template<typename MATRIX, typename VECTOR>
+  static lapack_int hsteqr(int matrix_layout, char compz, lapack_int n,
+                           VECTOR& d, VECTOR& e, MATRIX& z) {
+    return LAPACKE_zsteqr(matrix_layout, compz, n, storage(d), storage(e), storage(z), lda(z));
+  }
+};
+
+} // end of anonymous namespace
+
+template<typename MATRIX, typename VECTOR>
+lapack_int hsteqr(char compz, VECTOR& d, VECTOR& e, MATRIX& z) {
+  BOOST_STATIC_ASSERT(std::is_same<norm_t<MATRIX>, value_t<VECTOR>>::value);
+  lapack_int n = rows(z);
+  if (rows(z) != cols(z))
+    throw std::invalid_argument("matrix Z size mismatch");
+  if (size(d) != n)
+    throw std::invalid_argument("vector d size mismatch");
+  if (size(e) != n)
+    throw std::invalid_argument("vector e size mismatch");
+  return hsteqr_dispatch<value_t<MATRIX>>::
+    hsteqr((is_col_major(z) ? LAPACK_COL_MAJOR : LAPACK_ROW_MAJOR), compz, n, d, e, z);
+}
+
+template<typename MATRIX, typename VECTOR>
+lapack_int steqr(char compz, VECTOR& d, VECTOR& e, MATRIX& z) {
+  return hsteqr(compz, d, e, z);
+};
+
+} // end namespace lapack
+} // end namespace rokko
+
+#endif // ROKKO_LAPACK_HSTEQR_HPP
