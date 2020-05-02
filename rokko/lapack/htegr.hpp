c70fd217643213739e06055f721108faa105fc36
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-05-02 22:22:57 +0900

    Add dispatch for LAPACKE of tegr

---
+++ rokko/lapack/htegr.hpp
@@ -0,0 +1,97 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2017-2020 by Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#ifndef ROKKO_LAPACK_HTEGR_HPP
+#define ROKKO_LAPACK_HTEGR_HPP
+
+#include <complex>
+#include <lapacke.h>
+#undef I
+#include <rokko/traits/norm_t.hpp>
+#include <rokko/traits/value_t.hpp>
+#include "complex_cast.hpp"
+
+namespace rokko {
+namespace lapack {
+
+namespace {
+
+template<typename T> struct htegr_dispatch;
+
+template<>
+struct htegr_dispatch<float> {
+  template<typename MATRIX, typename VECTOR, typename VECTOR_INT>
+  static lapack_int htegr(int matrix_layout, char jobz, char range, lapack_int n, VECTOR& d, VECTOR& e,
+                          float vl, float vu, lapack_int il, lapack_int iu, float abstol,
+                          lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+    return LAPACKE_sstegr(matrix_layout, jobz, range, n, storage(d), storage(e), vl, vu, il, iu, abstol, &m, storage(w), storage(z), ld(z), storage(isuppz));
+  }
+};
+
+template<>
+struct htegr_dispatch<double> {
+  template<typename MATRIX, typename VECTOR, typename VECTOR_INT>
+  static lapack_int htegr(int matrix_layout, char jobz, char range, lapack_int n, VECTOR& d, VECTOR& e,
+                          double vl, double vu, lapack_int il, lapack_int iu, double abstol,
+                          lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+    return LAPACKE_dstegr(matrix_layout, jobz, range, n, storage(d), storage(e), vl, vu, il, iu, abstol, &m, storage(w), storage(z), ld(z), storage(isuppz));
+  }
+};
+
+template<>
+struct htegr_dispatch<std::complex<float>> {
+  template<typename MATRIX, typename VECTOR, typename VECTOR_INT>
+  static lapack_int htegr(int matrix_layout, char jobz, char range, lapack_int n, VECTOR& d, VECTOR& e,
+                          float vl, float vu, lapack_int il, lapack_int iu, float abstol,
+                          lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+    return LAPACKE_sstegr(matrix_layout, jobz, range, n, storage(d), storage(e), vl, vu, il, iu, abstol, &m, storage(w), complex_cast(storage(z)), ld(z), storage(isuppz));
+  }
+};
+
+template<>
+struct htegr_dispatch<std::complex<double>> {
+  template<typename MATRIX, typename VECTOR, typename VECTOR_INT>
+  static lapack_int htegr(int matrix_layout, char jobz, char range, lapack_int n, VECTOR& d, VECTOR& e,
+                          double vl, double vu, lapack_int il, lapack_int iu, double abstol,
+                          lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+    return LAPACKE_dstegr(matrix_layout, jobz, range, n, storage(d), storage(e), vl, vu, il, iu, abstol, &m, storage(w), complex_cast(storage(z)), ld(z), storage(isuppz));
+  }
+};
+
+} // end of anonymous namespace
+
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int htegr(char jobz, char range, VECTOR& d, VECTOR& e,
+                 T vl, T vu, lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+  BOOST_STATIC_ASSERT(std::is_same<norm_t<MATRIX>, T>::value);
+  BOOST_STATIC_ASSERT(std::is_same<value_t<VECTOR>, T>::value);
+
+  lapack_int n = size(d);
+  if (size(e) != n)  // e(n) is used as internal workspace
+    throw std::invalid_argument("vector e size mismatch");
+  if (size(w) != n)
+    throw std::invalid_argument("vector w size mismatch");
+  return htegr_dispatch<value_t<MATRIX>>::
+    htegr((is_col_major(z) ? LAPACK_COL_MAJOR : LAPACK_ROW_MAJOR), jobz, range, n, d, e, vl, vu, il, iu, abstol, m, w, z, isuppz);
+}
+
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int stegr(char jobz, char range, VECTOR& d, VECTOR& e,
+                 T vl, T vu, lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+  return htegr(jobz, range, d, e, vl, vu, il, iu, abstol, m, w, z, isuppz);
+};
+
+} // end namespace lapack
+} // end namespace rokko
+
+#endif // ROKKO_LAPACK_HTEGR_HPP
