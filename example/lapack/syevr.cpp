95e3530cd5cac681920b488372994dcbc81d12a9
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-05-04 16:50:10 +0900

    Add dispatch for LAPACKE of heevr

---
+++ example/lapack/syevr.cpp
@@ -0,0 +1,60 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#include <rokko/blas.hpp>
+#include <rokko/lapack/syevr.hpp>
+#include <rokko/lapack/lange.hpp>
+#include <rokko/eigen3.hpp>
+
+int main(int argc, char** argv) {
+  constexpr double eps = 1e-10;
+  int n = 5;
+  if (argc > 1) n = std::stoi(argv[1]);
+
+  // generate matrix
+  Eigen::MatrixXd a(n, n);
+  for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < n; ++i) {
+      a(i, j) = std::min(i, j) + 1;
+    }
+  }
+  Eigen::MatrixXd a0 = a;
+  std::cout << "Matrix A: " << std::endl << a << std::endl;
+
+  // diagonalization
+  Eigen::VectorXd w(n);
+  Eigen::MatrixXd u(n,n);
+  constexpr double vl = 0, vu = 0;
+  constexpr int il = 0, iu = 0;
+  constexpr double abstol = 0.;
+  int m;
+  Eigen::VectorXi isuppz(n);
+
+  int info = rokko::lapack::syevr('V', 'A', 'U', a, vl, vu, il, iu, abstol, m, w, u, isuppz);
+  if (info) throw std::runtime_error("Error: syevr failed");
+  std::cout << "Eigenvalues: " << std::endl << w << std::endl;
+  std::cout << "Eigenvectors: " << std::endl << u << std::endl;
+
+  // orthogonality check
+  Eigen::MatrixXd check1 = u.adjoint() * u - Eigen::MatrixXd::Identity(n, n);
+  double norm1 = rokko::lapack::lange('F', check1);
+  std::cout << "|| U^t U - I || = " << norm1 << std::endl;
+  if (norm1 > eps) throw std::runtime_error("Error: orthogonality check");
+
+  // eigenvalue check
+  Eigen::MatrixXd check2 = u.adjoint() * a0 * u;
+  check2.diagonal() -= w;
+  double norm2 = rokko::lapack::lange('F', check2);
+  std::cout << "|| U^t A U - diag(w) || = " << norm2 << std::endl;
+  if (norm2 > eps) throw std::runtime_error("Error: eigenvalue check");
+
+  return 0;
+}
---
+++ rokko/lapack/heevr.hpp
@@ -0,0 +1,157 @@
+/*****************************************************************************
+*
+* Rokko: Integrated Interface for libraries of eigenvalue decomposition
+*
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#ifndef ROKKO_LAPACK_HEEVR_HPP
+#define ROKKO_LAPACK_HEEVR_HPP
+
+#include <complex>
+#include <lapacke.h>
+#undef I
+#include <rokko/traits/norm_t.hpp>
+#include <rokko/traits/value_t.hpp>
+#include "complex_cast.hpp"
+#include <rokko/lapack/null_matrix.hpp>
+#include <rokko/alias_template_function.hpp>
+
+namespace rokko {
+namespace lapack {
+
+namespace {
+
+template<typename T> struct heevr_dispatch;
+
+template<>
+struct heevr_dispatch<float> {
+  template<typename MATRIX, typename MATRIX2, typename VECTOR, typename VECTOR_INT>
+  static lapack_int heevr(int matrix_layout, char jobz, char range, char uplo, lapack_int n, MATRIX& a,
+                          float vl, float vu, lapack_int il, lapack_int iu, float abstol,
+                          lapack_int& m, VECTOR& w, MATRIX2& z, VECTOR_INT& isuppz) {
+    return LAPACKE_ssyevr(matrix_layout, jobz, range, uplo, n, storage(a), ld(a), vl, vu, il, iu, abstol, &m, storage(w), storage(z), ld(z), storage(isuppz));
+  }
+};
+
+template<>
+struct heevr_dispatch<double> {
+  template<typename MATRIX, typename MATRIX2, typename VECTOR, typename VECTOR_INT>
+  static lapack_int heevr(int matrix_layout, char jobz, char range, char uplo, lapack_int n, MATRIX& a,
+                          double vl, double vu, lapack_int il, lapack_int iu, double abstol,
+                          lapack_int& m, VECTOR& w, MATRIX2& z, VECTOR_INT& isuppz) {
+    return LAPACKE_dsyevr(matrix_layout, jobz, range, uplo, n, storage(a), ld(a), vl, vu, il, iu, abstol, &m, storage(w), storage(z), ld(z), storage(isuppz));
+  }
+};
+
+template<>
+struct heevr_dispatch<std::complex<float>> {
+  template<typename MATRIX, typename MATRIX2, typename VECTOR, typename VECTOR_INT>
+  static lapack_int heevr(int matrix_layout, char jobz, char range, char uplo, lapack_int n, MATRIX& a,
+                          float vl, float vu, lapack_int il, lapack_int iu, float abstol,
+                          lapack_int& m, VECTOR& w, MATRIX2& z, VECTOR_INT& isuppz) {
+    return LAPACKE_cheevr(matrix_layout, jobz, range, uplo, n, complex_cast(storage(a)), ld(a), vl, vu, il, iu, abstol, &m, storage(w), complex_cast(storage(z)), ld(z), storage(isuppz));
+  }
+};
+
+template<>
+struct heevr_dispatch<std::complex<double>> {
+  template<typename MATRIX, typename MATRIX2, typename VECTOR, typename VECTOR_INT>
+  static lapack_int heevr(int matrix_layout, char jobz, char range, char uplo, lapack_int n, MATRIX& a,
+                          double vl, double vu, lapack_int il, lapack_int iu, double abstol,
+                          lapack_int& m, VECTOR& w, MATRIX2& z, VECTOR_INT& isuppz) {
+    return LAPACKE_zheevr(matrix_layout, jobz, range, uplo, n, complex_cast(storage(a)), ld(a), vl, vu, il, iu, abstol, &m, storage(w), complex_cast(storage(z)), ld(z), storage(isuppz));
+  }
+};
+
+} // end of anonymous namespace
+
+template<typename T, typename MATRIX, typename MATRIX2, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char jobz, char range, char uplo, MATRIX& a,
+                 T vl, T vu, lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX2& z, VECTOR_INT& isuppz) {
+  BOOST_STATIC_ASSERT(std::is_same<norm_t<MATRIX>, value_t<VECTOR>>::value);
+  lapack_int n = rows(a);
+  if (rows(a) != cols(a))
+    throw std::invalid_argument("matrix A size mismatch");
+  if (size(w) != n)
+    throw std::invalid_argument("vector w size mismatch");
+  return heevr_dispatch<value_t<MATRIX>>::
+    heevr((is_col_major(a) ? LAPACK_COL_MAJOR : LAPACK_ROW_MAJOR), jobz, range, uplo, n, a, vl, vu, il, iu, abstol, m, w, z, isuppz);
+}
+
+// eigenvalues & eigenvectors (without jobz)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char range, char uplo, MATRIX& a,
+                 T vl, T vu, lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+  return heevr('V', range, uplo, a, vl, vu, il, iu, abstol, m, w, z, isuppz);
+}
+
+// eigenvalues & eigenvectors, use all (without jobz, range, vl, vu, il, iu)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char uplo, MATRIX& a,
+                 T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+  return heevr('A', uplo, a, 0., 0., 0, 0, abstol, m, w, z, isuppz);
+}
+
+// eigenvalues & eigenvectors, use vl, vu (without jobz, range, il, iu)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char uplo, MATRIX& a,
+                 T vl, T vu, T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+  return heevr('V', uplo, a, vl, vu, 0, 0, abstol, m, w, z, isuppz);
+}
+
+// eigenvalues & eigenvectors, use il, iu (without jobz, range, vl, vu)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char uplo, MATRIX& a,
+                 lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, MATRIX& z, VECTOR_INT& isuppz) {
+  return heevr('I', uplo, a, 0., 0., il, iu, abstol, m, w, z, isuppz);
+}
+
+// only eigenvalues (without jobz)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char range, char uplo, MATRIX& a,
+                 T vl, T vu, lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, VECTOR_INT& isuppz) {
+  constexpr null_matrix<T> z_null;
+  return heevr('N', range, uplo, a, vl, vu, il, iu, abstol, m, w, z_null, isuppz);
+}
+
+// only eigenvalues, use all (without jobz, range, vl, vu, il, iu)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char uplo, MATRIX& a,
+                 T abstol,
+                 lapack_int& m, VECTOR& w, VECTOR_INT& isuppz) {
+  return heevr('A', uplo, a, 0., 0., 0, 0, abstol, m, w, isuppz);
+}
+
+// only eigenvalues, use vl, vu (without jobz, range, il, iu)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char uplo, MATRIX& a,
+                 T vl, T vu, T abstol,
+                 lapack_int& m, VECTOR& w, VECTOR_INT& isuppz) {
+  return heevr('V', uplo, a, vl, vu, 0, 0, abstol, m, w, isuppz);
+}
+
+// only eigenvalues, use il, iu (without jobz, range, vl, vu)
+template<typename T, typename MATRIX, typename VECTOR, typename VECTOR_INT>
+lapack_int heevr(char uplo, MATRIX& a,
+                 lapack_int il, lapack_int iu, T abstol,
+                 lapack_int& m, VECTOR& w, VECTOR_INT& isuppz) {
+  return heevr('I', uplo, a, 0., 0., il, iu, abstol, m, w, isuppz);
+}
+
+ALIAS_TEMPLATE_FUNCTION(syevr, heevr);
+
+} // end namespace lapack
+} // end namespace rokko
+
+#endif // ROKKO_LAPACK_HEEVR_HPP
