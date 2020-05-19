a21ee3347564c3dcc335e9665978e9df21f631b3
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-05-19 19:21:24 +0900

    Fix: Generalize Python's gather & scatter to template

--- rokko/pyrokko_collective.hpp
+++ rokko/pyrokko_collective.hpp
@@ -19,25 +19,25 @@
 namespace rokko {
 
 template<typename T, typename MATRIX_MAJOR>
-void pyrokko_gather(wrap_distributed_matrix<double,MATRIX_MAJOR> const& from, Eigen::Ref<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,rokko::eigen3_major<MATRIX_MAJOR>>> to_in, int root) {
+void pyrokko_gather(wrap_distributed_matrix<T,MATRIX_MAJOR> const& from, Eigen::Ref<Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,rokko::eigen3_major<MATRIX_MAJOR>>> to_in, int root) {
   constexpr int major = rokko::eigen3_major<MATRIX_MAJOR>;
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,major> to;
-  new (&to) Eigen::Map<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,major>>(to_in.data(), to_in.rows(), to_in.cols());
+  Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,major> to;
+  new (&to) Eigen::Map<Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,major>>(to_in.data(), to_in.rows(), to_in.cols());
 
   gather(from, to, root);
 
-  new (&to) Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,major>();
+  new (&to) Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,major>();
 }
 
 template<typename T, typename MATRIX_MAJOR>
-void pyrokko_scatter(Eigen::Ref<Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,rokko::eigen3_major<MATRIX_MAJOR>>> from_in, wrap_distributed_matrix<double,MATRIX_MAJOR>& to, int root) {
+void pyrokko_scatter(Eigen::Ref<Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,rokko::eigen3_major<MATRIX_MAJOR>>> from_in, wrap_distributed_matrix<T,MATRIX_MAJOR>& to, int root) {
   constexpr int major = rokko::eigen3_major<MATRIX_MAJOR>;
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,major> from;
-  new (&from) Eigen::Map<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,major>>(from_in.data(), from_in.rows(), from_in.cols());
+  Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,major> from;
+  new (&from) Eigen::Map<Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,major>>(from_in.data(), from_in.rows(), from_in.cols());
 
   scatter(from, to, root);
 
-  new (&from) Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,major>();
+  new (&from) Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,major>();
 }
 
 } // end namespace rokko
