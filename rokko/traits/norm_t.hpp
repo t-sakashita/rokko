a60ff8b556201c59081754bd7579d83e9a96b7f5
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-05-20 17:48:03 +0900

    Rewrite norm_t without boost::numeric::ublas::type_traits

--- rokko/traits/norm_t.hpp
+++ rokko/traits/norm_t.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2017 by Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 by Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -14,36 +14,39 @@
 
 #include "value_t.hpp"
 #include <complex>
-#include <boost/numeric/ublas/traits.hpp>
 
 namespace rokko {
 
 namespace detail {
 
 template<typename T>
+struct norm_scalar_type_traits {
+};
+
+template<>
+struct norm_scalar_type_traits<float> {
+  using type = float;
+};
+
+template<>
+struct norm_scalar_type_traits<std::complex<float>> {
+  using type = float;
+};
+
+template<>
+struct norm_scalar_type_traits<double> {
+  using type = double;
+};
+
+template<>
+struct norm_scalar_type_traits<std::complex<double>> {
+  using type = double;
+};
+
+template<typename T>
 struct norm_type_traits {
   using value_type = rokko::value_t<T>;
-  using type = typename boost::numeric::ublas::type_traits<value_type>::real_type;
-};
-
-template<>
-struct norm_type_traits<float> {
-  using type = float;
-};
-
-template<>
-struct norm_type_traits<std::complex<float>> {
-  using type = float;
-};
-
-template<>
-struct norm_type_traits<double> {
-  using type = double;
-};
-
-template<>
-struct norm_type_traits<std::complex<double>> {
-  using type = double;
+  using type = typename norm_scalar_type_traits<value_type>::type;
 };
 
 } // end namespace detail
