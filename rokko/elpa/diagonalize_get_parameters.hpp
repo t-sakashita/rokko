1881f3c9b5b24ad64f3f377fa2437f756e6c1ebf
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-06-23 19:03:45 +0900

    Add include in elpa/diagonalize_get_parameters.hpp

--- rokko/elpa/diagonalize_get_parameters.hpp
+++ rokko/elpa/diagonalize_get_parameters.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2015 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,6 +11,8 @@
 
 #ifndef ROKKO_ELPA_DIAGONALIZE_GET_PARAMETERS_HPP
 #define ROKKO_ELPA_DIAGONALIZE_GET_PARAMETERS_HPP
+
+#include <rokko/parameters.hpp>
 
 namespace rokko {
 namespace elpa {
