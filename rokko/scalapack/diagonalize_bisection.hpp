2e3c74c2d0c3d7c70d3f7566f89a1814791c2230
Tatsuya Sakashita <tatsuyasakashita@eng.tamagawa.ac.jp>
2020-06-24 19:02:22 +0900

    Remove unused include file

--- rokko/scalapack/diagonalize_bisection.hpp
+++ rokko/scalapack/diagonalize_bisection.hpp
@@ -2,7 +2,7 @@
 *
 * Rokko: Integrated Interface for libraries of eigenvalue decomposition
 *
-* Copyright (C) 2012-2019 Rokko Developers https://github.com/t-sakashita/rokko
+* Copyright (C) 2012-2020 Rokko Developers https://github.com/t-sakashita/rokko
 *
 * Distributed under the Boost Software License, Version 1.0. (See accompanying
 * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -14,7 +14,6 @@
 
 #include <rokko/distributed_matrix.hpp>
 #include <rokko/parameters.hpp>
-#include <rokko/cscalapack.h>
 #include <rokko/lapack/diagonalize_get_parameters.hpp>
 #include <rokko/scalapack/diagonalize_psyevx.hpp>
 #include <rokko/utility/timer.hpp>
