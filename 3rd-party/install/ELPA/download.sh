96cce892cdc167eded4d73255e8010e632579bfb
Tatsuya Sakashita <sakashita@qiqb.osaka-u.ac.jp>
2021-06-23 22:23:08 +0900

    Update ELPA's directory in URL

--- 3rd-party/install/ELPA/download.sh
+++ 3rd-party/install/ELPA/download.sh
@@ -7,5 +7,5 @@ set_prefix
 
 FILE=elpa-$ELPA_VERSION.tar.gz
 if [ -f $SOURCE_DIR/$FILE ]; then :; else
-  wget -O $SOURCE_DIR/$FILE http://elpa.mpcdf.mpg.de/html/Releases/$ELPA_VERSION/$FILE
+  wget -O $SOURCE_DIR/$FILE https://elpa.mpcdf.mpg.de/software/tarball-archive/Releases/$ELPA_VERSION/$FILE
 fi
