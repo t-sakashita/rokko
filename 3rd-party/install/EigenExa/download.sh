25d6596689f51d8d66749b7c50ddfebfce465209
Tatsuya Sakashita <sakashita@qiqb.osaka-u.ac.jp>
2021-06-23 21:38:02 +0900

    Update EigenExa to 2.7

--- 3rd-party/install/EigenExa/download.sh
+++ 3rd-party/install/EigenExa/download.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/version.sh
 set_prefix
 
-FILE="EigenExa-$EIGENEXA_VERSION.tgz"
+FILE="EigenExa-$EIGENEXA_VERSION.tar.gz"
 if [ -f $SOURCE_DIR/$FILE ]; then :; else
   wget -O $SOURCE_DIR/$FILE https://www.r-ccs.riken.jp/labs/lpnctrt/projects/eigenexa/$FILE
 fi
--- 3rd-party/install/EigenExa/setup.sh
+++ 3rd-party/install/EigenExa/setup.sh
@@ -10,7 +10,7 @@ sh $SCRIPT_DIR/download.sh
 cd $BUILD_DIR
 if [ -d eigenexa-$EIGENEXA_VERSION ]; then :; else
   check mkdir -p eigenexa-$EIGENEXA_VERSION
-  tar zxf $SOURCE_DIR/EigenExa-$EIGENEXA_VERSION.tgz -C eigenexa-$EIGENEXA_VERSION --strip-components=1
+  tar zxf $SOURCE_DIR/EigenExa-$EIGENEXA_VERSION.tar.gz -C eigenexa-$EIGENEXA_VERSION --strip-components=1
   cd eigenexa-$EIGENEXA_VERSION
   if [ -f $SCRIPT_DIR/EigenExa-$EIGENEXA_VERSION.patch ]; then
     echo "applying EigenExa-$EIGENEXA_VERSION.patch"
